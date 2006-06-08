db4a66850df22ac88f5aceeec5f8d75f6c652037
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-06-08 12:20:56 +0000

    Made simpler removed extra bit
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4388 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/BacterialNomenclatureHandler.java
+++ genedb-access/src/org/genedb/db/loading/BacterialNomenclatureHandler.java
@@ -14,16 +14,15 @@ import java.util.regex.Pattern;
 
 public class BacterialNomenclatureHandler extends BaseNomenclatureHandler implements NomenclatureHandler {
 
-    private Pattern pattern;
-    
+       
     public BacterialNomenclatureHandler() {
-	//pattern = Pattern.compile("SP(A|B|C)(P|C)(\\w+)\\.\\d+c?");
+	
     }
     
     public Names findNamesInternal(Annotation an) {
 	
 	Names ret = new Names();
-	//String geneId = null;
+	
 	String temp = null;
 
         List<String> names = MiningUtils.getProperties("gene", an);
@@ -38,7 +37,6 @@ public class BacterialNomenclatureHandler extends BaseNomenclatureHandler implem
         		temp = names.get(i);
         		if(temp.charAt(0)== 'T' && temp.charAt(1) == 'W')
         		{
-        			//geneId = temp;
         			names.remove(i);
         			ret.setSystematicIdAndTemp(temp, false);
         			 		
@@ -48,58 +46,9 @@ public class BacterialNomenclatureHandler extends BaseNomenclatureHandler implem
         }
         else
         {
-        	//geneId = names.get(0);
         	ret.setSystematicIdAndTemp(names.get(0), false);
         }
-        
-        
-        // Remove duplicates, maintaining order
-        //      Set tmp = new LinkedHashSet();
-        /*
-        Set<String> tmp = new InsertionOrderedSet<String>();
-        tmp.addAll(names);
-        names.clear();
-        names.addAll(tmp);
-
-        // Move all the systematics ids into a new list
-        List<String> systematics = new ArrayList<String>();
-        Iterator nameIt = names.iterator();
-        while (nameIt.hasNext()) {
-            String test = (String) nameIt.next();
-            // SP(A|B|C)(P|C)$cosmid.num(c?)
-            //Matcher matcher = pattern.matcher(test);
-            //if (matcher.matches()) {
-                systematics.add(test);
-                nameIt.remove();
-            //}
-        }
-
-        // Set the gene name if there is one
-        if (names.size() > 0) {
-            ret.setPrimary(names.get(0));
-            names.remove(0);
-        }
-
-        String geneId = null;
-        if (systematics.size() == 0) {
-            System.err.println("Annotation '"+an+"'");
-            throw new RuntimeException("SpombeNomenclatureHandler: No systematic id");
-            //System.err.println("WARN: Setting gene id to gene name ie " + gene.getName());
-            //geneId = ret.getPrimary();
-        }
-
-        if (systematics.size() > 0) {
-            // Have more than one systematic name. Find the "alphabetically" first
-            Collections.sort(systematics);
-            geneId = systematics.get(0);
-            systematics.remove(0);
-        }*/
-
-        
-
-        //names.addAll(systematics);
-        
-                
+                    
         return ret;
     }
     
