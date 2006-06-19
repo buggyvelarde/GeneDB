9ca57ae348ef22f74261192a3a68ae2477215d65
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-06-19 13:10:29 +0000

    Add NPE check
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4434 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/genedb/db/jpa/Feature.java
+++ genedb-db/src/org/genedb/db/jpa/Feature.java
@@ -3,16 +3,16 @@ package org.genedb.db.jpa;
 import org.genedb.db.hibernate3gen.BaseFeature;
 import org.genedb.db.hibernate3gen.FeatureProp;
 
-import java.util.Set;
-
 public class Feature extends BaseFeature {
 
     @Override
     public void setResidues(String residues) {
-	super.setResidues(residues);
-	super.setSeqlen(residues.length());
-	// TODO - should be fixed once packages sorted
-//	super.setMd5checksum(FeatureUtils.calcMD5(residues));
+        if (residues != null) {
+            super.setResidues(residues);
+            super.setSeqlen(residues.length());
+            // TODO - should be fixed once packages sorted
+            //	super.setMd5checksum(FeatureUtils.calcMD5(residues));
+        }
     }
     
     public void addFeatureProp(FeatureProp fp) {
