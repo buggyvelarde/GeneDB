607fb05ae7a5d373016e970118965ce9465f3201
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-07-05 16:15:20 +0000

    Add find GO cvterm
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4520 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/dao/CvTermDao.java
+++ genedb-access/src/org/genedb/db/dao/CvTermDao.java
@@ -2,10 +2,18 @@ package org.genedb.db.dao;
 
 import org.genedb.db.hibernate3gen.Cv;
 import org.genedb.db.hibernate3gen.CvTerm;
+import org.genedb.db.hibernate3gen.CvTermDbXRef;
+import org.genedb.db.hibernate3gen.Db;
+import org.genedb.db.hibernate3gen.DbXRef;
 
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
+import java.util.Set;
 
 public class CvTermDao extends HibernateDaoSupport {
 
@@ -28,5 +36,34 @@ public class CvTermDao extends HibernateDaoSupport {
 		"acc", value);
 	return terms.get(0);
     }
-
+    
+    @SuppressWarnings("unchecked")
+    public CvTerm findGoCvTermByAccViaDb(final String id, final DaoFactory daoFactory) {
+        final Db DB_GO = daoFactory.getDbDao().findByName("GO");
+        
+        // Find cvterm for db_xref
+        TransactionTemplate tt = new TransactionTemplate(daoFactory.getTransactionManager());
+        CvTerm cvTerm = (CvTerm) tt.execute(
+                new TransactionCallback() {
+                    public Object doInTransaction(TransactionStatus status) {
+                        final DbXRef dbxref = daoFactory.getDbXRefDao().findByDbAndAcc(DB_GO, id);
+                        if (dbxref == null) {
+                            logger.warn("Can't find dbxref for GO id of '"+id+"'");
+                            return null;
+                        }
+                        Set<CvTerm> cvTermDbXRefs = dbxref.getCvterms();
+                        if (cvTermDbXRefs.size() > 1) {
+                            logger.warn("More than one cvTerm for go id of '"+id+"'");
+                            return null;
+                        }
+                        if (cvTermDbXRefs.size() == 0) {
+                            logger.warn("No cvTerm for go id of '"+id+"'");
+                            return null;
+                        }
+                        return cvTermDbXRefs.iterator().next();
+                    }
+                });
+        return cvTerm;
+    }
+    
 }
