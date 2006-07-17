7794cb86f954babb79a5ece574e6e42dc03f8967
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-07-17 15:42:34 +0000

    Add a few more constants
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4548 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/EmblQualifiers.java
+++ genedb-access/src/org/genedb/db/loading/EmblQualifiers.java
@@ -32,38 +32,37 @@ package org.genedb.db.loading;
 public class EmblQualifiers {
 
 
-    public static final String QUAL_CHROMOSOME   = "chromosome";
-    public static final String QUAL_SUPERCONTIG  = "supercontig";
-    public static final String QUAL_CONTIG       = "contig";
-    public static final String QUAL_SO_TYPE      = "so_type";
-    public static final String QUAL_PRIVATE      = "private";
-    public static final String QUAL_PSEUDO       = "pseudo";
-    public static final String QUAL_NOTE         = "note";
-    public static final String QUAL_CURATION     = "curation";
-    public static final String QUAL_DB_XREF      = "db_xref";
-    public static final String QUAL_PRODUCT      = "product";
-    public static final String QUAL_GO           = "GO";
-    public static final String QUAL_C_CURATION   = "controlled_curation";
-    public static final String QUAL_EVIDENCE     = "evidence";
+    public static final String QUAL_CHROMOSOME    = "chromosome";
+    public static final String QUAL_SUPERCONTIG   = "supercontig";
+    public static final String QUAL_CONTIG        = "contig";
+    public static final String QUAL_SO_TYPE       = "so_type";
+    public static final String QUAL_PRIVATE       = "private";
+    public static final String QUAL_PSEUDO        = "pseudo";
+    public static final String QUAL_NOTE          = "note";
+    public static final String QUAL_CURATION      = "curation";
+    public static final String QUAL_DB_XREF       = "db_xref";
+    public static final String QUAL_PRODUCT       = "product";
+    public static final String QUAL_GO            = "GO";
+    public static final String QUAL_C_CURATION    = "controlled_curation";
+    public static final String QUAL_EVIDENCE      = "evidence";
     
     // Naming
-    public static final String QUAL_SYS_ID       = "systematic_id";
-    public static final String QUAL_TEMP_SYS_ID  = "temporary_systematic_id";
-    public static final String QUAL_PRIMARY      = "primary_name";
-    public static final String QUAL_SYNONYM      = "synonym";
-    public static final String QUAL_OBSOLETE     = "obsolete_name";
-    public static final String QUAL_RESERVED     = "reserved_name";
-    public static final String QUAL_PREV_SYS_ID  = "prev_systematic_id";
+    public static final String QUAL_SYS_ID        = "systematic_id";
+    public static final String QUAL_TEMP_SYS_ID   = "temporary_systematic_id";
+    public static final String QUAL_PRIMARY       = "primary_name";
+    public static final String QUAL_SYNONYM       = "synonym";
+    public static final String QUAL_OBSOLETE      = "obsolete_name";
+    public static final String QUAL_RESERVED      = "reserved_name";
+    public static final String QUAL_PREV_SYS_ID   = "prev_systematic_id";
+    public static final String QUAL_PROTEIN_NAME  = "protein_name";
     
     // Deprecated ie not output but will be read
-    public static final String QUAL_D_COLOUR     = "colour";
-    public static final String QUAL_D_GENE       = "gene";
-    public static final String QUAL_D_FASTA_FILE = "fasta_file";
-    public static final String QUAL_D_LITERATURE = "literature";
+    public static final String QUAL_D_COLOUR      = "colour";
+    public static final String QUAL_D_GENE        = "gene";
+    public static final String QUAL_D_FASTA_FILE  = "fasta_file";
+    public static final String QUAL_D_LITERATURE  = "literature";
     public static final String QUAL_D_PSU_DB_XREF = "psu_db_xref";
+    public static final String QUAL_D_EC_NUMBER   = "ec_number";
     
-    
-    
-	
 }
 
