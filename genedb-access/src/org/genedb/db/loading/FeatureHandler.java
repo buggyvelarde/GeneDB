b3fdcbcdfb33d7827cb59f75a88249a4c92f0b84
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-09-11 12:17:09 +0000

    Simplify feature handler interface as not used as much
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4727 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/FeatureHandler.java
+++ genedb-access/src/org/genedb/db/loading/FeatureHandler.java
@@ -1,30 +1,14 @@
 package org.genedb.db.loading;
 
-import org.genedb.db.dao.DaoFactory;
-import org.genedb.db.hibernate3gen.Organism;
-import org.genedb.db.jpa.Feature;
+import org.gmod.schema.sequence.Feature;
 
 import org.biojava.bio.BioException;
 import org.biojava.bio.seq.Sequence;
 import org.biojava.utils.ChangeVetoException;
 
-public interface FeatureHandler {
-
-    public void setDaoFactory(DaoFactory daoFactory);
-    
-    public void setOrganism(Organism organism);
+public interface FeatureHandler extends FeatureProcessor {
     
     public abstract Feature processSources(Sequence seq)
 	    throws ChangeVetoException, BioException;
-
-    public abstract void processCDS(Sequence seq, Feature topLevel, int offset);
-
-    public void setFeatureUtils(FeatureUtils utils);
-    
-    public void setGoParser(GoParser goParser);
-
-    public void setNomenclatureHandler(NomenclatureHandler handler);
-    
-    public void afterPropertiesSet();
 
 }
