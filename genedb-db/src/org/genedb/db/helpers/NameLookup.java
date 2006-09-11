3c0c397332a58d5375de404c3fe3d26a0e079201
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-09-11 13:53:27 +0000

    Add utility class for looking up information
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4734 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-db/src/org/genedb/db/helpers/NameLookup.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2006 Genome Research Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by  the Free Software Foundation; either version 2 of the License or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307 USA
+ */
+
+package org.genedb.db.helpers;
+
+//import java.util.List;
+
+public class NameLookup {
+    
+    private String lookup; // The name to lookup, using * for wildcards
+    //private List<Taxon> taxons; // The organism(s) to search through
+    private boolean needWildcards; // Whether to add wildcards at the beginning and end
+    private int start = 0; // Which number to start at if paging
+    private int pageSize; // How many results per page, if paging
+    //private FeatureTypeFilter filter;
+
+    public String getLookup() {
+        return this.lookup;
+    }
+    
+    public void setLookup(String lookup) {
+        this.lookup = lookup;
+    }
+    
+    public boolean isNeedWildcards() {
+        return this.needWildcards;
+    }
+    
+    public void setNeedWildcards(boolean needWildcards) {
+        this.needWildcards = needWildcards;
+    }
+    public int getPageSize() {
+        return this.pageSize;
+    }
+    
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+    
+    public int getStart() {
+        return this.start;
+    }
+    
+    public void setStart(int start) {
+        this.start = start;
+    }
+    
+//    public List<Taxon> getTaxons() {
+//        return this.taxons;
+//    }
+//    
+//    public void setTaxons(List<Taxon> taxons) {
+//        this.taxons = taxons;
+//    }
+
+}
