b36ee5932550af29f468112da1adbcad19e73043
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-09-12 10:23:47 +0000

    New Addition
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4745 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-db/src/org/genedb/db/propinterface/PropertyI.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.genedb.db.propinterface;
+
+import org.gmod.schema.cv.CvTerm;
+
+/**
+ * @author cp2
+ *
+ */
+public abstract interface PropertyI {
+
+	public abstract CvTerm getCvTerm();
+}
