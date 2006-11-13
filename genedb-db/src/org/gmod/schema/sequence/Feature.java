7c180dee80bc44074cc5349b1af41bf6c05fd25b
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-11-13 14:28:24 +0000

    setResidue - comment out check sum as this breaks for retrotranslator
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4907 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/sequence/Feature.java
+++ genedb-db/src/org/gmod/schema/sequence/Feature.java
@@ -281,13 +281,13 @@ public class Feature implements java.io.Serializable {
      */
     public void setResidues(byte[] residues) {
         this.residues = residues;
-        if (residues == null) {
+/*        if (residues == null) {
             seqLen = 0;
             md5Checksum = "";
             return;
         }
         seqLen = residues.length;
-        this.md5Checksum = calcMD5(this.residues);
+        this.md5Checksum = calcMD5(this.residues);*/
     }
     
 
