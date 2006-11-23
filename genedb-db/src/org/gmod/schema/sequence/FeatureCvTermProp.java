950a47b90432fade8d988d9cc025da87165d6c13
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-11-23 12:27:08 +0000

    Started documenting
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@4993 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/sequence/FeatureCvTermProp.java
+++ genedb-db/src/org/gmod/schema/sequence/FeatureCvTermProp.java
@@ -15,28 +15,82 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+
+/**
+ * This represents a key/value pair attached to a FeatureCvTerm. The key is itself a CvTerm, with 
+ * a free-text value
+ * 
+ * Database constraints: feature_cvtermprop_c1 unique (feature_cvterm_id, type_id, rank)
+ * 
+ * @author art
+ */
+/**
+ * @author art
+ *
+ */
 @Entity
 @Table(name="feature_cvtermprop")
 public class FeatureCvTermProp implements Serializable, PropertyI, Rankable {
 
+/*
+create table feature_cvtermprop (
+    feature_cvtermprop_id serial not null,
+    primary key (feature_cvtermprop_id),
+    feature_cvterm_id int not null,
+    foreign key (feature_cvterm_id) references feature_cvterm (feature_cvterm_id) on delete cascade,
+    type_id int not null,
+    foreign key (type_id) references cvterm (cvterm_id) on delete cascade INITIALLY DEFERRED,
+    value text null,
+    rank int not null default 0,
+    constraint feature_cvtermprop_c1 unique (feature_cvterm_id,type_id,rank)
+);
+
+COMMENT ON TABLE feature_cvtermprop IS 'Extensible properties for feature to cvterm associations. Examples: GO evidence codes; qualifiers; metadata such as the date on which the entry was curated and the source of the association';
+
+create index feature_cvtermprop_idx1 on feature_cvtermprop (feature_cvterm_id);
+create index feature_cvtermprop_idx2 on feature_cvtermprop (type_id);
+*/
+    
+    
+    
+    
     // Fields    
 
-     @Id
+    
+     /**
+     * Database unique primary key 
+     */
+    @Id
     @Column(name="feature_cvtermprop_id", unique=false, nullable=false, insertable=true, updatable=true)
      private int featureCvTermPropId;
      
-     @ManyToOne(cascade={}, fetch=FetchType.LAZY)
+
+    /**
+     * The CvTerm that acts as the key in this map of properties
+     */
+    @ManyToOne(cascade={}, fetch=FetchType.LAZY)
          @JoinColumn(name="type_id", unique=false, nullable=false, insertable=true, updatable=true)
      private CvTerm cvTerm;
      
-     @ManyToOne(cascade={},fetch=FetchType.LAZY)
+     /**
+     * The FeatureCvTerm to which this property is attached
+     */
+    @ManyToOne(cascade={},fetch=FetchType.LAZY)
          @JoinColumn(name="feature_cvterm_id", unique=false, nullable=false, insertable=true, updatable=true)
      private FeatureCvTerm featureCvTerm;
      
-     @Column(name="value", unique=false, nullable=true, insertable=true, updatable=true)
+     /**
+     * The value of this property
+     */
+    @Column(name="value", unique=false, nullable=true, insertable=true, updatable=true)
      private String value;
      
-     @Column(name="rank", unique=false, nullable=false, insertable=true, updatable=true)
+     /**
+     * The rank is used to distinguish multiple 
+     * values for the same key eg /foo="value1", /foo="value2" in an EMBL file could be stored as two FeatureCvTerm with 
+     * different ranks. The default is 0;
+     */
+    @Column(name="rank", unique=false, nullable=false, insertable=true, updatable=true)
      private int rank;
 
      // Constructors
@@ -105,30 +159,38 @@ public class FeatureCvTermProp implements Serializable, PropertyI, Rankable {
     }
     
 
-    /* (non-Javadoc)
-     * @see org.genedb.db.jpa.FeatureCvTermPropI#getValue()
+    /**
+     * Accessor for value
+     * 
+     * @see value
      */
     public String getValue() {
         return this.value;
     }
     
-    /* (non-Javadoc)
-     * @see org.genedb.db.jpa.FeatureCvTermPropI#setValue(java.lang.String)
+    /**
+     * Accessor for value
+     * 
+     * @see value
      */
     public void setValue(String value) {
         this.value = value;
     }
     
 
-    /* (non-Javadoc)
-     * @see org.genedb.db.jpa.FeatureCvTermPropI#getRank()
+    /**
+     * Accessor for rank
+     * 
+     * @see rank
      */
     public int getRank() {
         return this.rank;
     }
     
-    /* (non-Javadoc)
-     * @see org.genedb.db.jpa.FeatureCvTermPropI#setRank(int)
+    /**
+     * Accessor for rank
+     * 
+     * @see rank
      */
     public void setRank(int rank) {
         this.rank = rank;
