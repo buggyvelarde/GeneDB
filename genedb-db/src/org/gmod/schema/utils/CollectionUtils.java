f978203c53bfbd857ff3765731e613583aecfef8
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-12-04 13:48:12 +0000

    Start adding comments
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5051 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/utils/CollectionUtils.java
+++ genedb-db/src/org/gmod/schema/utils/CollectionUtils.java
@@ -3,8 +3,20 @@ package org.gmod.schema.utils;
 import java.util.Collection;
 import java.util.HashSet;
 
+/**
+ * Static utility class for miscellaneous collection handling
+ * 
+ * @author art
+ */
 public class CollectionUtils {
 
+    /**
+     * 
+     * 
+     * @param <T> 
+     * @param collection 
+     * @return the original collection, or an empty collection
+     */
     public static <T> Collection<T> safeGetter(Collection<T> collection) {
         if (collection != null) {
             return collection;
--- genedb-db/src/org/gmod/schema/utils/propinterface/PropertyI.java
+++ genedb-db/src/org/gmod/schema/utils/propinterface/PropertyI.java
@@ -1,15 +1,13 @@
-/**
- * 
- */
 package org.gmod.schema.utils.propinterface;
 
 import org.gmod.schema.cv.CvTerm;
 
 /**
+ * 
  * @author cp2
- *
  */
-public abstract interface PropertyI {
+public interface PropertyI {
 
 	public abstract CvTerm getCvTerm();
+
 }
