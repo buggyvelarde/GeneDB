9337f046c10e5aa802ebc15093e1ad795ab3721d
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-01-17 16:11:53 +0000

    Read in an sql file - maybe should be in installer instead?
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5190 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-access/src/org/genedb/db/loading/SqlUtils.java
@@ -0,0 +1,32 @@
+package org.genedb.db.loading;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+public class SqlUtils {
+    
+    private void executeSql(Reader r, DataSource ds, boolean includeDrop) throws IOException, SQLException {
+        BufferedReader in = new BufferedReader(r);
+        StringBuffer cmd = new StringBuffer();
+        String line;
+        while ((line=in.readLine())!=null) {
+            cmd.append(line);
+            if (cmd.toString().endsWith(";")) {
+                String command = cmd.toString().substring(0, cmd.length()-1);
+                cmd = new StringBuffer();
+                //System.err.println("[EXEC] "+command);
+                if (!includeDrop && command.startsWith("drop")) {
+                    continue;
+                }
+                Statement st = ds.getConnection().createStatement();
+                st.execute(command);
+            }
+        }
+    }
+    
+}
