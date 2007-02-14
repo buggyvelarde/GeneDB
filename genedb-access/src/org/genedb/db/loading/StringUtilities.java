036c90bc1e7c7017a19bb1df45a1d4021dc62882
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-02-14 16:05:08 +0000

    Fix formatting
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5385 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/StringUtilities.java
+++ genedb-access/src/org/genedb/db/loading/StringUtilities.java
@@ -7,14 +7,14 @@ public class StringUtilities {
     public static final String EMPTY_STRING = new String();
     
     private StringUtilities() {
-	// Utility class - don't instantiate
+        // Utility class - don't instantiate
     }
 
     public static String emptyOrValue(String s) {
-	if (StringUtils.hasLength(s)) {
-	    return s;
-	}
-	return StringUtilities.EMPTY_STRING;
+        if (StringUtils.hasLength(s)) {
+            return s;
+        }
+        return StringUtilities.EMPTY_STRING;
     }
 
 }
