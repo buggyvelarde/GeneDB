2dc42394a9ef7d566b38fe1f47ff68056b4e7e81
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-02-20 15:04:21 +0000

    Add cvterm for mitochondrialTranslationTable
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5434 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-installer/etc/genedb-initial.sql
+++ genedb-installer/etc/genedb-initial.sql
@@ -443,20 +443,6 @@ insert into cvterm(cv_id, name, definition, dbxref_id, is_obsolete, is_relations
 
 
 insert into dbxref(db_id, accession, description) values (
-		(select db_id from db where name='null'), 
-		'genedb_synonym_type:mitochondrialTranslationTable',
-		'dbxref for cvterm mitochondrialTranslationTable'
-);
-insert into cvterm(cv_id, name, definition, dbxref_id, is_obsolete, is_relationshiptype) values (
-		(select cv_id from cv where name='genedb_misc_...'), 
-		'mitochondrialTranslationTable',
-		'mitochondrialTranslationTable',
-		(select dbxref_id from dbxref where accession='genedb_synonym_type:temporary_systematic_id'),
-		0, 0
-);
-
-
-insert into dbxref(db_id, accession, description) values (
 		(select db_id from db where name='genedb_internal'), 
 		'genedb_internal:taxonList',
 		'dbxref for cvterm taxonList'
@@ -522,6 +508,20 @@ insert into cvterm(cv_id, name, definition, dbxref_id, is_obsolete, is_relations
 		'curatorEmail',
 		'curatorEmail',
 		(select dbxref_id from dbxref where accession='genedb_internal:curatorEmail'),
+		0, 0
+);
+
+
+insert into dbxref(db_id, accession, description) values (
+		(select db_id from db where name='genedb_internal'), 
+		'genedb_internal:mitochondrialTranslationTable',
+		'dbxref for cvterm mitochondrialTranslationTable'
+);
+insert into cvterm(cv_id, name, definition, dbxref_id, is_obsolete, is_relationshiptype) values (
+		(select cv_id from cv where name='genedb_misc'), 
+		'mitochondrialTranslationTable',
+		'mitochondrialTranslationTable',
+		(select dbxref_id from dbxref where accession='genedb_internal:mitochondrialTranslationTable'),
 		0, 0
 );
 -- Load phylogeny relationships
