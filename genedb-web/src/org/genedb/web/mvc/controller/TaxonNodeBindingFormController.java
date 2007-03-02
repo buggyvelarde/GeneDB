20fbf24a77c3cc9ed2bae8c83f09ba3781641dfd
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-03-02 14:29:47 +0000

    Add form controller with binding for TaxonNode[] already set up
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5576 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/web/mvc/controller/TaxonNodeBindingFormController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2006-2007 Genome Research Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by  the Free Software Foundation; either version 2 of the License or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307 USA
+ */
+
+package org.genedb.web.mvc.controller;
+
+import org.genedb.db.loading.TaxonNode;
+import org.genedb.db.loading.TaxonNodeArrayPropertyEditor;
+
+import org.springframework.web.bind.ServletRequestDataBinder;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+
+/**
+ * A SimpleFormController which already binds a TaxonNode[] property editor and
+ * allows submission by GET as well as POST
+ * 
+ * @author Adrian Tivey (art)
+ */
+public class TaxonNodeBindingFormController extends PostOrGetFormController {
+
+    private TaxonNodeArrayPropertyEditor taxonNodeArrayPropertyEditor;
+
+    @Override
+    protected void initBinder(HttpServletRequest request, ServletRequestDataBinder srdb) throws Exception {
+        super.initBinder(request, srdb);
+        srdb.registerCustomEditor(TaxonNode[].class, taxonNodeArrayPropertyEditor);
+    }
+    
+    public void setTaxonNodeArrayPropertyEditor(
+            TaxonNodeArrayPropertyEditor taxonNodeArrayPropertyEditor) {
+        this.taxonNodeArrayPropertyEditor = taxonNodeArrayPropertyEditor;
+    }
+
+}
