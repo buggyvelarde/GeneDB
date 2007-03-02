affc93afcfc524e0f0b0d0bd2a5a5c317f18afae
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-03-02 12:24:11 +0000

    Move complex querying code out of Search into it's own class, and fix warnings
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5570 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/SearchController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/SearchController.java
@@ -3,12 +3,6 @@ package org.genedb.web.mvc.controller;
 import org.genedb.db.dao.CvDao;
 import org.genedb.db.dao.SequenceDao;
 import org.genedb.db.loading.FeatureUtils;
-import org.genedb.query.BasicQueryI;
-import org.genedb.query.NumberedQueryI;
-import org.genedb.query.QueryPlaceHolder;
-import org.genedb.query.bool.BooleanOp;
-import org.genedb.query.bool.BooleanQuery;
-import org.genedb.web.tags.bool.QueryTreeWalker;
 
 import org.gmod.schema.cv.Cv;
 import org.gmod.schema.cv.CvTerm;
@@ -104,7 +98,8 @@ public class SearchController extends MultiActionController implements Initializ
 	}
 
 	
-	public ModelAndView FeatureById(HttpServletRequest request, HttpServletResponse response) {
+	@SuppressWarnings("unchecked")
+    public ModelAndView FeatureById(HttpServletRequest request, HttpServletResponse response) {
 	    int id = ServletRequestUtils.getIntParameter(request, "id", -1);
 	    if (id == -1) {
 		    
@@ -113,7 +108,7 @@ public class SearchController extends MultiActionController implements Initializ
 	    Map model = new HashMap(3);
 	    model.put("feature", feat);		
 	    String viewName = "features/gene";
-	    String type = feat.getCvTerm().getName();
+	    //String type = feat.getCvTerm().getName();
 	    // TODO
 	    // Check if features/type is known about
 	    // otherwise go to features/generic
@@ -123,7 +118,8 @@ public class SearchController extends MultiActionController implements Initializ
 	
 	private static final String NO_VALUE_SUPPLIED = "_NO_VALUE_SUPPLIED";
 	
-	public ModelAndView DummyGeneFeature(HttpServletRequest request, HttpServletResponse response) {
+	@SuppressWarnings("unchecked")
+    public ModelAndView DummyGeneFeature(HttpServletRequest request, HttpServletResponse response) {
 	    Feature feat = new Feature();
 	    feat.setName("dummy_name");
 	    feat.setUniqueName("dummy_id");
@@ -136,7 +132,8 @@ public class SearchController extends MultiActionController implements Initializ
 	    return new ModelAndView(viewName, model);
 	}
 	
-	public ModelAndView FeatureByName(HttpServletRequest request, HttpServletResponse response) {
+	@SuppressWarnings("unchecked")
+    public ModelAndView FeatureByName(HttpServletRequest request, HttpServletResponse response) {
 	    String name = ServletRequestUtils.getStringParameter(request, "name", NO_VALUE_SUPPLIED);
 	    if (name.equals(NO_VALUE_SUPPLIED)) {
 		    
@@ -216,7 +213,7 @@ public class SearchController extends MultiActionController implements Initializ
 	
 	public ModelAndView GeneOntology(HttpServletRequest request, HttpServletResponse response){
 		
-		String go = ServletRequestUtils.getStringParameter(request,"go", NO_VALUE_SUPPLIED);
+		//String go = ServletRequestUtils.getStringParameter(request,"go", NO_VALUE_SUPPLIED);
 		String viewName = "features/GO";
 		Map model = new HashMap(2);
 		
@@ -229,7 +226,8 @@ public class SearchController extends MultiActionController implements Initializ
 	    this.cvDao = cvDao;
 	}
 
-	public ModelAndView FindCvByName(HttpServletRequest request, HttpServletResponse response) {
+	@SuppressWarnings("unchecked")
+    public ModelAndView FindCvByName(HttpServletRequest request, HttpServletResponse response) {
 	    String name = ServletRequestUtils.getStringParameter(request, "name", "%");
 	    List cvs = this.cvDao.getCvByName(name);
 	    Map model = new HashMap();
@@ -269,7 +267,8 @@ public class SearchController extends MultiActionController implements Initializ
 		return new ModelAndView(viewName,model);
 	}
 
-	public ModelAndView CvTermByCvName(HttpServletRequest request, HttpServletResponse response) {
+	@SuppressWarnings("unchecked")
+    public ModelAndView CvTermByCvName(HttpServletRequest request, HttpServletResponse response) {
 	    String cvName = ServletRequestUtils.getStringParameter(request, "cvName", NO_VALUE_SUPPLIED);
 	    String cvTermName = ServletRequestUtils.getStringParameter(request, "cvTermName", "*");
 	    System.err.println("cvName="+cvName+"   :   cvTermName="+cvTermName);
@@ -302,7 +301,8 @@ public class SearchController extends MultiActionController implements Initializ
 //	    return new ModelAndView("db/pub", model);
 //	}
 	
-	public ModelAndView FeatureByCvTermNameAndCvName(HttpServletRequest request, HttpServletResponse response) {
+	@SuppressWarnings("unchecked")
+    public ModelAndView FeatureByCvTermNameAndCvName(HttpServletRequest request, HttpServletResponse response) {
 		String viewName = null;
 		Map model = null;
 		String name = ServletRequestUtils.getStringParameter(request, "name", NO_VALUE_SUPPLIED);
@@ -392,139 +392,7 @@ public class SearchController extends MultiActionController implements Initializ
 		return new ModelAndView(viewName,model);
 		
 	}
-	
-	/**
-	 * Custom handler for examples
-	 * @param request current HTTP request
-	 * @param response current HTTP response
-	 * @return a ModelAndView to render the response
-	 */
-	@SuppressWarnings("unchecked")
-	public ModelAndView BooleanQuery(HttpServletRequest request, HttpServletResponse response) {
-		List<String> answers = new ArrayList<String>();
-		if (!WebUtils.extractTaxonNodesFromRequest(request, answers, true, false)) {
-			return new ModelAndView("chooseTaxonContextView");
-		}
-		
-		QueryForm qf = parseQueryForm(0, request);
-		NumberedQueryI q = qf.getNumberedQueryI();
-		if (q == null) {
-			q = new QueryPlaceHolder();
-			qf.setNumberedQuery(q);
-		}
 
-		boolean submitPressed = false;
-		if ("Run".equals(request.getParameter("runquery"))) {
-		    if (q.isComplete()) {
-			return new ModelAndView("resultListView");
-		    }
-		    // query not complete - fall back to query page
-		} else {
-		
-		    // Check for set expansion...
-		    for (Object o : request.getParameterMap().keySet()) {
-			String key = (String) o;
-			if (key.startsWith("bop.")) {
-			    String[] parts = key.split("\\.");
-			    BooleanOp op = BooleanOp.valueOf(parts[1]);
-			    int q1 = Integer.parseInt(parts[2]);
-			    q = replaceNode(q, q1, op);
-			    QueryTreeWalker qtw = new QueryTreeWalker((NumberedQueryI)q, 0);
-			    qtw.go();
-			    break;
-			}
-		    }
-		    // Now let's see if an expansion was requested
-		}
-			
-		Map model = new HashMap();
-		model.put(WebConstants.QUERY_FORM, qf);
-		model.put(WebConstants.TAX_ID, answers.get(0));
-		return new ModelAndView("queryWorking", WebConstants.MODEL_MAP, model );
-	}
-
-	
-	private NumberedQueryI replaceNode(NumberedQueryI q, int q1, BooleanOp op) {	
-		if (q1 == 0) {
-			return new BooleanQuery(op, q, new QueryPlaceHolder());
-		}
-		recurseTree(q, q1, op);
-		return q;
-	}
-		
-		
-	private boolean recurseTree(BasicQueryI q, int q1, BooleanOp op) {
-		// Looking for parent...
-		if (!(q instanceof BooleanQuery)) {
-			return false;
-		}
-		BooleanQuery bool = (BooleanQuery) q;
-		
-		if (bool.getFirstQuery().getIndex()==q1) {
-			BasicQueryI node = bool.getFirstQuery();
-			BooleanQuery newNode = new BooleanQuery(op, node, new QueryPlaceHolder());
-			bool.setFirstQuery(newNode);
-			return true;
-		}
-        
-		if (bool.getSecondQuery().getIndex()==q1) {
-		        BasicQueryI node = bool.getSecondQuery();
-		        BooleanQuery newNode = new BooleanQuery(op, node, new QueryPlaceHolder());
-		        bool.setSecondQuery(newNode);
-		        return true;
-		}
-		
-		if (recurseTree(bool.getFirstQuery(), q1, op)) {
-				return true;
-		}
-		return recurseTree(bool.getSecondQuery(), q1, op);
-		
-	}
-
-
-	/**
-	 * @param index
-	 * @param request
-	 * @return
-     */
-	private QueryForm parseQueryForm(int index, HttpServletRequest request) {
-		QueryForm qf = new QueryForm();
-		qf.setNumberedQuery(parseQuery(index, request));
-		return qf;
-		
-	}
-	
-	/**
-	 * @param index
-	 * @param request
-	 * @return
-	 */
-	private NumberedQueryI parseQuery(int index, HttpServletRequest request) {
-		String value = request.getParameter("node."+index);
-		if (value == null) {
-			return null;
-		}
-		if (value.startsWith("bool.")) {
-			String[] parts = value.split("\\.");
-			BooleanOp op = BooleanOp.valueOf(parts[1]);
-			int q1 = Integer.parseInt(parts[2]);
-			BasicQueryI query1 = parseQuery(q1, request);
-			int q2 = Integer.parseInt(parts[3]);
-			BasicQueryI query2 = parseQuery(q2, request);
-			NumberedQueryI q = new BooleanQuery(op, query1, query2);
-			q.setIndex(index);
-			return q;
-		}
-		//String name = request.getParameter("question."+index);
-		QueryPlaceHolder qph = new QueryPlaceHolder();
-		if (!"none".equals(value)) {
-			qph.setName(value);
-		}
-		qph.setIndex(index);
-		
-		return qph;
-		
-	}
 
 	public void setSequenceDao(SequenceDao sequenceDao) {
 	    this.sequenceDao = sequenceDao;
