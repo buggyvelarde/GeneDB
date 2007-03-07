41cc4baca00b676caee779e05069f47d3ba2ddb9
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-03-07 12:16:13 +0000

    Add new utility method for converting TaxonNodes into space seperated String
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5629 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/TaxonUtils.java
+++ genedb-web/src/org/genedb/web/mvc/controller/TaxonUtils.java
@@ -1,8 +1,16 @@
 package org.genedb.web.mvc.controller;
 
+import org.genedb.db.loading.TaxonNode;
+
+import org.springframework.util.StringUtils;
+
 import java.util.List;
 
 public class TaxonUtils {
+    
+    public static String getTaxonListFromNodes(TaxonNode[] nodes) {
+        return StringUtils.arrayToDelimitedString(nodes, " ");
+    }
 
 	public static Taxon getTaxonFromList(List<String> answers, int i) {
 		// TODO Auto-generated method stub
