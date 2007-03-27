4e08ee53b4a3a983066f9495385ca6cd1deee357
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-03-27 17:20:52 +0000

    Just created - not yet working. Will get different forms of organism name when a tn isn't available
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5802 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/web/tags/db/OrganismNameTag.java
@@ -0,0 +1,57 @@
+package org.genedb.web.tags.db;
+
+import org.gmod.schema.utils.propinterface.PropertyI;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class OrganismNameTag extends SimpleTagSupport {
+	
+	private Collection<PropertyI> collection;
+	private String name;
+    private String var;
+
+
+	public void setVar(String var) {
+        this.var = var;
+    }
+
+    public void setCollection(Collection<PropertyI> collection) {
+        this.collection = collection;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+	public void doTag() throws JspException, IOException {
+    	List filtered = new ArrayList();
+        if (collection != null) {
+        	//String type = collection.getClass().getName();
+            for (PropertyI prop : collection) {
+            	if (name.equals(prop.getCvTerm().getName())) {
+            		filtered.add(prop);
+            	}
+            }
+        } else {
+            System.err.println("Collection is null");
+        }
+		JspWriter out = getJspContext().getOut();
+        getJspContext().setAttribute(var, filtered);
+        getJspBody().invoke(out);
+        getJspContext().removeAttribute(var);
+	}
+	
+//		PageContext pc = (PageContext) getJspContext();
+//		HttpServletRequest req = (HttpServletRequest) pc.getRequest();
+//		String contextPath = req.getContextPath();
+
+}
