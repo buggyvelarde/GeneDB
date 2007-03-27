d2808f18c38e86ef55db1742e57b28f3a7dfd69d
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-03-27 17:21:49 +0000

    New routine to take a TaxonNode[] and generate a list of terms that can be used in an SQL or HQL in statement
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5803 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/TaxonUtils.java
+++ genedb-web/src/org/genedb/web/mvc/controller/TaxonUtils.java
@@ -4,7 +4,9 @@ import org.genedb.db.loading.TaxonNode;
 
 import org.springframework.util.StringUtils;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TaxonUtils {
     
@@ -12,6 +14,26 @@ public class TaxonUtils {
         return StringUtils.arrayToDelimitedString(nodes, " ");
     }
 
+    public static String getOrgNamesInHqlFormat(TaxonNode[] nodes) {
+    	Set<String> orgNames = new HashSet<String>();
+    	for (TaxonNode node : nodes) {
+			orgNames.addAll(node.getAllChildrenNames());
+		}
+    	StringBuilder ret = new StringBuilder();
+    	boolean notFirst = false;
+    	for (String orgName : orgNames) {
+    		if (notFirst) {
+    			ret.append(", ");
+    		} else {
+    			notFirst = true;
+    		}
+    		ret.append('\'');
+    		ret.append(orgName);
+    		ret.append('\'');
+    	}
+    	return ret.toString();
+    }
+    
 	public static Taxon getTaxonFromList(List<String> answers, int i) {
 		// TODO Auto-generated method stub
 		return new Taxon();
