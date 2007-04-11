06958cc150dc749e54e0712d85b61e9a656b5bd7
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-04-11 14:19:19 +0000

    code for phylogeny menu, so that it can be used as tag
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5885 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/tags/db/DisplayPhylogeny.java
+++ genedb-web/src/org/genedb/web/tags/db/DisplayPhylogeny.java
@@ -1,66 +1,66 @@
 package org.genedb.web.tags.db;
 
 import java.io.IOException;
+
 import java.util.List;
-
-import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.tagext.SimpleTagSupport;
+import org.genedb.db.loading.TaxonNode;
+import org.genedb.web.menu.CompositeMenu;
+import org.genedb.web.menu.Menu;
+import org.genedb.web.menu.SimpleMenu;
 
-import org.genedb.db.dao.OrganismDao;
-import org.genedb.db.dao.PhylogenyDao;
-import org.gmod.schema.phylogeny.Phylonode;
 
-public class DisplayPhylogeny extends SimpleTagSupport{
+public class DisplayPhylogeny extends AbstractHomepageTag{
 	
-	private PhylogenyDao phylogenyDao; 
-	private OrganismDao organismDao; 
-
-	@Override
-    public void doTag() throws JspException, IOException {
-		phylogenyDao = new PhylogenyDao();
-		organismDao = new OrganismDao();
-		
-		List<Phylonode> topNodes = this.phylogenyDao.getPhylonodeByDepthAndParent(2, null);
-		JspWriter out = getJspContext().getOut();
-		
-		for (Phylonode topNode : topNodes) {
-			drawNode(topNode,null,out);
-			getNodes(2,topNode,out);
-		}
-		
-		out.close();
-	}
+	int top = 200;
+	int left = 154;
 	
-	private void getNodes(double depth,Phylonode parent,JspWriter out) {
-		List<Phylonode> topNodes = this.phylogenyDao.getPhylonodeByDepthAndParent(++depth, parent);
-		for (Phylonode topNode : topNodes) {
-				if (this.organismDao.getOrganismByCommonName(topNode.getLabel()) != null) {
-					drawNode(topNode,parent,out);
-				} else {
-					drawNode(topNode,parent,out);
-					getNodes(depth,topNode,out);
-				}
-		}
-		
+	public void setLeft(int left) {
+		this.left = left;
 	}
 
-	private void drawNode(Phylonode node, Phylonode parent,JspWriter out) {
-		double depth = node.getDistance();
-		for (double i = 1; i<depth; i++) {
-			try {
-				out.print("\t");
-			} catch (IOException e) {
-				e.printStackTrace();
+	public void setTop(int top) {
+		this.top = top;
+	}
+
+	private void buildMenu(TaxonNode node, CompositeMenu comSrc) {
+		List<TaxonNode> childs = node.getChildren();
+		for (TaxonNode child : childs) {
+			if(child.isLeaf()) {
+				SimpleMenu sm = new SimpleMenu(Integer.toString(Menu.counter++),child.getLabel(),null);
+				comSrc.add(sm);
+			} else {
+				CompositeMenu parentMenu = new CompositeMenu(Integer.toString(Menu.counter++),child.getLabel(),null,false);
+				comSrc.add(parentMenu);
+				buildMenu(child,parentMenu);
 			}
 		}
-		try {
-			out.println(node.getLabel());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
-
+	
+	@Override
+	protected void display(TaxonNode node, JspWriter out, int indent) throws IOException {
+		Menu.left = this.left;
+		Menu.top = this.top;
+		List<TaxonNode> nodes = node.getChildren();
+		StringBuffer sb = new StringBuffer();
+		StringBuffer top = new StringBuffer();
+        int j=1;
+        for (TaxonNode tnode : nodes) {
+        	CompositeMenu menu = new CompositeMenu(Integer.toString(Menu.counter++),tnode.getLabel(),null,true);
+			top.append(Menu.counter-1);
+			top.append(",");
+			menu.setLevelCoord(Integer.toString(j));
+			j++;
+			buildMenu(tnode,menu);
+			sb.append(menu.render(j-1));
+		}
+		top.deleteCharAt(top.length()-1);
+		//System.out.println(sb.toString());
+		sb.append("<input type=\"hidden\" id=\"itemsLength\" value=\"" + Menu.counter + "\"/>");
+		sb.append("<input type=\"hidden\" id=\"topItems\" value=\"" + top.toString() + "\"/>");
+		Menu.counter = 0;
+		out.print(sb.toString());
+	}
 }
 
 
