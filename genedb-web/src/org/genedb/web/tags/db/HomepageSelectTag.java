0818af7fe01e44dd2d4fdcded9c55e51079bf16e
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-04-11 17:16:33 +0000

    Copied from SimpleSelect
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5890 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/tags/db/HomepageSelectTag.java
+++ genedb-web/src/org/genedb/web/tags/db/HomepageSelectTag.java
@@ -1,20 +1,38 @@
 package org.genedb.web.tags.db;
 
-import org.genedb.db.loading.TaxonNameType;
 import org.genedb.db.loading.TaxonNode;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.jsp.JspWriter;
 
 public class HomepageSelectTag extends AbstractHomepageTag {
     
     @Override
-    protected void display(TaxonNode node, JspWriter out, int indent) throws IOException {
-        out.write(""+indent+" ");
-        out.write(node.getName(TaxonNameType.FULL));
+    protected void display(TaxonNode root, JspWriter out, int indent) throws IOException {
+        List<TaxonNode> nodes = new ArrayList<TaxonNode>();
+        getAllChildren(root, nodes);
+        for (TaxonNode child : root.getChildren()) {
+            nodes.add(child);
+        }
+        
+        out.write("<select name=\"organism\">");
+        for (TaxonNode node : nodes) {
+            out.write("<option value=\"");
+            out.write(node.getLabel());
+            out.write("\">");
+            out.write(node.getLabel());
+            out.write("</option>");
+        }
+        out.write("</select>");
+    }
+    
+    private void getAllChildren(TaxonNode node, List<TaxonNode> list) {
+        list.add(node);
         for (TaxonNode child : node.getChildren()) {
-            display(child, out, indent+1);
+            getAllChildren(child, list);
         }
     }
 
