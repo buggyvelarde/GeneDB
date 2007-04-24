a58b538aff8d075fef5c0682068768ec3f4058da
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-04-24 13:12:07 +0000

    new function to get taxon nodes from string
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@5944 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/TaxonUtils.java
+++ genedb-web/src/org/genedb/web/mvc/controller/TaxonUtils.java
@@ -1,15 +1,18 @@
 package org.genedb.web.mvc.controller;
 
 import org.genedb.db.loading.TaxonNode;
-
+import org.genedb.db.loading.TaxonNodeManager;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class TaxonUtils {
     
+	private static TaxonNodeManager tnm;
+	
     public static String getTaxonListFromNodes(TaxonNode[] nodes) {
         return StringUtils.arrayToDelimitedString(nodes, " ");
     }
@@ -34,9 +37,39 @@ public class TaxonUtils {
     	return ret.toString();
     }
     
+    public static String getOrgNamesInHqlFormat(String org) {
+    	String sections[] = org.split(",");
+    	Set<String> orgNames = new HashSet<String>();
+    	for (String orgLabel : sections) {
+    		TaxonNode node = tnm.getTaxonNodeForLabel(orgLabel);
+    		if(node.getAllChildrenNames().size() > 0) {
+    			orgNames.addAll(node.getAllChildrenNames());
+    		} else {
+    			orgNames.add(node.getLabel());
+    		}
+		}
+    	StringBuilder ret = new StringBuilder();
+    	boolean notFirst = false;
+    	for (String orgName : orgNames) {
+    		if (notFirst) {
+    			ret.append(", ");
+    		} else {
+    			notFirst = true;
+    		}
+    		ret.append('\'');
+    		ret.append(orgName);
+    		ret.append('\'');
+    	}
+    	return ret.toString();
+    }
+    
 	public static Taxon getTaxonFromList(List<String> answers, int i) {
 		// TODO Auto-generated method stub
 		return new Taxon();
+	}
+
+	public void setTnm(TaxonNodeManager tnm) {
+		this.tnm = tnm;
 	}
 
 }
