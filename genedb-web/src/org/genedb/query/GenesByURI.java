b179c70cf55897065b7812f5e16930646532d296
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-06-20 11:52:09 +0000

    Class generation work for query
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@6127 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/query/GenesByURI.java
@@ -0,0 +1,62 @@
+package org.genedb.query;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URI;
+import java.net.URLConnection;
+import java.util.List;
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
+public class GenesByURI implements Query {
+
+	private static final String NAME = "GenesByURI";
+    private URI uri;
+	private List<String> results = null;
+	
+	public Object getResults() throws QueryException {
+		if (results != null) {
+			return results;
+		}
+		// Fetch from URI
+        BufferedReader br = null;
+        try {
+            URLConnection connection = uri.toURL().openConnection();
+            InputStream is = connection.getInputStream();
+            br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = br.readLine()) != null) {
+                results.add(line);
+            }
+        } catch (IOException exp) {
+            throw new QueryException(exp);
+        } finally {
+            IOUtils.closeQuietly(br);
+        }
+            return null;
+	}
+
+
+	public URI getUri() {
+		return uri;
+	}
+
+	@Required
+	public void setUri(URI uri) {
+		this.uri = uri.normalize();
+    }
+
+
+    public String getParseableDescription() {
+        // TODO Auto-generated method stub
+        return NAME + "{uri=\""+uri.toASCIIString()+"\"}";
+    }
+
+}
