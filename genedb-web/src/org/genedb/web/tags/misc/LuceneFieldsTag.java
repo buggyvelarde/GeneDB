3d68edd0f00f8d5d5ac1db4a499fd9471e0d8321
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-09-24 14:57:53 +0000

    Class to add an html select tag that will list the fields from the lucene generated index
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@6419 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/web/tags/misc/LuceneFieldsTag.java
@@ -0,0 +1,28 @@
+package org.genedb.web.tags.misc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import org.apache.lucene.index.IndexReader;
+
+public class LuceneFieldsTag extends SimpleTagSupport {
+	
+	@Override
+    public void doTag() throws JspException, IOException {
+		IndexReader ir = IndexReader.open("/Users/cp2/external/lucene/index/gff/");
+		Collection<String> c = ir.getFieldNames(IndexReader.FieldOption.INDEXED);
+		JspWriter out = getJspContext().getOut();
+		out.write("<select name=\"field\">");
+		out.write("<option value=\"ALL\">ALL</option>");
+		for (String object : c) {
+			out.write("<option value=\"" + object + "\">" + object + "</option>");
+		}
+		out.write("</select>");
+	}
+}
