793a02946d97feb9d4ce393fb77215657c74caa2
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-09-30 12:50:41 +0000

    class that loads data from the UPenn file
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@6453 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-access/src/org/genedb/db/loading/Cluster2GeneDB.java
@@ -0,0 +1,183 @@
+package org.genedb.db.loading;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.genedb.db.dao.GeneralDao;
+import org.genedb.db.dao.SequenceDao;
+import org.gmod.schema.general.Db;
+import org.gmod.schema.general.DbXRef;
+import org.gmod.schema.sequence.Feature;
+import org.gmod.schema.sequence.FeatureDbXRef;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+
+public class Cluster2GeneDB {
+	
+	protected static final Log logger = LogFactory.getLog(Cluster2GeneDB.class);
+	
+    private SequenceDao sequenceDao;
+
+    private GeneralDao generalDao;
+    
+    private HibernateTransactionManager hibernateTransactionManager;
+    
+    private SessionFactory sessionFactory;
+    
+    private Session session;
+    
+    protected Db GENEDB_MISC;
+    
+    public static void main (String[] args) throws FileNotFoundException {
+        
+        String[] filePaths = args;
+
+        if (filePaths.length == 0) {
+        	System.err.println("No input files specified");
+        	System.exit(-1);
+        }
+        
+        Properties overrideProps = new Properties();
+        overrideProps.setProperty("dataSource.username", "chado");
+     
+        PropertyOverrideHolder.setProperties("dataSourceMunging", overrideProps);
+
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                new String[] {"NewRunner.xml"});
+
+        Cluster2GeneDB application = (Cluster2GeneDB) ctx.getBean("cluster2genedb", Cluster2GeneDB.class);
+        application.afterPropertiesSet();
+        File[] inputs = new File[filePaths.length];
+        long start = new Date().getTime();
+        for (int i = 0; i < filePaths.length; i++) {
+        	inputs[i] = new File(filePaths[i]);
+		}
+		application.process(inputs);
+		long duration = (new Date().getTime()-start)/1000;
+		logger.info("Processing completed: "+duration / 60 +" min "+duration  % 60+ " sec.");
+    }
+    
+    public void afterPropertiesSet() {
+		session = sessionFactory.openSession();
+		GENEDB_MISC = generalDao.getDbByName("genedb_misc");
+    }
+
+
+	public void process(final File[] files) {
+		for (File file : files) {
+			System.err.println("Processing '"+file.getName()+"'");
+    		Map<String,String> map = null;
+			Reader r = null;
+			try {
+				r = new FileReader(file);
+				map = parseFile(r);
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+			writeToDb(map);
+		}
+	}
+    
+	private void writeToDb(Map<String, String> map) {
+
+		Iterator it = map.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pairs = (Map.Entry)it.next();
+	        String key = (String)pairs.getKey();
+	        String value = (String)pairs.getValue();
+	        
+	        if(value != "") {
+		        Feature polypeptide = getPolypeptide(key);
+		        
+		        DbXRef dbXRef = generalDao.getDbXRefByDbAndAcc(GENEDB_MISC,value);
+		        if(dbXRef == null) {
+		        	dbXRef = new DbXRef(GENEDB_MISC,key);
+		        	generalDao.persist(dbXRef);
+		        }
+		        
+		        FeatureDbXRef fDbXRef = new FeatureDbXRef(dbXRef,polypeptide,true);
+		        sequenceDao.persist(fDbXRef);
+	        }
+	    }
+		
+	}
+
+	private Feature getPolypeptide(String geneName) {
+    	geneName = geneName.concat(":pep");
+    	Feature polypeptide = sequenceDao.getFeatureByUniqueName(geneName, "polypeptide");
+    	if(polypeptide == null) {
+    		return null;
+    	}
+    	logger.warn("polypeptide is " + polypeptide + "gene name is " + geneName);
+    	int id = polypeptide.getFeatureId();
+    	polypeptide = (Feature)session.load(Feature.class,new Integer(id));
+    	return polypeptide;
+	}
+	
+	private Map<String,String> parseFile(Reader r) {
+		BufferedReader input = new BufferedReader(r);
+		String line = null;
+		Map<String,String> map = new HashMap<String,String>();
+		try {
+			while((line = input.readLine()) != null) {
+
+	    		String terms[] = line.split("\t");
+	    		map.put(terms[0], terms[1]);
+			}
+		} catch (IOException e) {
+				e.printStackTrace();
+		}
+		return map;
+	}
+
+	public GeneralDao getGeneralDao() {
+		return generalDao;
+	}
+
+	public void setGeneralDao(GeneralDao generalDao) {
+		this.generalDao = generalDao;
+	}
+
+	public HibernateTransactionManager getHibernateTransactionManager() {
+		return hibernateTransactionManager;
+	}
+
+	public void setHibernateTransactionManager(
+			HibernateTransactionManager hibernateTransactionManager) {
+		this.hibernateTransactionManager = hibernateTransactionManager;
+	}
+
+	public SequenceDao getSequenceDao() {
+		return sequenceDao;
+	}
+
+	public void setSequenceDao(SequenceDao sequenceDao) {
+		this.sequenceDao = sequenceDao;
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+    
+    
+}
