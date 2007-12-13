7a254d5f1c76c202bf28cacb8049c618d868f048
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-12-13 18:12:21 +0000

    Expand this stub
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@6762 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-domain/src/org/genedb/db/domain/misc/MethodResult.java
+++ genedb-domain/src/org/genedb/db/domain/misc/MethodResult.java
@@ -1,9 +1,29 @@
 package org.genedb.db.domain.misc;
 
-public class MethodResult {
+import java.io.Serializable;
+
+public class MethodResult implements Serializable {
 	
-	private boolean success;
-	private String msg;
+	public static final MethodResult SUCCESS = new MethodResult();
 	
+	private boolean successful;
+	private String errorMsg;
+	
+	public MethodResult() {
+		successful = true;
+	}
+	
+	public MethodResult(String errorMsg) {
+		successful = false;
+		this.errorMsg = errorMsg;
+	}
+	
+	public boolean isSuccessful() {
+		return successful;
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
 
 }
