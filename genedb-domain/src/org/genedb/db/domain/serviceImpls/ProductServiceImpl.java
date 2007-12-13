a59c58408328be1731a95823c64959c3b389d1f7
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-12-13 18:12:45 +0000

    Extend test data
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@6763 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-domain/src/org/genedb/db/domain/serviceImpls/ProductServiceImpl.java
+++ genedb-domain/src/org/genedb/db/domain/serviceImpls/ProductServiceImpl.java
@@ -11,22 +11,43 @@ import org.genedb.db.domain.services.ProductService;
 
 public class ProductServiceImpl implements ProductService {
 
-	@Override
-	public MethodResult rationaliseProduct(Product newProduct,
-			List<Product> products) {
-		
-		
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Product> getProductList() {
-		List<Product> products = new ArrayList<Product>();
+	private List<Product> products = new ArrayList<Product>();
+	
+	public ProductServiceImpl() {
 		products.add(new Product("peri", 1));
 		products.add(new Product("rose", 2));
 		products.add(new Product("donna", 3));
 		products.add(new Product("martha", 4));
+		products.add(new Product("susan", 5));
+		products.add(new Product("sarah-jane", 6));
+		products.add(new Product("leela", 7));
+		products.add(new Product("romanadvertrelundar", 8));
+		products.add(new Product("tegan", 9));
+		products.add(new Product("nyssa", 10));
+		products.add(new Product("mel", 11));
+		products.add(new Product("ace", 12));
+		products.add(new Product("jo", 13));
+	}
+
+	
+	@Override
+	public MethodResult rationaliseProduct(Product newProduct,
+			List<Product> oldProducts) {
+		
+		boolean success = true;
+		for (Product product : oldProducts) {
+			success &= products.remove(product);
+		}
+		if (success) {
+			return MethodResult.SUCCESS;
+		}
+		return new MethodResult("Failed");
+	}
+
+	
+	@Override
+	public List<Product> getProductList() {
+		System.err.println("The number of products is '"+products.size()+"'");
 		return products;
 	}
 
