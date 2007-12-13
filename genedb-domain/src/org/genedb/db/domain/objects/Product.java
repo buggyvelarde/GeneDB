5e35f1497b806e410c33d7b0f0ff5fc667d97294
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-12-13 18:13:10 +0000

    Add equals and hashcode methods
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@6764 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-domain/src/org/genedb/db/domain/objects/Product.java
+++ genedb-domain/src/org/genedb/db/domain/objects/Product.java
@@ -16,7 +16,36 @@ public class Product implements Serializable {
 	public String toString() {
 		return description;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int PRIME = 31;
+		int result = 1;
+		result = PRIME * result + ((description == null) ? 0 : description.hashCode());
+		result = PRIME * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		
+		final Product other = (Product) obj;
+		if (id != other.id) {
+			return false;
+		}
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else {
+			if (!description.equals(other.description)) {
+				return false;
+			}
+		}
+		return true;
+	}
 	
 }
