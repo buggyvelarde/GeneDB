c915982fe289773c7e2ff79f08277a2d4858afb9
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-12-17 17:59:32 +0000

    Start generating off db
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@6790 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-domain/src/org/genedb/db/domain/serviceImpls/ProductServiceImpl.java
+++ genedb-domain/src/org/genedb/db/domain/serviceImpls/ProductServiceImpl.java
@@ -3,69 +3,69 @@ package org.genedb.db.domain.serviceImpls;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.genedb.db.dao.CvDao;
 import org.genedb.db.domain.misc.GeneListReservations;
 import org.genedb.db.domain.misc.MethodResult;
 import org.genedb.db.domain.objects.Product;
 import org.genedb.db.domain.services.ProductService;
+import org.gmod.schema.cv.CvTerm;
+import org.gmod.schema.sequence.FeatureCvTerm;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 @Repository
 public class ProductServiceImpl implements ProductService {
 
-	private List<Product> products = new ArrayList<Product>();
 	private GeneListReservations geneListReservations;
 	private SessionFactory sessionFactory;
+	private CvDao cvDao;
 	
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
-	}
-	
-	public ProductServiceImpl() {
-		products.add(new Product("peri", 1));
-		products.add(new Product("rose", 2));
-		products.add(new Product("donna", 3));
-		products.add(new Product("martha", 4));
-		products.add(new Product("susan", 5));
-		products.add(new Product("sarah-jane", 6));
-		products.add(new Product("leela", 7));
-		products.add(new Product("romanadvertrelundar", 8));
-		products.add(new Product("tegan", 9));
-		products.add(new Product("nyssa", 10));
-		products.add(new Product("mel", 11));
-		products.add(new Product("ace", 12));
-		products.add(new Product("jo", 13));
 	}
 
 	
 	@SuppressWarnings("unchecked")
 	@Override
+	@Transactional
 	public MethodResult rationaliseProduct(Product newProduct,
 			List<Product> oldProducts) {
-		
+		Session session = SessionFactoryUtils.getSession(sessionFactory, true);
 		List<String> problems = new ArrayList<String>();
 		checkProduct(newProduct, problems);
 		for (Product p : oldProducts) {
 			checkProduct(p, problems);
 		}
 		if (problems.size() > 0) {
+			session.close();
 			return new MethodResult(StringUtils.collectionToCommaDelimitedString(problems));
 		}
+
+		CvTerm newCvTerm = cvDao.getCvTermById(newProduct.getId());
 		
 		for (Product p : oldProducts) {
-			List<String> genes = sessionFactory.getCurrentSession().createQuery("select f.uniqueName" +
+			List<FeatureCvTerm> fcts = session.createQuery("select fct" +
 			        " from CvTerm cvt,FeatureCvTerm fct, Feature f" +
-					"where f=fct.feature and cvt=fct.cvTerm and cvt.id="+p.getId()).list();
+					" where f=fct.feature and cvt=fct.cvTerm and cvt.id="+p.getId()).list();
 			// FIXME Check for locks
-			for (String geneName : genes) {
-				int count = sessionFactory.getCurrentSession().createQuery(
-						"update FeatureCvTerm fct set fct.cvtTerm.id="+p.getId()+" where fct.feature.uniqueName= :geneName").executeUpdate();
-				if (count != 1) {
-					problems.add("Unable to update product for '"+geneName+"'");
-				}
+			for (FeatureCvTerm fct : fcts) {
+				System.err.println("Found a fct '"+fct+"' for product '"+newCvTerm.getName()+"'");
+				//fct.setCvTerm(newCvTerm);
+//				cvDao.update(fct);
+				//int count = session.createQuery(
+				//		"update FeatureCvTerm fct set fct.cvtTerm.id="+p.getId()+" where fct.feature.uniqueName="+geneName").executeUpdate();
+				//if (count != 1) {
+				//	problems.add("Unable to update product for '"+geneName+"'");
+				//}
 			}
 		}
+		
+		session.close();
 		if (problems.size() > 0) {
 			return new MethodResult(StringUtils.collectionToCommaDelimitedString(problems));
 		}
@@ -81,13 +81,21 @@ public class ProductServiceImpl implements ProductService {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Product> getProductList() {
-		return sessionFactory.getCurrentSession().createQuery("select new Product(cvt.name,cvt.id)" +
-        " from CvTerm cvt,FeatureCvTerm fct" +
-		"where cvt=fct.cvTerm and cvt.cv.name='genedb_products' group by cvt.name").list();
+		Session session = SessionFactoryUtils.getSession(sessionFactory, true);
+		Query q = session.createQuery("select distinct new org.genedb.db.domain.objects.Product(cvt.name, cvt.id)" +
+        " from CvTerm cvt, FeatureCvTerm fct" +
+		" where cvt=fct.cvTerm and cvt.cv.name='genedb_products' order by cvt.name");
+		List<Product> products = (List<Product>) q.list();
+		session.close();
+		return products;
 	}
 
 	public void setGeneListReservations(GeneListReservations geneListReservations) {
 		this.geneListReservations = geneListReservations;
+	}
+
+	public void setCvDao(CvDao cvDao) {
+		this.cvDao = cvDao;
 	}
 
 }
