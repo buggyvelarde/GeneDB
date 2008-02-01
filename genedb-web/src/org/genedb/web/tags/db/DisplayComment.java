35c9c9351ae79617d3f51b650ec5f1e5f3e5275f
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-02-01 18:57:56 +0000

    displays comment on the gene page
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7061 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/web/tags/db/DisplayComment.java
@@ -0,0 +1,51 @@
+package org.genedb.web.tags.db;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import org.gmod.schema.sequence.Feature;
+import org.gmod.schema.sequence.FeatureProp;
+
+public class DisplayComment extends SimpleTagSupport {
+	
+	Feature polypeptide;
+	
+	@Override
+    public void doTag() throws JspException, IOException {
+		if (polypeptide != null) {
+			Collection<FeatureProp> fprops = null;
+			fprops = polypeptide.getFeatureProps();
+			
+			if (fprops != null) {
+				List<String> comments = new ArrayList<String>();
+				
+				for (FeatureProp prop : fprops) {
+					if (prop.getCvTerm().getName() == "comment") {
+						comments.add(prop.getValue());
+					}
+				}
+				JspWriter out = getJspContext().getOut();
+				out.println("<table width=\"100%\" cellpadding=\"2\" cellspacing=\"2\">");
+				out.println("<tr>");
+				out.println("<th>No.</th>");
+				out.println("<th>Comment</th>");
+				out.println("</tr>");
+				
+				int count = 1;
+				for (String comment : comments) {
+					out.println("<tr>");
+					out.println("<td>" + count + "</td>");
+					out.println("<td>" + comment + "</td>");
+					out.println("</tr>");
+					count++;
+				}
+			}
+		}
+	}
+}
