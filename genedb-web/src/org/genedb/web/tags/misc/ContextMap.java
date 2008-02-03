2b33bda77fe3333ae3a6c78d6dfa8e7fba2d8f4f
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-02-03 15:09:27 +0000

    Have webapp name passed in
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7091 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/tags/misc/ContextMap.java
+++ genedb-web/src/org/genedb/web/tags/misc/ContextMap.java
@@ -1,15 +1,19 @@
 package org.genedb.web.tags.misc;
 
+import static javax.servlet.jsp.PageContext.REQUEST_SCOPE;
+
 import java.io.IOException;
 
+import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 import org.genedb.web.gui.ImageInfo;
-import org.genedb.web.mvc.controller.WebUtils;
+import org.genedb.web.mvc.controller.GeneDBWebUtils;
 import org.gmod.schema.sequence.Feature;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.util.WebUtils;
 
 public class ContextMap extends SimpleTagSupport {
 	
@@ -17,9 +21,18 @@ public class ContextMap extends SimpleTagSupport {
 	
 	@Override
     public void doTag() throws JspException, IOException {
-		ImageInfo info = WebUtils.drawContextMap(gene);
+        String prefix = getContextPathFromJspContext(getJspContext());
+		ImageInfo info = GeneDBWebUtils.drawContextMap(gene, prefix);
 	}
-
+	
+	private String getContextPathFromJspContext(JspContext context) {
+		String prefix = (String) context.getAttribute(WebUtils.FORWARD_CONTEXT_PATH_ATTRIBUTE, REQUEST_SCOPE);
+        if (!prefix.equals("/")) {
+        	prefix += "/";
+        }
+		return prefix;
+	}
+	
 	public void setGene(Feature gene) {
 		this.gene = gene;
 	}
