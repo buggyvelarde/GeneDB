80810d623fd2ad436e1734caa9e452483db97808
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-03-07 15:36:46 +0000

    Dummy version always returns true
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7215 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- Jogra/src/org/genedb/jogra/drawing/JograLoginService.java
+++ Jogra/src/org/genedb/jogra/drawing/JograLoginService.java
@@ -7,13 +7,12 @@ public class JograLoginService extends LoginService {
 	@Override
 	public boolean authenticate(String uname, char[] passwd, String server)
 			throws Exception {
-		String password = new String(passwd);
-		System.err.println("Trying to log in as '"+password+"'");
-		// TODO Auto-generated method stub
-		if (password.equals("fred")) {
-			return true;
-		}
-		return false;
+		//String password = new String(passwd);
+		//System.err.println("Trying to log in as '"+password+"'");
+//		if (password.equals("fred")) {
+//			return true;
+//		}
+		return true; // FIXME
 	}
 
 }
