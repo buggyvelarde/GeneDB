11206208598d5a3a9d4d0505ce7cc593769597e2
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-03-10 16:19:52 +0000

    Split DbServer interface out into seperate file
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7218 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-installer/src/org/genedb/install/Server.groovy
+++ genedb-installer/src/org/genedb/install/Server.groovy
@@ -3,8 +3,3 @@ package org.genedb.install;
 interface Server {
     void install(AntBuilder ant, String repository, String target, String port)
 }
-
-interface DbServer extends Server {
-    void loadSchema()
-    void loadBootstrapData()
-}
