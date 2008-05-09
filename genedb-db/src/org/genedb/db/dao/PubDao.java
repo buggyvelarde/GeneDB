d23ccd397a7a50aa5283c24696ee32f37499a8fa
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-09 14:17:11 +0000

    Last bit of cleanup. Shouldn't break anything.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7381 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/genedb/db/dao/PubDao.java
+++ genedb-db/src/org/genedb/db/dao/PubDao.java
@@ -11,69 +11,40 @@ import java.util.List;
 
 public class PubDao extends BaseDao implements PubDaoI {
 
-    /* (non-Javadoc)
-     * @see org.genedb.db.dao.PubDaoI#getPubById(int)
-     */
     public Pub getPubById(int id) {
         return (Pub) getHibernateTemplate().load(Pub.class, id);
     }
 
-    /* (non-Javadoc)
-     * @see org.genedb.db.dao.PubDaoI#getPubsByUniqueName(java.lang.String)
-     */
     @SuppressWarnings("unchecked")
     public Pub getPubByUniqueName(String uniqueName) {
         List<Pub> list = getHibernateTemplate().findByNamedParam(
-                "from Pub pub where pub.uniqueName like :uniqueName", "uniqueName", uniqueName);
+            "from Pub pub where pub.uniqueName like :uniqueName",
+            "uniqueName", uniqueName);
         return firstFromList(list, "uniqueName", uniqueName);
     }
 
-	public Pub getPubByDbXRef(DbXRef dbXRef) {
+    public Pub getPubByDbXRef(DbXRef dbXRef) {
+        @SuppressWarnings("unchecked")
         List<Pub> list = getHibernateTemplate().findByNamedParam(
-                "select p from PubDbXRef pubDbXRef, Pub p where pubDbXRef.pub = p and pubDbXRef.dbXRef = :dbXRef", "dbXRef", dbXRef);
+            "select pub from PubDbXRef where dbXRef = :dbXRef",
+            "dbXRef", dbXRef);
         return firstFromList(list, "dbXRef", dbXRef);
-	}
-
-//    public Pub findOrCreateByPmid(String pmid) {
-//	Dbxref xref = dbXRefDao.findByDbAcc(pmid);
-//	Pub pub = null;
-//	if (xref == null) {
-//	    // No pubmed entry - create everything
-//	    xref = new Dbxref();
-//	    xref.setDb(DB_PUBMED);
-//	    xref.setAccession(dbAcc);
-//	    pub = new Pub();
-//	    pub.setUniquename("Temporary placeholder for "+pmid);
-//	    
-//	    
-//	    return pub;
-//	}
-//	// Pubmed entry - try and find corresponding Pub
-//	List<PubDbxref> pdxrs = pubDbXRefDao.findByDbXref(xref);
-//	if (pdxr != null) {
-//	    for (PubDbxref pdxr : pdxrs) {
-//		
-//	    }
-//	    
-//	    
-//	    PubDbxref pd = new PubDbxref();
-//	}
-//    }
-//	
-//	// TODO Auto-generated method stub
-//	return null;
-//    }
+    }
 
     public List<PubProp> getPubPropByPubAndCvTerm(Pub pub,CvTerm cvTerm){
+        @SuppressWarnings("unchecked")
     	List<PubProp> list = getHibernateTemplate().findByNamedParam(
-    			"from PubProp pp where pp.pub=:pub and pp.cvTerm=:cvTerm",new String[]{"pub","cvTerm"},new Object[]{pub,cvTerm});
+    	    "from PubProp pp where pp.pub=:pub and pp.cvTerm=:cvTerm",
+    	    new String[]{"pub","cvTerm"},
+    	    new Object[]{pub,cvTerm});
     	return list;
     }
 
     
     public List<PubDbXRef> getPubDbXRef() {
-        // TODO Auto-generated method stub
-        return null;
+        @SuppressWarnings("unchecked")
+        List<PubDbXRef> list = getHibernateTemplate().loadAll(PubDbXRef.class);
+        return list;
     }
 
 }
