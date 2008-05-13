d779f2dbb3bb1e048973f6238286a4875ab07b64
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-13 12:23:42 +0000

    Implement TextByteType methods as documentation says they should be
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7396 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/genedb/db/helpers/TextByteType.java
+++ genedb-db/src/org/genedb/db/helpers/TextByteType.java
@@ -5,69 +5,80 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Arrays;
 
 import org.hibernate.HibernateException;
 import org.hibernate.usertype.UserType;
 
-public class TextByteType implements UserType{
 
-	public Object assemble(Serializable arg0, Object arg1) throws HibernateException {
-		return null;
-	}
+/**
+ * Converts text strings into byte arrays. Used for the feature.residues column,
+ * which uses only a limited set of characters (i.e. letters representing nucleotides
+ * or amino acids) to reduce the space requirements.
+ */
+public class TextByteType implements UserType {
+    @SuppressWarnings("unused")
+    public byte[] assemble(Serializable cached, Object owner) throws HibernateException {
+        return deepCopy(cached);
+    }
 
-	public Object deepCopy(Object value) throws HibernateException {
-		if (value == null) return null;
-        return (byte[])value;
-		
-	}
+    public byte[] deepCopy(Object originalOb) throws HibernateException {
+        byte[] original = (byte[]) originalOb;
+        if (original == null)
+            return null;
+        return Arrays.copyOf(original, original.length);
+    }
 
-	public Serializable disassemble(Object arg0) throws HibernateException {
-		return null;
-	}
+    @SuppressWarnings("unused")
+    public byte[] disassemble(Object value) throws HibernateException {
+        return deepCopy(value);
+    }
 
-	public boolean equals(Object x, Object y) throws HibernateException {
-		return (x == y)
-        || (x != null
-            && y != null
-            && (x.equals(y)));
-	}
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return (x == y) || (x != null && y != null && (x.equals(y)));
+    }
 
-	public int hashCode(Object arg0) throws HibernateException {
-		return 0;
-	}
+    @SuppressWarnings("unused")
+    public int hashCode(Object arg0) throws HibernateException {
+        return 0;
+    }
 
-	public boolean isMutable() {
-		return false;
-	}
+    public boolean isMutable() {
+        return false;
+    }
 
-	public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
-		String string = rs.getString(7);
-		if (string == null) {
-			return null;
-		}
-		return string.getBytes();
-	}
+    @SuppressWarnings("unused")
+    public byte[] nullSafeGet(ResultSet rs, String[] names, Object owner)
+            throws HibernateException, SQLException {
+        String string = rs.getString(names[0]);
+        if (string == null) {
+            return null;
+        }
+        return string.getBytes();
+    }
 
-	public void nullSafeSet(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
-		if(value == null) {
-			st.setString(index, "");
-		} else {
-			byte[] b = (byte[]) value;
-			String s = new String(b);
-			st.setString(index, s);
-		}
-	}
+    public void nullSafeSet(PreparedStatement st, Object value, int index)
+            throws HibernateException, SQLException {
+        if (value == null) {
+            st.setString(index, "");
+        } else {
+            byte[] b = (byte[]) value;
+            String s = new String(b);
+            st.setString(index, s);
+        }
+    }
 
-	public Object replace(Object arg0, Object arg1, Object arg2) throws HibernateException {
-		return null;
-	}
+    @SuppressWarnings("unused")
+    public byte[] replace(Object original, Object target, Object owner) throws HibernateException {
+        return deepCopy(original);
+    }
 
-	public Class<Byte> returnedClass() {
-		return byte.class;
-	}
+    public Class<byte[]> returnedClass() {
+        return byte[].class;
+    }
 
-	public int[] sqlTypes() {
-		return new int[] {Types.VARCHAR};
-	}
+    public int[] sqlTypes() {
+        return new int[] { Types.VARCHAR };
+    }
 
 }
