7e982e9895965cd299be7d7367606952f831db68
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-15 10:58:34 +0000

    Clean up whitespace, and simplify toString method
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7436 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/TaxonNode.java
+++ genedb-access/src/org/genedb/db/loading/TaxonNode.java
@@ -18,10 +18,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * @author art
- *
+ * 
  */
 public class TaxonNode {
 
@@ -31,66 +30,49 @@ public class TaxonNode {
     private List<TaxonNode> children = new ArrayList<TaxonNode>();
     private boolean webLinkable = false;
     private boolean organism = false;
-    private Map<String, Map<String, Object>> appDetails = new HashMap<String, Map<String, Object>>(0);
+    private Map<String, Map<String, Object>> appDetails = new HashMap<String, Map<String, Object>>(
+            0);
     private Map<TaxonNameType, String> names = new HashMap<TaxonNameType, String>(7);
-    
 
     public TaxonNode(Phylonode phylonode) {
         this.phylonode = phylonode;
         names.put(TaxonNameType.LABEL, phylonode.getLabel());
-        names.put(TaxonNameType.FULL, phylonode.getLabel()); // will be overriden for orgs later
-        
+        names.put(TaxonNameType.FULL, phylonode.getLabel()); // will be
+                                                                // overriden for
+                                                                // orgs later
+
         Collection<PhylonodeOrganism> pos = phylonode.getPhylonodeOrganisms();
-		//System.err.println("Looking at '"+shortName+"'");
+        // System.err.println("Looking at '"+shortName+"'");
         if (pos != null && pos.size() > 0) {
-        	if (pos.size() > 1) {
-        		System.err.println("We have too many PhylonodeOrganisms");
-        	} else {
-        		Organism org = pos.iterator().next().getOrganism();
-        		organism = true;
-        		//System.err.println("Found organism for '"+shortName+"'");
+            if (pos.size() > 1) {
+                System.err.println("We have too many PhylonodeOrganisms");
+            } else {
+                Organism org = pos.iterator().next().getOrganism();
+                organism = true;
+                // System.err.println("Found organism for '"+shortName+"'");
                 // TODO What organism props do we want?
-                
+
                 this.taxonId = getOrganismProperty(org, "taxonId");
-                //String curatorName = getOrganismProperty(org, "curatorName");
-                //String curatorEmail = getOrganismProperty(org, "curatorEmail");
-                //String nickname = getOrganismProperty(org, "nickname");
-                //String curatorName = getOrganismProperty(org, "curatorName");
+                // String curatorName = getOrganismProperty(org, "curatorName");
+                // String curatorEmail = getOrganismProperty(org,
+                // "curatorEmail");
+                // String nickname = getOrganismProperty(org, "nickname");
+                // String curatorName = getOrganismProperty(org, "curatorName");
                 names.put(TaxonNameType.HTML_SHORT, getOrganismProperty(org, "htmlShortName"));
                 names.put(TaxonNameType.HTML_FULL, getOrganismProperty(org, "htmlFullName"));
-                //int translationTable = Integer.parseInt(getOrganismProperty(org, "translationTable"));
-                //int mitochondrialTranslationTable = Integer.parseInt(getOrganismProperty(org, "mitochondrialTranslationTable"));
+                // int translationTable =
+                // Integer.parseInt(getOrganismProperty(org,
+                // "translationTable"));
+                // int mitochondrialTranslationTable =
+                // Integer.parseInt(getOrganismProperty(org,
+                // "mitochondrialTranslationTable"));
                 String fullName = org.getGenus() + ' ' + org.getSpecies();
                 names.put(TaxonNameType.FULL, fullName);
-              
-        	}
-        }
-        
-        
-    }
-    
-//    public TaxonNode(TaxonNode parent, Phylonode phylonode, Organism organism) {
-//        this.fullName = this.organism.getGenus() + ' ' + this.organism.getSpecies();
-//        if (this.organism != null) {
-//            this.webLinkable = true;
-//        } else {
-//            String webLink = getPhylonodeProperty("");
-//            if ("true".equals(webLink)) {
-//                webLinkable = true;
-//            }
-//        }
-//    }
 
-    
-//    private String getPhylonodeProperty(String key) {
-//        for (PhylonodeProp prop : phylonode.getPhylonodeProps()) {
-//            if (prop.getCvTerm().getName().equals(key)) {
-//                return prop.getValue();
-//            }
-//        }
-//        return null;
-//    }
-    
+            }
+        }
+
+    }
 
     private String getOrganismProperty(Organism org, String key) {
         for (OrganismProp prop : org.getOrganismProps()) {
@@ -101,28 +83,20 @@ public class TaxonNode {
         return null;
     }
 
-
     public boolean isRoot() {
         return (parent == null);
     }
-
 
     public boolean isLeaf() {
         return children.size() == 0;
     }
 
-
     public String getLabel() {
         return getName(TaxonNameType.LABEL);
     }
-    
 
     public String getName(TaxonNameType tnt) {
         return names.get(tnt);
-//        if (name == null) {
-//            throw new RuntimeException("Name '"+tnt+"' not configured for '"+getLabel()+"'");
-//        }
-//        return name;
     }
 
     public String getTaxonId() {
@@ -134,10 +108,10 @@ public class TaxonNode {
     }
 
     private void setParent(TaxonNode parent) {
-		this.parent = parent;
-	}
+        this.parent = parent;
+    }
 
-	void addChild(TaxonNode child) {
+    void addChild(TaxonNode child) {
         this.children.add(child);
         child.setParent(this);
     }
@@ -149,65 +123,51 @@ public class TaxonNode {
     public Phylonode getPhylonode() {
         return this.phylonode;
     }
-    
+
     public boolean isWebLinkable() {
         return webLinkable;
     }
-    
+
     public Map<String, Object> getAppDetails(String key) {
         if (appDetails.containsKey(key)) {
             return Collections.unmodifiableMap(appDetails.get(key));
         }
         return Collections.emptyMap();
     }
-    
-    public List<String> getAllChildrenNames() {
-    	List<TaxonNode> allChildren = getAllChildren();
-    	List<String> names = new ArrayList<String>();
-//    	StringBuilder ret = new StringBuilder();
-    	for (TaxonNode child : allChildren) {
-			if (child.isOrganism()) {
-				names.add(child.getLabel());
-			}
-		}
-    	if (isOrganism()) {
-    		names.add(getLabel());
-    	}
-    	return names;
-    }
-    
-    public boolean isOrganism() {
-    	return organism;
-    }
-    
-    private List<TaxonNode> getAllChildren() {
-    	List<TaxonNode> ret = new ArrayList<TaxonNode>();
-    	
-    	List<TaxonNode> immediateChildren = getChildren();
-    	for (TaxonNode child : immediateChildren) {
-			ret.add(child);
-			ret.addAll(child.getAllChildren());
-		}
-		return ret;
-	}
 
-	/**
-     * @param detailed
-     * @return
-     */
+    public List<String> getAllChildrenNames() {
+        List<TaxonNode> allChildren = getAllChildren();
+        List<String> names = new ArrayList<String>();
+
+        for (TaxonNode child : allChildren) {
+            if (child.isOrganism()) {
+                names.add(child.getLabel());
+            }
+        }
+        if (isOrganism()) {
+            names.add(getLabel());
+        }
+        return names;
+    }
+
+    public boolean isOrganism() {
+        return organism;
+    }
+
+    private List<TaxonNode> getAllChildren() {
+        List<TaxonNode> ret = new ArrayList<TaxonNode>();
+
+        List<TaxonNode> immediateChildren = getChildren();
+        for (TaxonNode child : immediateChildren) {
+            ret.add(child);
+            ret.addAll(child.getAllChildren());
+        }
+        return ret;
+    }
+
     @Override
     public String toString() {
-        StringBuffer ret = new StringBuffer();
-        ret.append("taxon id='");
-        ret.append(getTaxonId());
-        ret.append("' fullName='");
-        ret.append(getName(TaxonNameType.FULL));
-        ret.append("' label='");
-        ret.append(getLabel());
-        ret.append("' organism=");
-        ret.append(organism);
-        ret.append("' ");
-        return ret.toString();
+        return String.format("taxon id='%s' fullName='%s' label='%s' organism='%s' ",
+            getTaxonId(), getName(TaxonNameType.FULL), getLabel(), organism);
     }
-
 }
