63b3fb25372830efc56437b7fcdbaff4c5ebd66b
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-15 16:17:20 +0000

    Add brief class documentation
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7450 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- HibernateSearch/anttasks/org/genedb/anttasks/Password.java
+++ HibernateSearch/anttasks/org/genedb/anttasks/Password.java
@@ -5,6 +5,14 @@ import java.io.Console;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
+/**
+ * A simple ant task that requests a password from the user, on the console.
+ *
+ * If no console is available, it will simply fail. It could easily be extended
+ * to pop up a dialog box in that case, perhaps using code from <code>com.jera.anttasks.Query</code>.
+ * 
+ * @author rh11
+ */
 public class Password extends Task {
     private String name;
     private String prompt = "Password";
