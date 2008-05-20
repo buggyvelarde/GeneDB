f3d93bbe691b3eeec6c923cf8380d3701d49d29e
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-20 09:42:19 +0000

    Disable the clause limit
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7474 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/LuceneDao.java
+++ genedb-web/src/org/genedb/web/mvc/controller/LuceneDao.java
@@ -8,54 +8,66 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 
 public class LuceneDao {
-	
-	private static final Logger log = Logger.getLogger(LuceneDao.class);
-	
-	private static final String PROP_LUCENE_INDEX_DIRECTORY = "lucene.indexDirectory";
-	
-	private static final ResourceBundle projectProperties = ResourceBundle.getBundle("project");
-	private static final String luceneIndexDirectory = projectProperties.getString(PROP_LUCENE_INDEX_DIRECTORY);
-	
-	/**
-	 * Open the named Lucene index from the default location.
-	 * (The default location is specifed by lucene.indexDirectory in project.properties.)
-	 * 
-	 * @param indexName The name of the index
-	 * @return
-	 * @throws IOException
-	 */
-	public IndexReader openIndex(String indexName) throws IOException {
-		String indexDir = String.format("%s/%s", luceneIndexDirectory, indexName);
-		return IndexReader.open(indexDir);
-	}
-	
-	/**
-	 * Perform a Lucene search
-	 * @param ir           A Lucene index, as returned by #openIndex
-	 * @param analyzer     An analyzer, used to parse the query
-	 * @param defaultField The name of the field to use as the default for the query
-	 * @param queryString  The actual query
-	 * @return
-	 */
-	public Hits search(IndexReader ir, Analyzer analyzer, String defaultField, String queryString) {
-		Query query = null;
-		IndexSearcher searcher = new IndexSearcher(ir);
-		log.debug("searcher is -> " + searcher.toString());
-		QueryParser qp = new QueryParser(defaultField, analyzer);
-		
-		try {
-			query = qp.parse(queryString);
-			log.debug("query is -> " + query.toString());
-			return searcher.search(query);
-		} catch (ParseException e) {
-			throw new RuntimeException(String.format("Lucene failed to parse query '%s'", queryString), e);
-		} catch (IOException e) {
-			throw new RuntimeException(String.format("I/O error during Lucene query '%s'", queryString), e);
-		}
-	}
+
+    private static final Logger log = Logger.getLogger(LuceneDao.class);
+
+    private static final String PROP_LUCENE_INDEX_DIRECTORY = "lucene.indexDirectory";
+
+    private static final ResourceBundle projectProperties = ResourceBundle.getBundle("project");
+    private static final String luceneIndexDirectory = projectProperties
+            .getString(PROP_LUCENE_INDEX_DIRECTORY);
+    
+    static {
+        // Effectively disable the clause limit.
+        // (Wildcard and range queries are expanded into many clauses.)
+        BooleanQuery.setMaxClauseCount(Integer.MAX_VALUE);
+    }
+
+    /**
+     * Open the named Lucene index from the default location. (The default
+     * location is specifed by lucene.indexDirectory in project.properties.)
+     * 
+     * @param indexName The name of the index
+     * @return
+     * @throws IOException
+     */
+    public IndexReader openIndex(String indexName) throws IOException {
+        String indexDir = String.format("%s/%s", luceneIndexDirectory, indexName);
+        return IndexReader.open(indexDir);
+    }
+
+    /**
+     * Perform a Lucene search
+     * 
+     * @param ir A Lucene index, as returned by #openIndex
+     * @param analyzer An analyzer, used to parse the query
+     * @param defaultField The name of the field to use as the default for the
+     *                query
+     * @param queryString The actual query
+     * @return
+     */
+    public Hits search(IndexReader ir, Analyzer analyzer, String defaultField, String queryString) {
+        Query query = null;
+        IndexSearcher searcher = new IndexSearcher(ir);
+        log.debug("searcher is -> " + searcher.toString());
+        QueryParser qp = new QueryParser(defaultField, analyzer);
+
+        try {
+            query = qp.parse(queryString);
+            log.debug("query is -> " + query.toString());
+            return searcher.search(query);
+        } catch (ParseException e) {
+            throw new RuntimeException(String.format("Lucene failed to parse query '%s'",
+                queryString), e);
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("I/O error during Lucene query '%s'",
+                queryString), e);
+        }
+    }
 }
