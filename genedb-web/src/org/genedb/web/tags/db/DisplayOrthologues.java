84845ed78e97b7bc8b69e23b805dfb4573358426
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-21 09:12:48 +0000

    Remove unused imports
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7484 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/tags/db/DisplayOrthologues.java
+++ genedb-web/src/org/genedb/web/tags/db/DisplayOrthologues.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 import org.directwebremoting.util.Logger;
