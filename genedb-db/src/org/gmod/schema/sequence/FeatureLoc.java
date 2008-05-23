6495891456f77ed83d8f51fd3d60d4315b39f5d0
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-23 11:13:28 +0000

    Trivial changes (mostly to whitespace)
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7548 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/sequence/FeatureLoc.java
+++ genedb-db/src/org/gmod/schema/sequence/FeatureLoc.java
@@ -1,13 +1,9 @@
 package org.gmod.schema.sequence;
 
-
 import static javax.persistence.GenerationType.SEQUENCE;
-
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -22,101 +18,93 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name="featureloc")
+@Table(name = "featureloc")
 public class FeatureLoc implements Serializable {
 
-    // Fields 
-    
-    @SequenceGenerator(name="generator", sequenceName="featureloc_featureloc_id_seq")
-    @Id @GeneratedValue(strategy=SEQUENCE, generator="generator")
-    @Column(name="featureloc_id", unique=false, nullable=false, insertable=true, updatable=true)
+    // Fields
+
+    @SequenceGenerator(name = "generator", sequenceName = "featureloc_featureloc_id_seq")
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "generator")
+    @Column(name = "featureloc_id", unique = false, nullable = false, insertable = true, updatable = true)
     private int featureLocId;
-     
-    @ManyToOne(cascade={}, fetch=FetchType.LAZY)
-    @JoinColumn(name="srcfeature_id", unique=false, nullable=true, insertable=true, updatable=true)
+
+    @ManyToOne(cascade = {}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "srcfeature_id", unique = false, nullable = true, insertable = true, updatable = true)
     private Feature featureBySrcFeatureId;
-     
-    @ManyToOne(cascade={},fetch=FetchType.LAZY)
-    @JoinColumn(name="feature_id", unique=false, nullable=false, insertable=true, updatable=true)
+
+    @ManyToOne(cascade = {}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "feature_id", unique = false, nullable = false, insertable = true, updatable = true)
     private Feature featureByFeatureId;
-     
-    @Column(name="fmin", unique=false, nullable=true, insertable=true, updatable=true)
+
+    @Column(name = "fmin", unique = false, nullable = true, insertable = true, updatable = true)
     private Integer fmin;
-     
-    @Column(name="is_fmin_partial", unique=false, nullable=false, insertable=true, updatable=true)
+
+    @Column(name = "is_fmin_partial", unique = false, nullable = false, insertable = true, updatable = true)
     private boolean fminPartial;
-     
-    @Column(name="fmax", unique=false, nullable=true, insertable=true, updatable=true)
+
+    @Column(name = "fmax", unique = false, nullable = true, insertable = true, updatable = true)
     private Integer fmax;
-     
-    @Column(name="is_fmax_partial", unique=false, nullable=false, insertable=true, updatable=true)
+
+    @Column(name = "is_fmax_partial", unique = false, nullable = false, insertable = true, updatable = true)
     private boolean fmaxPartial;
-     
-    @Column(name="strand", unique=false, nullable=true, insertable=true, updatable=true)
+
+    @Column(name = "strand", unique = false, nullable = true, insertable = true, updatable = true)
     private Short strand;
-     
-    @Column(name="phase", unique=false, nullable=true, insertable=true, updatable=true)
+
+    @Column(name = "phase", unique = false, nullable = true, insertable = true, updatable = true)
     private Integer phase;
-     
-    @Column(name="residue_info", unique=false, nullable=true, insertable=true, updatable=true)
+
+    @Column(name = "residue_info", unique = false, nullable = true, insertable = true, updatable = true)
     private String residueInfo;
-     
-    @Column(name="locgroup", unique=false, nullable=false, insertable=true, updatable=true)
+
+    @Column(name = "locgroup", unique = false, nullable = false, insertable = true, updatable = true)
     private int locGroup;
-     
-    @Column(name="rank", unique=false, nullable=false, insertable=true, updatable=true)
+
+    @Column(name = "rank", unique = false, nullable = false, insertable = true, updatable = true)
     private int rank;
-     
-    @OneToMany(cascade={}, fetch=FetchType.LAZY, mappedBy="featureLoc")
-     private Collection<FeatureLocPub> featureLocPubs;
+
+    @OneToMany(cascade = {}, fetch = FetchType.LAZY, mappedBy = "featureLoc")
+    private Collection<FeatureLocPub> featureLocPubs;
 
     /**
      * This field does not participate in the Hibernate mapping, and is neither
      * automatically populated nor persisted. It may be explicitly assigned
-     * using setSrcFeatureId(int). It's currently used by Artemis to cache
-     * the equivalent of getFeatureBySrcFeatureId().getFeatureId().
+     * using setSrcFeatureId(int). It's currently used by Artemis to cache the
+     * equivalent of getFeatureBySrcFeatureId().getFeatureId().
      */
-     @Transient
+    @Transient
     private int srcFeatureId;
-    
-     // Constructors
+
+    // Constructors
 
     /** default constructor */
     public FeatureLoc() {
-    	// Deliberately empty default constructor
+        // Deliberately empty default constructor
     }
 
-	/** minimal constructor */
-    @SuppressWarnings("unused")
-	private FeatureLoc(Feature featureBySrcFeatureId, boolean fminPartial, boolean fmaxPartial, int locGroup, int rank) {
-        this.featureBySrcFeatureId = featureBySrcFeatureId;
+    /** large constructor */
+    public FeatureLoc(Feature featureBySrcfeatureId, Feature featureByFeatureId, Integer fmin,
+            boolean fminPartial, Integer fmax, boolean fmaxPartial, Short strand, Integer phase,
+            int locGroup, int rank) {
+        this.featureBySrcFeatureId = featureBySrcfeatureId;
+        this.featureByFeatureId = featureByFeatureId;
+        this.fmin = fmin;
         this.fminPartial = fminPartial;
+        this.fmax = fmax;
         this.fmaxPartial = fmaxPartial;
+        this.strand = strand;
+        this.phase = phase;
         this.locGroup = locGroup;
         this.rank = rank;
     }
-    /** large constructor */
-    public FeatureLoc(Feature featureBySrcfeatureId, Feature featureByFeatureId, Integer fmin, boolean fminPartial, Integer fmax, boolean fmaxPartial, Short strand, Integer phase, int locGroup, int rank) {
-       this.featureBySrcFeatureId = featureBySrcfeatureId;
-       this.featureByFeatureId = featureByFeatureId;
-       this.fmin = fmin;
-       this.fminPartial = fminPartial;
-       this.fmax = fmax;
-       this.fmaxPartial = fmaxPartial;
-       this.strand = strand;
-       this.phase = phase;
-       this.locGroup = locGroup;
-       this.rank = rank;
-    }
-    
-   
-    // Property accessors
 
+    // Property accessors
 
     public Feature getFeatureBySrcFeatureId() {
         return this.featureBySrcFeatureId;
     }
-    
+
     public void setFeatureBySrcFeatureId(Feature featureBySrcFeatureId) {
         this.featureBySrcFeatureId = featureBySrcFeatureId;
     }
@@ -124,79 +112,79 @@ public class FeatureLoc implements Serializable {
     public Feature getFeatureByFeatureId() {
         return this.featureByFeatureId;
     }
-    
+
     public void setFeatureByFeatureId(Feature featureByFeatureId) {
         this.featureByFeatureId = featureByFeatureId;
     }
-    
+
     public Integer getFmin() {
         return this.fmin;
     }
-    
+
     public void setFmin(Integer fmin) {
         this.fmin = fmin;
     }
-    
+
     public boolean isFminPartial() {
         return this.fminPartial;
     }
-    
+
     public void setFminPartial(boolean fminPartial) {
         this.fminPartial = fminPartial;
     }
-    
+
     public Integer getFmax() {
         return this.fmax;
     }
-    
+
     public void setFmax(Integer fmax) {
         this.fmax = fmax;
     }
-    
+
     public boolean isFmaxPartial() {
         return this.fmaxPartial;
     }
-    
+
     public void setFmaxPartial(boolean fmaxPartial) {
         this.fmaxPartial = fmaxPartial;
     }
-    
+
     public Short getStrand() {
         return this.strand;
     }
-    
+
     public void setStrand(Short strand) {
         this.strand = strand;
     }
-    
+
     public Integer getPhase() {
         return this.phase;
     }
-    
+
     public void setPhase(Integer phase) {
         this.phase = phase;
     }
-    
+
     public String getResidueInfo() {
         return this.residueInfo;
     }
-    
+
     public void setResidueInfo(String residueInfo) {
         this.residueInfo = residueInfo;
     }
-    
+
     public int getLocGroup() {
         return this.locGroup;
     }
-    
+
     public void setLocGroup(int locGroup) {
         this.locGroup = locGroup;
     }
-    
+
     public int getRank() {
         return this.rank;
     }
-    
+
     public void setRank(int rank) {
         this.rank = rank;
     }
@@ -204,7 +192,7 @@ public class FeatureLoc implements Serializable {
     public Collection<FeatureLocPub> getFeatureLocPubs() {
         return this.featureLocPubs;
     }
-    
+
     public void setFeaturelocPubs(Collection<FeatureLocPub> featureLocPubs) {
         this.featureLocPubs = featureLocPubs;
     }
@@ -218,8 +206,9 @@ public class FeatureLoc implements Serializable {
     }
 
     /**
-     * Return the (unmapped) srcFeatureId property. This must be explicitly populated
-     * beforehand using setSrcFeatureId(int); it is not automatically populated by Hibernate.
+     * Return the (unmapped) srcFeatureId property. This must be explicitly
+     * populated beforehand using setSrcFeatureId(int); it is not automatically
+     * populated by Hibernate.
      * 
      * Perhaps you really wanted getFeatureBySrcFeatureId().getFeatureId()?
      * 
@@ -228,11 +217,11 @@ public class FeatureLoc implements Serializable {
     public int getSrcFeatureId() {
         return srcFeatureId;
     }
-    
+
     /**
      * Set the value of the (unmapped) srcFeatureId property. This is not
-     * automatically populated by Hibernate, and must be set explicitly
-     * if it is used. Currently used only by Artemis.
+     * automatically populated by Hibernate, and must be set explicitly if it is
+     * used. Currently used only by Artemis.
      * 
      * @param srcFeatureId
      */
@@ -240,5 +229,3 @@ public class FeatureLoc implements Serializable {
         this.srcFeatureId = srcFeatureId;
     }
 }
-
-
