265942c5aa52be72bb9fdb10e189a9bbd9d644cc
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-23 11:13:52 +0000

    Move Gene into org.gmod.schema.sequence.feature package
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7549 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-db/src/org/gmod/schema/sequence/feature/Gene.java
@@ -0,0 +1,58 @@
+package org.gmod.schema.sequence.feature;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Transient;
+
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Store;
+
+@SuppressWarnings("serial")
+@DiscriminatorValue("792")
+@Indexed
+public class Gene extends AbstractGene {
+    
+    public Collection<MRNA> getCodingTranscripts() {
+        Collection<MRNA> ret = new ArrayList<MRNA>();
+        
+        for (Transcript transcript : getTranscripts()) {
+            if (transcript instanceof MRNA)
+                ret.add((MRNA) transcript);
+        }
+        
+        return ret;
+    }
+
+    @Transient @Field(name = "protein", store = Store.YES)
+    public String getProteinUniqueNamesTabSeparated() {
+        StringBuilder ret = new StringBuilder();
+        boolean first = true;
+        for (MRNA transcript: getCodingTranscripts()) {
+            if (first)
+                first = false;
+            else
+                ret.append('\t');
+            ret.append(transcript.getProteinUniqueName());
+        }
+        return ret.toString();
+    }
+
+    @Transient @Field(name = "product", index = Index.TOKENIZED, store = Store.YES)
+    public String getProductsAsTabSeparatedString() {
+        StringBuilder products = new StringBuilder();
+
+        boolean first = true;
+        for (MRNA transcript : getCodingTranscripts()) {
+            if (first)
+                first = false;
+            else
+                products.append('\t');
+            products.append(transcript.getProductsAsTabSeparatedString());
+        }
+        return products.toString();
+    }
+}
