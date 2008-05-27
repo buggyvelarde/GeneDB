e18e75f795ce73106572e1929f2919818b8f8a56
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-27 12:21:03 +0000

    whitespace and warnings
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7569 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/ProductSearchController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/ProductSearchController.java
@@ -19,101 +19,103 @@ import org.apache.lucene.search.Query;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 
-public class ProductSearchController extends TaxonNodeBindingFormController{
-	
-	private String listResultsView;
-	
-	protected ModelAndView onSubmit(HttpServletRequest request, 
-    		HttpServletResponse response, Object command, 
-    		BindException be) throws Exception {
-		
-		ProductLookupBean plb = (ProductLookupBean) command;
-		String queryString = plb.getProduct();
-		
-		Map<String, Object> model = new HashMap<String, Object>(4);
-		String viewName = null;
-				
-		IndexReader ir = IndexReader.open("/Users/cp2/external/lucene/index/product/");
-		IndexSearcher searcher = null;
-		Hits hits = null;
-		
-		searcher = new IndexSearcher(ir);
-		Analyzer analyzer = new StandardAnalyzer();
-		QueryParser qp = new QueryParser("Product",analyzer);
-		Query query = qp.parse(queryString);
-		hits = searcher.search(query);
-		
-		List<SearchHit> results = new ArrayList<SearchHit>();
-		
-		for (int i=0;i<hits.length();i++) {
-		    Document doc = hits.doc(i);
-		    SearchHit ph = new SearchHit();
-		    ph.setTitle(doc.get("Product"));
-		    ph.setChr(doc.get("SysID"));
-		    results.add(ph);
-		}
-		model.put("results", results);
-		viewName = listResultsView;
-		return new ModelAndView(viewName,model);
-	}
+public class ProductSearchController extends TaxonNodeBindingFormController {
 
-	public String getListResultsView() {
-		return listResultsView;
-	}
+    private String listResultsView;
 
-	public void setListResultsView(String listResultsView) {
-		this.listResultsView = listResultsView;
-	}
+    @Override
+    protected ModelAndView onSubmit(@SuppressWarnings("unused")
+    HttpServletRequest request, @SuppressWarnings("unused")
+    HttpServletResponse response, Object command, @SuppressWarnings("unused")
+    BindException be) throws Exception {
+
+        ProductLookupBean plb = (ProductLookupBean) command;
+        String queryString = plb.getProduct();
+
+        Map<String, Object> model = new HashMap<String, Object>(4);
+        String viewName = null;
+
+        IndexReader ir = IndexReader.open("/Users/cp2/external/lucene/index/product/");
+        IndexSearcher searcher = null;
+        Hits hits = null;
+
+        searcher = new IndexSearcher(ir);
+        Analyzer analyzer = new StandardAnalyzer();
+        QueryParser qp = new QueryParser("Product", analyzer);
+        Query query = qp.parse(queryString);
+        hits = searcher.search(query);
+
+        List<SearchHit> results = new ArrayList<SearchHit>();
+
+        for (int i = 0; i < hits.length(); i++) {
+            Document doc = hits.doc(i);
+            SearchHit ph = new SearchHit();
+            ph.setTitle(doc.get("Product"));
+            ph.setChr(doc.get("SysID"));
+            results.add(ph);
+        }
+        model.put("results", results);
+        viewName = listResultsView;
+        return new ModelAndView(viewName, model);
+    }
+
+    public String getListResultsView() {
+        return listResultsView;
+    }
+
+    public void setListResultsView(String listResultsView) {
+        this.listResultsView = listResultsView;
+    }
 }
 
 class ProductLookupBean {
-	private String product;
+    private String product;
 
-	public String getProduct() {
-		return product;
-	}
+    public String getProduct() {
+        return product;
+    }
 
-	public void setProduct(String product) {
-		this.product = product;
-	}
-	
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
 }
 
 class ProductHit {
-	
-	private String product;
-	
-	private String sysId;
-	
-	private String temp;
-	
-	public String getTemp() {
-		return temp;
-	}
 
-	public void setTemp(String temp) {
-		this.temp = temp;
-	}
+    private String product;
 
-	public ProductHit(String tmp, String sysId) {
-		this.temp = tmp;
-		this.sysId = sysId;
-	}
-	
-	public String getProduct() {
-		return product;
-	}
+    private String sysId;
 
-	public void setProduct(String product) {
-		this.product = product;
-	}
+    private String temp;
 
-	public String getSysId() {
-		return sysId;
-	}
+    public String getTemp() {
+        return temp;
+    }
 
-	public void setSysId(String sysId) {
-		this.sysId = sysId;
-	}
-	
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public ProductHit(String tmp, String sysId) {
+        this.temp = tmp;
+        this.sysId = sysId;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getSysId() {
+        return sysId;
+    }
+
+    public void setSysId(String sysId) {
+        this.sysId = sysId;
+    }
+
 }
