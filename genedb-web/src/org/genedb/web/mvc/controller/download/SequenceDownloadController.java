8f14944e96ff44a527baf4adc497e4b72a98ae6b
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-30 15:22:40 +0000

    First attempt at EMBL writing
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7649 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/download/SequenceDownloadController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/download/SequenceDownloadController.java
@@ -1,11 +1,17 @@
 package org.genedb.web.mvc.controller.download;
 
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 @RequestMapping("/SequenceDownload")
@@ -20,14 +26,56 @@ public class SequenceDownloadController {
 	public String processSubmit(
 		@ModelAttribute("seq") SequenceDownloadBean bean, 
 		BindingResult result, 
-		SessionStatus status 
+		SessionStatus status,
+		Writer writer
 	) {
 		
 		if (result.hasErrors()) {
 			return "err/formtest";
 		}
 		
-		return "err/formok";
+		PrintWriter out;
+		if (writer instanceof PrintWriter) {
+			out = (PrintWriter) writer;
+		} else {
+			out = new PrintWriter(writer);
+		}
+		
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put("systematic_id", "fred");
+		writeEmblEntry(out, bean.topLevelFeature, true, 1, 10000, attributes);
+		writeEmblEntry(out, "CDS", true, 1, 100, attributes);
+		writeEmblEntry(out, "misc_feature", true, 100, 200, attributes);
+		writeEmblEntry(out, "wibble", false, 1, 10, attributes);
+		writeEmblEntry(out, "fred", false, 1045, 1096, attributes);
+		writeEmblEntry(out, "foo", true, 1, 100, attributes);
+		
+		
+		return null;
 	}
+
+	private void writeEmblEntry(PrintWriter out, String featureType, 
+			boolean strand, int min, int max,
+			Map<String, String> qualifiers) {
+
+		out.format("FT "+featureType);
+		if (!strand) {
+			out.print("complement(");
+		}
+		
+		out.print(min+"..."+max);
+		
+		if (!strand) {
+			out.print(")");
+		}
+		
+		for (Map.Entry<String, String> qualifier: qualifiers.entrySet()) {
+			out.println("FT       "+qualifier.getKey()+"=\""+qualifier.getValue()+"\"");
+		}
+		
+	}
+	
+	
+	
 	
 }
