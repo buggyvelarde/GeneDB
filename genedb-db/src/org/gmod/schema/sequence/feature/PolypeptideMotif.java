79cfc89cf40f0c130429c2be17252e3c6b7f03cc
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-06 15:50:55 +0000

    New subclass for polypeptide motif
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7734 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-db/src/org/gmod/schema/sequence/feature/PolypeptideMotif.java
@@ -0,0 +1,11 @@
+package org.gmod.schema.sequence.feature;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import org.gmod.schema.sequence.Feature;
+
+@SuppressWarnings("serial")
+@Entity
+@DiscriminatorValue("1154")
+public class PolypeptideMotif extends Feature { }
