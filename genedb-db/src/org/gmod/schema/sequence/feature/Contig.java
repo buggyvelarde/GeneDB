b1fd69388c5cb521e9e2e3baa4b12f2426a18f35
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-09 09:34:55 +0000

    more org.gmod.schema.sequence.feature class
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7746 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-db/src/org/gmod/schema/sequence/feature/Contig.java
@@ -0,0 +1,8 @@
+package org.gmod.schema.sequence.feature;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("236")
+public class Contig extends TopLevelFeature {}
