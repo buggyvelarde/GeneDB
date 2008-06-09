6ccf8f746f99f51eced895af0d14f7a2bb4f0a41
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-09 09:35:22 +0000

    added discriminator value
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7747 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/sequence/feature/Chromosome.java
+++ genedb-db/src/org/gmod/schema/sequence/feature/Chromosome.java
@@ -1,3 +1,8 @@
 package org.gmod.schema.sequence.feature;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("427")
 public abstract class Chromosome extends TopLevelFeature {}
