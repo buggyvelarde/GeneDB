fd40250723465c335023514cc1f73328c654ef6e
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-09 12:52:49 +0000

    changed the design and structure a bit
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7750 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/tags/db/DisplayOrthologues.java
+++ genedb-web/src/org/genedb/web/tags/db/DisplayOrthologues.java
@@ -15,6 +15,8 @@ import org.gmod.schema.cv.CvTerm;
 import org.gmod.schema.sequence.Feature;
 import org.gmod.schema.sequence.FeatureCvTerm;
 import org.gmod.schema.sequence.FeatureRelationship;
+import org.gmod.schema.sequence.feature.Gene;
+import org.gmod.schema.sequence.feature.Polypeptide;
 
 /**
  * Displays orthologous (cluster and individual) for a particular gene on gene
@@ -40,47 +42,49 @@ public class DisplayOrthologues extends SimpleTagSupport {
             if (!"orthologous_to".equals(featRel.getCvTerm().getName()))
                 continue;
             
-            Feature feat = featRel.getFeatureByObjectId();
+            Feature feat =  featRel.getFeatureByObjectId();
+            if (feat instanceof Polypeptide) {
+                Gene gene = ((Polypeptide)feat).getGene();
+                orthologs.put(gene.getUniqueName(), gene.getProductsAsTabSeparatedString());
+            }
             if ("protein_match".equals(feat.getCvTerm().getName())) {
                 int clusterSize = feat.getFeatureRelationshipsForObjectId().size();
                 clusterSizes.put(feat.getUniqueName(), clusterSize);
                 logger.info(String.format("cluster name - %s  %d others", feat.getUniqueName(),
                     clusterSize));
-            } else
-                for (FeatureCvTerm featureCvt : feat.getFeatureCvTerms()) {
-                    CvTerm cvTerm = featureCvt.getCvTerm();
-                    if (!"genedb_products".equals(cvTerm.getCv().getName()))
-                        continue;
-                    String product = cvTerm.getName();
-                    /*
-                     * The below line gets Gene name from the
-                     * corresponding polypeptide - this can
-                     * be changed so that
-                     * something like polypeptide.getGene() can be used
-                     * either
-                     */
-                    Feature gene = GeneUtils.getGeneFromPart(feat);
-                    orthologs.put(gene.getUniqueName(), product);
-                }
+            } 
         }
         
         if (clusterSizes.isEmpty() && orthologs.isEmpty())
             return;
         
         PrintWriter out = new PrintWriter(getJspContext().getOut(), true);
-        out.println("<ul style=\"display: block; text-align: left;\">");
+
         for (Map.Entry<String, Integer> entry : clusterSizes.entrySet()) {
             String name = entry.getKey();
             int size = entry.getValue();
-            out.printf("<li> %s <a href=\"Orthologs?cluster=%1$s\">%d others</a>", name,
+            out.printf("%s <a href=\"Orthologs?cluster=%1$s\">%d others</a>", name,
                         size);
         }
+        out.println("<br>");
+        boolean first = true;
         for (Map.Entry<String, String> entry : orthologs.entrySet()) {
+            if (first) {
+                first = false;
+                out.println("<table>");
+                out.println("<tr>");
+                
+            }
             String name = entry.getKey();
             String product = entry.getValue();
-            out.printf("<li> <a href=\"NamedFeature?name=%s\"> %1$s </a>   %s", name, product);
+            
+            out.printf("<td> <a href=\"NamedFeature?name=%s\"> %1$s </a></td><td>%s</td>", name, product);
+            out.println("</tr>");
         }
-        out.println("</ul>");
+        if(!first) {
+            out.println("</tr>");
+            out.println("</table>");
+        }
     }
 
     public void setPolypeptide(Feature polypeptide) {
