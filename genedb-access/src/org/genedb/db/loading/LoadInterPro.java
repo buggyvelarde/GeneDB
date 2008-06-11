128a68210980b97086b0d14aa8afd713f657ac8c
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-11 19:51:58 +0000

    detypo
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7827 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/LoadInterPro.java
+++ genedb-access/src/org/genedb/db/loading/LoadInterPro.java
@@ -10,71 +10,71 @@ import org.springframework.orm.hibernate3.HibernateTransactionManager;
 
 public class LoadInterPro {
 
-	
-	private static SequenceDao sequenceDao;
-	
-	private static HibernateTransactionManager hibernateTransactionManager;
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
+
+    private static SequenceDao sequenceDao;
+
+    private static HibernateTransactionManager hibernateTransactionManager;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
 
         if (args.length == 0) {
-        	System.err.println("No input files specified");
-        	System.exit(-1);
+            System.err.println("No input files specified");
+            System.exit(-1);
         }
 
         Properties overrideProps = new Properties();
         PropertyOverrideHolder.setProperties("dataSourceMunging", overrideProps);
-        
+
         ApplicationContext ctx = new ClassPathXmlApplicationContext(
                 new String[] {"NewRunner.xml"});
 
         InterProParser runner = (InterProParser) ctx.getBean("ipparser", InterProParser.class);
-        
+
         runner.afterPropertiesSet();
-        
+
         String[] filePaths = args;
-		
-		long start = new Date().getTime();
+
+        long start = new Date().getTime();
 
         for (int i = 0; i < filePaths.length; i++) {
 
-            runner.Parse(filePaths[i]);
+            runner.parse(filePaths[i]);
 
-		}
+        }
 
         long stop = new Date().getTime();
-        
+
         System.err.println("Total time taken - " + (stop - start)/60000 + " min" );
-        
+
     }
-	
-
-	public void afterPropertiesSet() {
-
-		
-	}
 
 
-	public SequenceDao getSequenceDao() {
-		return sequenceDao;
-	}
-
-	public void setSequenceDao(SequenceDao sequenceDao) {
-		this.sequenceDao = sequenceDao;
-	}
+    public void afterPropertiesSet() {
 
 
-	public HibernateTransactionManager getHibernateTransactionManager() {
-		return hibernateTransactionManager;
-	}
+    }
 
 
-	public void setHibernateTransactionManager(
-			HibernateTransactionManager hibernateTransactionManager) {
-		this.hibernateTransactionManager = hibernateTransactionManager;
-	}
+    public SequenceDao getSequenceDao() {
+        return sequenceDao;
+    }
+
+    public void setSequenceDao(SequenceDao sequenceDao) {
+        this.sequenceDao = sequenceDao;
+    }
+
+
+    public HibernateTransactionManager getHibernateTransactionManager() {
+        return hibernateTransactionManager;
+    }
+
+
+    public void setHibernateTransactionManager(
+            HibernateTransactionManager hibernateTransactionManager) {
+        this.hibernateTransactionManager = hibernateTransactionManager;
+    }
 }
