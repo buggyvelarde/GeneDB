31f50b5164a2bf37d1203ee6f74928b42407a795
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-17 07:28:26 +0000

    Keep for now - fix compile problem due to change in cvDao
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7962 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/web/mvc/controller/binder/BrowseBean.java
@@ -0,0 +1,84 @@
+package org.genedb.web.mvc.controller.binder;
+
+import org.gmod.schema.cv.Cv;
+import org.gmod.schema.dao.CvDaoI;
+import org.gmod.schema.dao.SequenceDaoI;
+import org.gmod.schema.sequence.Feature;
+import org.gmod.schema.utils.CountedName;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.ApplicationContextException;
+
+import java.util.List;
+
+public class BrowseBean implements InitializingBean {
+
+    private static final int DEFAULT_LIMIT = 15;
+
+    private Cv cv;
+
+    private CvDaoI cvDao;
+
+    private SequenceDaoI sequenceDao;
+
+    private String cvName;
+
+    private int limit = DEFAULT_LIMIT;
+
+    public void afterPropertiesSet() throws Exception {
+    	// FIXME cvDao wrong
+        //List<Cv> cvs = cvDao.getCvByName(cvName);
+        cv = cvDao.getCvByName(cvName);
+
+//        if (cvs.size() == 0) {
+//            throw new ApplicationContextException("No cv of name '" + cvName
+//                    + "' found when configuring " + getClass());
+//        }
+//        if (cvs.size() > 1) {
+//            throw new ApplicationContextException("Too many ('" + cvs.size() + "') cv of name '"
+//                    + cvName + "' found when configuring " + getClass());
+//        }
+//        cv = cvs.get(0);
+    }
+
+    public List<String> getPossibleMatches(String search) {
+        return cvDao.getPossibleMatches(search, cv, limit);
+        // List<String> results = new ArrayList<String>();
+        // results.add("a");
+        // results.add("b");
+        // results.add("c");
+        // return results;
+    }
+
+    public List<CountedName> getAllTerms() {
+        return cvDao.getAllTermsInCvWithCount(cv);
+    }
+
+    public List<Feature> getFeaturesForCvTerm(String cvTermName) {
+        List<Feature> features = sequenceDao.getFeaturesByCvTermNameAndCvName(cvTermName, cvName);
+        // TODO Replace protein etc with genes
+
+        return features;
+    }
+
+    @Required
+    public void setCvDaoI(CvDaoI cvDao) {
+        this.cvDao = cvDao;
+    }
+
+    @Required
+    public void setCvName(String cvName) {
+        this.cvName = cvName;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    @Required
+    public void setSequenceDaoI(SequenceDaoI sequenceDao) {
+        this.sequenceDao = sequenceDao;
+    }
+
+}
