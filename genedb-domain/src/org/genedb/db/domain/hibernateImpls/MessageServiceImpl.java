185eca933d3e40630e3efc6d8ccff7e6504a97de
cp2 <cp2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-17 15:12:44 +0000

    Java 5 does not support Deque
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@7993 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-domain/src/org/genedb/db/domain/hibernateImpls/MessageServiceImpl.java
+++ genedb-domain/src/org/genedb/db/domain/hibernateImpls/MessageServiceImpl.java
@@ -3,24 +3,24 @@ package org.genedb.db.domain.hibernateImpls;
 import org.genedb.db.domain.misc.Message;
 import org.genedb.db.domain.services.MessageService;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MessageServiceImpl implements MessageService {
 
-    private Map<String, Deque<Message>> messageMap = new HashMap<String, Deque<Message>>();
+    private Map<String, List<Message>> messageMap = new HashMap<String, List<Message>>();
 
     public synchronized void addNotification(String clientName, String string, String string2) {
         if (!messageMap.containsKey(clientName)) {
-            messageMap.put(clientName, new ArrayDeque<Message>());
+            messageMap.put(clientName, new ArrayList<Message>());
         }
-        Deque<Message> messages = messageMap.get(clientName);
-        messages.addLast(new Message());
+        List<Message> messages = messageMap.get(clientName);
+        messages.add(new Message());
     }
 
-    public synchronized Deque<Message> checkMessages(String clientName) {
+    public synchronized List<Message> checkMessages(String clientName) {
         return messageMap.get(clientName);
     }
 
--- genedb-domain/src/org/genedb/db/domain/services/MessageService.java
+++ genedb-domain/src/org/genedb/db/domain/services/MessageService.java
@@ -2,12 +2,12 @@ package org.genedb.db.domain.services;
 
 import org.genedb.db.domain.misc.Message;
 
-import java.util.Deque;
+import java.util.List;
 
 public interface MessageService {
 
     void addNotification(String clientName, String string, String string2);
 
-    Deque<Message> checkMessages(String clientName);
+    List<Message> checkMessages(String clientName);
 
 }
