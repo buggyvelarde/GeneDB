c7df3505ea053cda36b5cfc98d9e259fb767b4bd
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-06-27 16:00:40 +0000

    Gaps should be indexed.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8061 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/sequence/feature/Gap.java
+++ genedb-db/src/org/gmod/schema/sequence/feature/Gap.java
@@ -4,7 +4,9 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 import org.gmod.schema.sequence.Feature;
+import org.hibernate.search.annotations.Indexed;
 
 @Entity
 @DiscriminatorValue("818")
+@Indexed
 public class Gap extends Feature {}
