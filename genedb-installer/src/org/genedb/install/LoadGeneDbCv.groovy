bfb7f01493b61450bfd641587f75b0b819dc8694
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-02 11:09:11 +0000

    Add new terms for storing algorithmically-predicted protein properties.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8077 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-installer/src/org/genedb/install/LoadGeneDbCv.groovy
+++ genedb-installer/src/org/genedb/install/LoadGeneDbCv.groovy
@@ -4,100 +4,100 @@ import groovy.sql.Sql
 import java.util.regex.Matcher
 
 /**
-* Simple class for creating CVs and their terms, 
-* from the  XML file at the bottom. Note it assumes the CVs 
+* Simple class for creating CVs and their terms,
+* from the  XML file at the bottom. Note it assumes the CVs
 * already exist
 */
 class LoadGeneDbCv {
 
     def db
-    
+
     int index
-    
+
     def cvDataSet
     def cvId
-    
+
     def cvTermDataSet
     def termId
-    
+
     def relationDataSet
-   
+
     def REL_IS_A
-    
+
     def dbXRefDataSet
     def dbXRefId
-    
+
     def dbDataSet
     def dbId
-    
+
     def currentCv
     def currentDb
-    
+
     boolean writeBack = true;
-    
+
     LoadGeneDbCv() {
 		db = Sql.newInstance(
 			'jdbc:postgresql://pathdbsrv1a.internal.sanger.ac.uk:10120/pathdb',
 			'xxx',
 			'xxx',
 			'org.postgresql.Driver')
-			
+
      	cvDataSet = db.dataSet("cv")
-		
+
 		cvTermDataSet = db.dataSet("cvterm")
-		
+
      	dbXRefDataSet = db.dataSet("dbxref")
-		
+
      	dbDataSet = db.dataSet("db")
-		
+
      	relationDataSet = db.dataSet("cvterm_relationship")
-     	
+
 	    List relations = db.rows("select * from cvterm cvt, cv cv where cv.name='relationship' and cvt.cv_id = cv.cv_id and cvt.name='is_a'");
 	    REL_IS_A = relations[0]."cvterm_id";
-	    
+
     }
 
-	
-	
+
+
 	void process(def inp) {
-	    def heirachy = new XmlParser().parseText(inp) 
-  
+	    def heirachy = new XmlParser().parseText(inp)
+
 	    heirachy.children().each(
 	            {createNode(it, null, -1)}
-        )		
+        )
 	}
 
-	
-	// Now walk the tree creating db entries  
+
+	// Now walk the tree creating db entries
 	void createNode(def node, def parent, int nestedCvTerm) {
 		assert node != null
 		switch (node.name()) {
-		    
+
 			case 'cv':
 				List rows = db.rows("select * from cv where name='"+node.'@name'+"'")
 				if (rows[0] == null) {
 				    throw new RuntimeException("Couldn't get cv for '"+node.'@name'+"'");
 				}
 				currentCv = rows[0]."cv_id"
-				
+
 				rows = db.rows("select * from db where name='"+node.'@db'+"'")
 				if (rows[0] == null) {
 				    throw new RuntimeException("Couldn't get db for '"+node.'@db'+"'");
 				}
 				currentDb = rows[0]."db_id"
-				
+
 				node.children().each(
 					{createNode(it, node, -1)}
 				)
 				break
-			
-			
+
+
 			case 'term':
 				checkAttributeExists(node, "description")
-			    
+
 				int newDbXRef = createDbXRef(node)
 				int newCvTerm = createCvTerm(node, newDbXRef)
-          	  
+
 				if (nestedCvTerm > 0) {
 				    // Create feature relationship
 				    if (writeBack) {
@@ -112,22 +112,22 @@ class LoadGeneDbCv {
 				  	      System.err.println("Problem storing cvterm - duplicate?")
 					   }
 				    }
-				    
+
 				}
-					
+
 				node.children().each(
 						{createNode(it, node, newCvTerm)}
 				)
 				break
-				
-				
+
+
 			default:
 				throw new RuntimeException("Saw a node of '"+node.name()
 				        +"' when expecting node or organism");
 		}
 	}
- 
-  
+
+
 	// Create a dbxref entry when a cvterm node is encountered
 	int createDbXRef(def node) {
 		String name = node.'@name';
@@ -136,9 +136,9 @@ class LoadGeneDbCv {
 		if (writeBack) {
 		    try {
 				dbXRefDataSet.add(
-    	   		 	description: description, 	
-       			 	accession:   description, 	
-       			 	db_id:          currentDb,		
+    	   		 	description: description,
+       			 	accession:   description,
+       			 	db_id:          currentDb,
        			 	version:     1
       			)
 		    } catch (Exception exp) {
@@ -148,8 +148,8 @@ class LoadGeneDbCv {
 		}
 		return getMaxIdFromPrimaryKey("dbxref")
 	}
-	
-	
+
+
 	// Create a set of db entries when an organism node is encountered
 	int createCvTerm(def node, def dbXRef) {
 		//println "CreateOrganism called with '"+node.'@name'+"'"
@@ -172,8 +172,8 @@ class LoadGeneDbCv {
 		}
 		return getMaxIdFromPrimaryKey("cvterm")
 	}
-        
-	
+
+
 	// Find a cvterm by name
 	int findCvTerm(String termName) {
 	    def rows = db.rows("select cvterm_id from cvterm where name='"+termName
@@ -183,10 +183,10 @@ class LoadGeneDbCv {
 	    }
 	    return rows[0]["cvterm_id"]
 	}
-	
-	
-	 
-	// Sanity check that an expected property is present 
+
+
+
+	// Sanity check that an expected property is present
 	void checkAttributeExists(def node, String key) {
 	    if (!node.attributes().containsKey(key)) {
 	        throw new AppException("No "+key+" for '"+node.'@name'+"'")
@@ -196,9 +196,9 @@ class LoadGeneDbCv {
 	        throw new AppException("The value for '"+key+"' is empty in '"+node.'@name'+"'")
 	    }
 	}
-	
 
-	// Find the maximum current value of the primary key for this table 
+
+	// Find the maximum current value of the primary key for this table
 	def getMaxIdFromPrimaryKey(String tableName) {
 		def col = tableName + "_id";
 	    int id = db.rows("SELECT max("+col+") as "+col+" from " + tableName)[0]["${col}"];
@@ -207,8 +207,8 @@ class LoadGeneDbCv {
 	    }
 	    return id;
 	}
-	
-	
+
+
 	static void main(args) {
     	LoadGeneDbCv lgc = new LoadGeneDbCv()
     	try {
@@ -219,7 +219,7 @@ class LoadGeneDbCv {
     	}
     	System.err.println("Done (writeBack='"+lgc.writeBack+"')");
 	}
-	
+
 
 	// The XML config file for loading the organism and phylogeny modules
 	static String input = '''<?xml version="1.0" encoding="UTF-8"?>
@@ -247,29 +247,37 @@ class LoadGeneDbCv {
                 <term name="RepeatMasker" description="Mask repeats ge for blasting" />
             </term>
         </term>
-        
-        <term name="top_level_seq" 
+
+        <term name="top_level_seq"
               description="Marker to indicate that a sequence should be considered a top level feature" />
 
-        <term name="taxonomy" 
+        <term name="taxonomy"
               description="CvTerm to indicate that a phylotree is a type of taxonomic tree" />
 
         <term name="protein_stat" description="Keys for storing information specifically for proteins">
             <term name="protein_charge" description="Protein charge in Daltons" />
             <term name="molecular_mass" description="Mass of the feature" />
-            <term name="isoelectric_point" description="Isoelectric point" />        
+            <term name="isoelectric_point" description="Isoelectric point" />
         </term>
-        
+
+        <term name="protein_property" description="A property of a polypeptide feature">
+            <term name="GPI_anchored" description="A flag to indicate that the protein is predicted to be GPI-anchored"/>
+            <term name="SignalP_prediction" description="The SignalP prediction for this protein. Possible values are 'Signal peptide' and 'Signal anchor'."/>
+            <term name="signal_peptide_probability" description="The probability that this protein is a signal peptide, as predicted by SignalP."/>
+            <term name="signal_anchor_probability" description="The probability that this protein is a signal anchor, as predicted by SignalP."/>
+            <term name="PlasmoAP_score" description="A score denoting how likely this protein is to target the apicoplast, as predicted by PlasmoAP."/>
+        </term>
+
         <term name="feature_cvterm_props" description="Keys for storing information specifically for FeatureCvTermProps">
             <term name="qualifier" description="eg NOT, colocalizes_with" />
             <term name="evidence" description="Typically an evidence code" />
 			<term name="residue" description="The residues affected by this" />
 			<term name="attribution" description="Who supplied this data" />
         </term>
-        
+
         <term name="feature_props" description="Keys for storing information, for any feature in general">
             <term name="curation" description="Free text note field for local curation" />
-            <term name="private" description="Free text note field for comments not to be made publicly visible" />     
+            <term name="private" description="Free text note field for comments not to be made publicly visible" />
  			<term name="EC_number" description="Free text note field for EC_number" />
  			<term name="colour" description="Numeric key for storing a display colour" />
         </term>
@@ -282,9 +290,9 @@ class LoadGeneDbCv {
                 <term name="curatorEmail" description="Email of organism curator" />
                 <term name="mitochondrialTranslationTable" description="Translation table used for mitochondrial DNA" />
                 <term name="translationTable" description="Translation table" />
-				<term name="app_www_homePage" description="Style of homepage wanted, if any" />  
+				<term name="app_www_homePage" description="Style of homepage wanted, if any" />
 				<term name="htmlShortName" description="The short name in HTML format" />
-				<term name="htmlFullName" description="The full name in HTML format" />                                                                                      
+				<term name="htmlFullName" description="The full name in HTML format" />
         </term>
 		<term name="artemis_specific" description="cvterm for various *_file qualifiers for artemis ">
 			<term name="blast_file"   description="Blast file location" />
@@ -302,15 +310,15 @@ class LoadGeneDbCv {
 		</term>
     </cv>
 
-    
-    	
+
+
     <cv name="genedb_literature" db="genedb_misc">
         <term name="unknown"   description="unknown literature type" />
         <term name="unfetched" description="unfetched literature type" />
         <term name="journal"   description="journal literature type" />
     </cv>
-    
-    
+
+
     <cv name="genedb_synonym_type" db="genedb_misc">
         <term name="reserved_name"   description="A name reserved for future use eg a paper pending" />
         <term name="synonym" description="synonym" />
