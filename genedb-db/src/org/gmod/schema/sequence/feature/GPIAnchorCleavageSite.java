626a834c7ccf5f2a5c9863579b4efd304b85b415
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-04 09:44:01 +0000

    Add comment.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8091 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/sequence/feature/GPIAnchorCleavageSite.java
+++ genedb-db/src/org/gmod/schema/sequence/feature/GPIAnchorCleavageSite.java
@@ -4,6 +4,11 @@ import javax.persistence.Entity;
 
 import org.gmod.schema.cfg.FeatureType;
 
+/*
+ * There isn't yet a suitable SO term for this, so we're temporarily
+ * using a local term. When this term is added to SO, we should switch
+ * to using the SO term instead.
+ */
 @Entity
 @FeatureType(cv="genedb_feature_type", term="GPI_anchor_cleavage_site")
 public class GPIAnchorCleavageSite extends PolypeptideRegion { }
