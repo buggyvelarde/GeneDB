9da017810ddfe6c141b866324247947d9396ccbc
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-07 08:26:57 +0000

    Doc tweaks
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8110 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/polypeptide/DbXRefManager.java
+++ genedb-access/src/org/genedb/db/loading/polypeptide/DbXRefManager.java
@@ -12,8 +12,13 @@ import org.hibernate.EmptyInterceptor;
 /**
  * Maintains a cache of the DbXRefs that have been created but
  * not yet flushed, so we can look them up even though they're
- * not in the database yet. The cache is cleared whenever
- * Hibernate flushes.
+ * not in the database yet. If you register your instance
+ * of this class as a Hibernate interceptor, the cache is cleared
+ * whenever Hibernate flushes.
+ * <p>
+ * This mechanism is available to {@link Loader} subclasses via
+ * the protected Loader field <code>dbxrefManager</code>. At the time of
+ * writing, it's only used by {@link InterProLoader}.
  *
  * @author rh11
  */
@@ -36,8 +41,8 @@ class DbXRefManager extends EmptyInterceptor {
 
     /**
      * Get or create a DbXRef. Works even if the DbXRef has been
-     * created but not yet flushed, unlike
-     * {@link FeatureUtils#findOrCreateDbXRefFromDbAndAccession(String,String)}
+     * created but not yet flushed, as long as it was created using
+     * this object.
      *
      * @param identifier A string of the form <code>db:accession</code>
      * @return the existing or newly-created DbXRef
@@ -52,8 +57,8 @@ class DbXRefManager extends EmptyInterceptor {
 
     /**
      * Get or create a DbXRef. Works even if the DbXRef has been
-     * created but not yet flushed, unlike
-     * {@link FeatureUtils#findOrCreateDbXRefFromDbAndAccession(String,String)}
+     * created but not yet flushed, as long as it was created using
+     * this object.
      *
      * @param dbName The database name
      * @param accession The database-specific identifier
@@ -71,8 +76,8 @@ class DbXRefManager extends EmptyInterceptor {
         /* The above statement can trigger a flush, which is why we
          * need this check afterwards rather than before. That was
          * a hell of a bug to discover at 6am, I can tell you!
-         * Especially when it conspired with a classpath misconfguration
-         * that meant the logging didn't work. (If the logging had worked
+         * (Especially when it conspired with a classpath misconfguration
+         * that meant the logging didn't work. If the logging had worked
          * it would have been pretty easy to see what was happening, to
          * tell you the truth.) -rh11
          */
@@ -85,8 +90,8 @@ class DbXRefManager extends EmptyInterceptor {
 
     /**
      * Get or create a DbXRef, and set the description if it's not already set.
-     * Works even if the DbXRef has been created but not yet flushed, unlike
-     * {@link FeatureUtils#findOrCreateDbXRefFromDbAndAccession(String,String)}
+     * Works even if the DbXRef has been created but not yet flushed, as long
+     * as it was created using this object.
      *
      * @param dbName The database name
      * @param accession The database-specific identifier
