3be7534416ad3104962dfa13140c66191234f15b
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-09 15:01:52 +0000

    Whitespace.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8155 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/BrowseBeanName.java
+++ genedb-web/src/org/genedb/web/mvc/controller/BrowseBeanName.java
@@ -7,42 +7,41 @@ import org.genedb.db.dao.SequenceDao;
 import org.gmod.schema.cv.CvTerm;
 import org.springframework.beans.factory.InitializingBean;
 
-public class BrowseBeanName implements InitializingBean{
+public class BrowseBeanName implements InitializingBean {
 
-	private static final int DEFAULT_LIMIT = 15;
+    private static final int DEFAULT_LIMIT = 15;
 
-	private SequenceDao sequenceDao;
+    private SequenceDao sequenceDao;
 
-	private CvDao cvDao;
+    private CvDao cvDao;
 
-	private String term;
+    private String term;
 
-	private CvTerm cvTerm;
+    private CvTerm cvTerm;
 
-	private int limit = DEFAULT_LIMIT;
+    private int limit = DEFAULT_LIMIT;
 
-	public void afterPropertiesSet() throws Exception {
-		cvTerm = cvDao.getCvTermByNameAndCvName(term, "sequence");
+    public void afterPropertiesSet() throws Exception {
+        cvTerm = cvDao.getCvTermByNameAndCvName(term, "sequence");
+    }
 
-	}
+    public List<String> getPossibleMatches(String search) {
+        return sequenceDao.getPossibleMatches(search, cvTerm, limit);
+    }
 
-	public List<String> getPossibleMatches(String search) {
-		return sequenceDao.getPossibleMatches(search, cvTerm, limit);
-	}
+    public void setSequenceDao(SequenceDao sequenceDao) {
+        this.sequenceDao = sequenceDao;
+    }
 
-	public void setSequenceDao(SequenceDao sequenceDao) {
-		this.sequenceDao = sequenceDao;
-	}
+    public void setCvTerm(String cvTerm) {
+        this.term = cvTerm;
+    }
 
-	public void setCvTerm(String cvTerm) {
-		this.term = cvTerm;
-	}
+    public void setCvDao(CvDao cvDao) {
+        this.cvDao = cvDao;
+    }
 
-	public void setCvDao(CvDao cvDao) {
-		this.cvDao = cvDao;
-	}
-
-	public void setLimit(int limit) {
-		this.limit = limit;
-	}
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 }
