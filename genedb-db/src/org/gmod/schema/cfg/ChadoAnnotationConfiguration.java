bc61a8bf3f52ef556411821be8e954f8de212b10
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-09 20:12:23 +0000

    Make setDataSource public. Add documentation.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8159 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/cfg/ChadoAnnotationConfiguration.java
+++ genedb-db/src/org/gmod/schema/cfg/ChadoAnnotationConfiguration.java
@@ -7,20 +7,35 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
+import org.hibernate.MappingException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.SingleTableSubclass;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.util.ClassUtils;
 
 /**
- * Extends org.hibernate.cfg.AnnotationConfiguration with support for the <code>@FeatureType</code> annotation.
- *
- * If you use this directly, you must set the data source. With Spring, use {@link ChadoSessionFactoryBean},
- * which defaults to this configuration and automatically sets the data source.
+ * Extends org.hibernate.cfg.AnnotationConfiguration in two ways:
+ * <ul>
+ * <li> Adds support for the {@link FeatureType} annotation.
+ * <li> Extends the behaviour of {@link #addPackage} to add all annotated classes in the package.
+ * </ul>
+ * If you use this directly, you must set the data source. From Spring, use {@link ChadoSessionFactoryBean}
+ * instead, which defaults to this configuration and automatically sets the data source.
+ * <p>
+ * The class {@link org.genedb.hibernate.search.IndexChado}, from the HibernateSearch
+ * module, uses this class directly. It may serve as an example of how it can be used.
+ * (Not that there's any magic to it: just use it in place of the ordinary Hibernate
+ * <code>Configuration</code> class, and remember to set the data source.)
  *
  * @author rh11
  */
@@ -28,18 +43,63 @@ public class ChadoAnnotationConfiguration extends AnnotationConfiguration {
     private static final Logger logger = Logger.getLogger(ChadoAnnotationConfiguration.class);
 
     private DataSource dataSource;
-    void setDataSource(DataSource dataSource) {
+    public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private ClassLoader classLoader = getClass().getClassLoader();
+    /**
+     * Set the class loader to use for locating the classes within a
+     * package, when <code>addPackage</code> is called. The default,
+     * if this method is not called, is to use the class loader that was
+     * used to load this class.
+     *
+     * @param classLoader
+     */
+    public void setClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    /**
+     * Adds the specified package, reading any package metadata, and
+     * <b>also</b> adds all annotated classes from this package.
+     */
+    @Override
+    public AnnotationConfiguration addPackage(String packageName) throws MappingException {
+        logger.info("Adding package: " + packageName);
+        addClassesFromAnnotatedPackage(packageName);
+        super.addPackage(packageName);
+        return this;
+    }
+
+    private void addClassesFromAnnotatedPackage(String packageName) {
+        ClassPathScanningCandidateComponentProvider scanner =
+            new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter( new AnnotationTypeFilter(Entity.class) );
+        scanner.addIncludeFilter( new AnnotationTypeFilter(MappedSuperclass.class) );
+
+        for (BeanDefinition bd : scanner.findCandidateComponents(packageName)) {
+          String className = bd.getBeanClassName();
+          Class<?> type = ClassUtils.resolveClassName(className, this.classLoader);
+          addAnnotatedClass(type);
+          logger.info(String.format("Added class '%s'", className));
+        }
     }
 
     @Override
     public SessionFactory buildSessionFactory() throws HibernateException {
+        logger.debug("Building session factory");
+        buildMappings();
+
+        /* Not quite as redundant as it looks: the <code>classes</code>
+         * field is declared as a plain old non-generic Map. */
         @SuppressWarnings("unchecked")
         Map<String,PersistentClass> classes = this.classes;
 
         for (Map.Entry<String,PersistentClass> e: classes.entrySet()) {
             String className = e.getKey();
             PersistentClass persistentClass = e.getValue();
+            logger.trace(String.format("Inspecting class '%s'", className));
             if (!(persistentClass instanceof SingleTableSubclass))
                 continue;
 
