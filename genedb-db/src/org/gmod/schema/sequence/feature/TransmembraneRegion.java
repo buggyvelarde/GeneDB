6326b9775a68f4594316bc05b3f7daad5a6e82a5
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-09 11:40:01 +0000

    Add comment.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8148 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/sequence/feature/TransmembraneRegion.java
+++ genedb-db/src/org/gmod/schema/sequence/feature/TransmembraneRegion.java
@@ -6,6 +6,12 @@ import org.gmod.schema.cfg.FeatureType;
 import org.gmod.schema.cv.CvTerm;
 import org.gmod.schema.organism.Organism;
 
+/*
+ * This term is specified by accession number, because the term name
+ * has recently been changed from 'transmembrane' to 'transmembrane_region',
+ * so we might see either depending on which version of Sequence Ontology
+ * is present.
+ */
 @Entity
 @FeatureType(cv="sequence", accession="0001077")
 public class TransmembraneRegion extends PolypeptideRegion {
