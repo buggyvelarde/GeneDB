70a0abf69f9b22a2e028d488beabaa4507f04a5e
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-11 11:26:55 +0000

    Suppress spurious warning. Remove unreachable code.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8209 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/validators/NamedFeatureFormValidator.java
+++ genedb-web/src/org/genedb/web/mvc/validators/NamedFeatureFormValidator.java
@@ -6,24 +6,18 @@ import org.springframework.validation.Validator;
 
 public class NamedFeatureFormValidator implements Validator{
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"unchecked", "unused"})
     public boolean supports(Class clazz) {
         return true;
     }
 
     public void validate(Object object, Errors errors) {
         NameLookupBean bean = (NameLookupBean) object;
-        
+
         if(bean.getName() == null && bean.getOrganism() == null) {
             errors.reject("no.params");
             return;
         }
-        
-        if(bean.getName() == null && bean.getOrganism() != null) {
-            errors.reject("show.form");
-            return;
-        }
-
     }
 
 }
