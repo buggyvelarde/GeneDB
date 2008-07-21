d9c1f635d1f2a0e9805e2f38ec96508fdfa10246
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-21 16:39:38 +0000

    Add constructor and makeHeirachy method
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8316 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/feature/Gene.java
+++ genedb-db/src/org/gmod/schema/feature/Gene.java
@@ -1,14 +1,24 @@
 package org.gmod.schema.feature;
 
 import org.gmod.schema.cfg.FeatureType;
+import org.gmod.schema.mapped.CvTerm;
+import org.gmod.schema.mapped.Feature;
+import org.gmod.schema.mapped.Organism;
+import org.gmod.schema.utils.StrandedLocation;
+import org.gmod.schema.utils.SingleLocation;
 
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.Store;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.CodingErrorAction;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
 
 import javax.persistence.Entity;
 import javax.persistence.Transient;
@@ -17,7 +27,13 @@ import javax.persistence.Transient;
 @FeatureType(cv="sequence", term="gene")
 @Indexed
 public class Gene extends AbstractGene {
-    @Transient
+
+	public Gene(Organism organism, String systematicId, boolean analysis,
+			boolean obsolete, Timestamp dateAccessioned) {
+		super(organism, systematicId, analysis, obsolete, dateAccessioned);
+	}
+	
+	@Transient
     public Collection<MRNA> getCodingTranscripts() {
         Collection<MRNA> ret = new ArrayList<MRNA>();
 
@@ -61,4 +77,59 @@ public class Gene extends AbstractGene {
         }
         return products.toString();
     }
+
+      
+	public static Gene make(Feature parent, StrandedLocation location, String systematicId, Organism organism, Timestamp now) {
+		Gene gene = new Gene(organism, systematicId, false, false, now);
+		parent.addLocatedChild(gene, location);
+		return gene;
+	}
+    
+	@Transactional
+	public static Gene makeHeirachy(Feature parent, StrandedLocation location, String systematicId,
+			Class transcriptType, boolean coding) {
+      
+		Organism organism = parent.getOrganism();
+		
+		Timestamp now = new Timestamp(new Date().getTime());
+
+
+      Gene gene = make(parent, location, systematicId, organism, now);
+      
+      // TODO Do by reflection
+      String transcriptId = systematicId + ":mRNA";
+      Transcript transcript = MRNA.make(parent, location, transcriptId, organism, now);
+      
+      gene.addTranscript(transcript);
+      
+      // Store exons
+      int exonCount = 0;
+      for (SingleLocation exonLocation : location.getLocations()) {
+          exonCount++;
+          
+          String exonSystematicId = systematicId + ":exon:"+ exonCount;
+          Exon exon = Exon.make(parent, exonLocation, exonSystematicId, organism, now);
+          
+          transcript.addExon(exon);
+      }
+//          Feature exon = this.featureUtils
+//          .createFeature("exon", this.gns.getExon(systematicId,
+//                  1, exonCount), this.organism);
+//          FeatureRelationship exonFr = this.featureUtils.createRelationship(
+//                  exon, rna, REL_PART_OF, exonCount -1);
+//          FeatureLoc exonFl = this.featureUtils.createLocation(parent, exon, l
+//                  .getMin()-1, l.getMax(), strand);
+      if (transcript instanceof ProductiveTranscript) {
+    	 Polypeptide polypeptide = Polypeptide.make(parent, location, systematicId+":pep", organism, now);
+    	 ProductiveTranscript pt = (ProductiveTranscript) transcript;
+    	 pt.setProtein(polypeptide);
+      }
+      
+		return gene;
+	}
+    
+
+	public void addTranscript(Transcript transcript) {
+		addFeatureRelationship(transcript, "relationship", "part_of");
+	}
 }
