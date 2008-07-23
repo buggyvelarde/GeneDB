d5aa58dc2addda49af2e8a96b8836ec2a05a6d35
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-23 11:53:16 +0000

    Add type for snoRNA
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8330 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-db/src/org/gmod/schema/feature/SnoRNA.java
@@ -0,0 +1,28 @@
+package org.gmod.schema.feature;
+
+import java.sql.Timestamp;
+
+import org.gmod.schema.cfg.FeatureType;
+import org.gmod.schema.mapped.Organism;
+
+import org.hibernate.search.annotations.Indexed;
+
+import javax.persistence.Entity;
+import javax.persistence.Transient;
+
+@Entity
+@FeatureType(cv="sequence", term="snoRNA")
+@Indexed
+public class SnoRNA extends Transcript {
+
+	public SnoRNA(Organism organism, String systematicId, boolean analysis,
+			boolean obsolete, Timestamp dateAccessioned) {
+		super(organism, systematicId, analysis, obsolete, dateAccessioned);
+	}
+	
+    @Override @Transient
+    public Integer getColourId() {
+        return null;
+    }
+
+}
