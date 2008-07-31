1b9f6440cedf19d49d5faaddae9a4fcb19c14e9c
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-31 21:12:45 +0000

    Tighten access permissions
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8410 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/utils/Strand.java
+++ genedb-db/src/org/gmod/schema/utils/Strand.java
@@ -1,16 +1,16 @@
 package org.gmod.schema.utils;
 
 public enum Strand {
-	
-	FORWARD((short)1), UNKNOWN((short)0), REVERSE((short)-1);
-	
-	short value;
-	
-	Strand(short value) {
-		this.value = value;
-	}
-	
-	public short getValue() {
-		return value;
-	}
+
+    FORWARD((short) 1), UNKNOWN((short) 0), REVERSE((short) -1);
+
+    private short value;
+
+    Strand(short value) {
+        this.value = value;
+    }
+
+    public short getValue() {
+        return value;
+    }
 }
