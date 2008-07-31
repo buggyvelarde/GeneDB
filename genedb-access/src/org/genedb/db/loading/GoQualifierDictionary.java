251646152017bf9349ebe8d3642590e6eabf75c9
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-31 15:46:12 +0000

    Tabs to spaces.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8395 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/GoQualifierDictionary.java
+++ genedb-access/src/org/genedb/db/loading/GoQualifierDictionary.java
@@ -36,9 +36,9 @@ import java.util.Set;
 
 import javax.xml.parsers.SAXParserFactory;
 
-
 /**
- * This class stores a list of GO and PSU valid values for the GO qualifier column
+ * This class stores a list of GO and PSU valid values for the GO qualifier
+ * column
  *
  * @author <a href="mailto:art@sanger.ac.uk">Adrian Tivey</a>
  */
@@ -48,144 +48,140 @@ public class GoQualifierDictionary {
     private Set<String> psuQualifiers = new HashSet<String>();
 
     private void initOfficialQualifiers() {
-	officialQualifiers = new HashSet<String>();
-	officialQualifiers.add("NOT");
-	officialQualifiers.add("contributes_to");
-	officialQualifiers.add("colocalizes_with");
+        officialQualifiers = new HashSet<String>();
+        officialQualifiers.add("NOT");
+        officialQualifiers.add("contributes_to");
+        officialQualifiers.add("colocalizes_with");
     }
 
     private static final String GO_QUALIFIER_FILENAME = "/nfs/disk222/yeastpub/analysis/yeast/pombe/CONTIGS/DOCS/PSU_GO_qualifiers.txt";
 
     private void initPsuQualifiers() {
-	FileReader r = null;
-	try {
-	    r = new FileReader(GO_QUALIFIER_FILENAME);
-	    GoQualifierParser gqp = new GoQualifierParser(r);
-	    gqp.go();
-	} catch (FileNotFoundException e) {
-	    System.err.println("ERROR: Can't load Val's qualifier file: '"+GO_QUALIFIER_FILENAME+"'");
-	}
+        FileReader r = null;
+        try {
+            r = new FileReader(GO_QUALIFIER_FILENAME);
+            GoQualifierParser gqp = new GoQualifierParser(r);
+            gqp.go();
+        } catch (FileNotFoundException e) {
+            System.err.println("ERROR: Can't load Val's qualifier file: '" + GO_QUALIFIER_FILENAME
+                    + "'");
+        }
     }
-
 
     GoQualifierDictionary() {
-	initOfficialQualifiers();
-	initPsuQualifiers();
+        initOfficialQualifiers();
+        initPsuQualifiers();
     }
-
 
     /**
      * @return
      */
     public boolean isQualifierPSUValid(String qualifier) {
-	if (qualifier == null) {
-	    return false;
-	}
-	if ( !(psuQualifiers.contains(qualifier) || officialQualifiers.contains(qualifier))) {
-	    return false;
-	}
-	return true;
+        if (qualifier == null) {
+            return false;
+        }
+        if (!(psuQualifiers.contains(qualifier) || officialQualifiers.contains(qualifier))) {
+            return false;
+        }
+        return true;
     }
 
     public boolean isQualifierGOValid(String qualifier) {
-	// TODO contributes_to only valid for function, colocalizes_with for component
-	if (qualifier == null) {
-	    return false;
-	}
-	if ( !officialQualifiers.contains(qualifier)) {
-	    return false;
-	}
-	return true;
+        // TODO contributes_to only valid for function, colocalizes_with for
+        // component
+        if (qualifier == null) {
+            return false;
+        }
+        if (!officialQualifiers.contains(qualifier)) {
+            return false;
+        }
+        return true;
     }
 
     /**
      * @return
      */
     public String getQualifierString(List<String> qualifier) {
-	if (qualifier == null || qualifier.size()==0) {
-	    return StringUtilities.EMPTY_STRING;
-	}
-	return StringUtils.collectionToDelimitedString(qualifier, "|");
+        if (qualifier == null || qualifier.size() == 0) {
+            return StringUtilities.EMPTY_STRING;
+        }
+        return StringUtils.collectionToDelimitedString(qualifier, "|");
     }
-
-
 
     public class GoQualifierParser extends DefaultHandler implements ErrorHandler {
 
-	private Reader reader;
+        private Reader reader;
 
-	public GoQualifierParser(Reader r) {
-	    this.reader = r;
-	}
+        public GoQualifierParser(Reader r) {
+            this.reader = r;
+        }
 
-	private StringBuilder accumulator = new StringBuilder();
+        private StringBuilder accumulator = new StringBuilder();
 
-	// When the parser encounters plain text (not XML elements), it calls
-	// this method, which accumulates them in a string buffer.
-	// Note that this method may be called multiple times, even with no
-	// intervening elements.
-	@Override
-    public void characters(char[] buffer, int start, int length) {
-	    accumulator.append(buffer, start, length);
-	}
+        // When the parser encounters plain text (not XML elements), it calls
+        // this method, which accumulates them in a string buffer.
+        // Note that this method may be called multiple times, even with no
+        // intervening elements.
+        @Override
+        public void characters(char[] buffer, int start, int length) {
+            accumulator.append(buffer, start, length);
+        }
 
+        @Override
+        public void warning(SAXParseException exception) {
+            System.err.println("WARN: GOParser line " + exception.getLineNumber() + ": "
+                    + exception.getMessage());
+        }
 
-	@Override
-    public void warning(SAXParseException exception) {
-	    System.err.println("WARN: GOParser line " + exception.getLineNumber() + ": " +
-		    exception.getMessage());
-	}
+        @Override
+        public void error(SAXParseException exception) {
+            System.err.println("ERROR: GOParser line " + exception.getLineNumber() + ": "
+                    + exception.getMessage());
+        }
 
+        // Report a non-recoverable error and exit
+        @Override
+        public void fatalError(SAXParseException exception) throws SAXException {
+            System.err.println("FATAL: GOParser line " + exception.getLineNumber() + ": "
+                    + exception.getMessage());
+            throw (exception);
+        }
 
-	@Override
-    public void error(SAXParseException exception) {
-	    System.err.println("ERROR: GOParser line " + exception.getLineNumber() + ": " +
-		    exception.getMessage());
-	}
+        public void go() {
 
-	// Report a non-recoverable error and exit
-	@Override
-    public void fatalError(SAXParseException exception) throws SAXException {
-	    System.err.println("FATAL: GOParser line " + exception.getLineNumber() + ": " +
-		    exception.getMessage());
-	    throw(exception);
-	}
+            try {
+                SAXParserFactory factory = SAXParserFactory.newInstance();
+                factory.setValidating(false);
+                factory.setNamespaceAware(true);
 
-	public void go() {
+                // Create the builder and parse the file
+                factory.newSAXParser().parse(new InputSource(reader), this);
+            } catch (SAXException se) {
+                System.err.println(se.getMessage());
+                System.exit(1);
+            } catch (Throwable t) {
+                System.err.println(t);
+                t.printStackTrace();
+            }
+        }
 
-	    try {
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-		factory.setValidating(false);
-		factory.setNamespaceAware(true);
+        // At the beginning of each new element, erase any accumulated text.
+        @Override
+        @SuppressWarnings("unused")
+        public void startElement(String namespaceURL, String localName, String qname,
+                Attributes attributes) {
+            accumulator.setLength(0);
+        }
 
-		// Create the builder and parse the file
-		factory.newSAXParser().parse(new InputSource(reader), this);
-	    } catch (SAXException se) {
-		System.err.println(se.getMessage());
-		System.exit(1);
-	    } catch (Throwable t) {
-		System.err.println(t);
-		t.printStackTrace();
-	    }
-	}
-
-
-	// At the beginning of each new element, erase any accumulated text.
-	@Override
-    public void startElement(String namespaceURL, String localName,
-		String qname, Attributes attributes) {
-	    accumulator.setLength(0);
-	}
-
-
-	// Take special action when we reach the end of selected elements.
-	@Override
-    public void endElement(String namespaceURL, String localName, String qname) {
-	    if ( localName.trim().equals("name")) {
-		String field = accumulator.toString().trim();
-		psuQualifiers.add( field );
-	    }
-	}
+        // Take special action when we reach the end of selected elements.
+        @Override
+        @SuppressWarnings("unused")
+        public void endElement(String namespaceURL, String localName, String qname) {
+            if (localName.trim().equals("name")) {
+                String field = accumulator.toString().trim();
+                psuQualifiers.add(field);
+            }
+        }
     }
 
 }
