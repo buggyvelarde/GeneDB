b2465c366d05a3545f8ab21b1e4bd6274e90275a
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-07-31 15:49:11 +0000

    Clean up some of the doc text.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8399 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/Names.java
+++ genedb-access/src/org/genedb/db/loading/Names.java
@@ -3,7 +3,7 @@ package org.genedb.db.loading;
 import java.util.List;
 
 public class Names {
-    
+
     private String systematicId; // Permanent, or temporary unique id
     private boolean idTemporary; // Is the systematic id permanent or temporary
     private String primary; // ie the gene name
@@ -12,58 +12,58 @@ public class Names {
     private List<String> obsolete; // a list of names that are no longer recommended
     private List<String> reserved; // A future primary name for this feature
     private List<String> previousSystematicIds; // a list of old systematic ids
-    
+
     /**
      * Get the protein name, which is often different from the gene name eg in case
-     * 
+     *
      * @return the protein name, or null if not set
      */
     public String getProtein() {
         return this.protein;
     }
-    
+
     /**
      * Set the protein name, which is often different from the gene name eg in case
-     * 
+     *
      * @param protein the protein name
      */
     public void setProtein(String protein) {
         this.protein = protein;
     }
-    
-    
+
+
     /**
      * Is the systematic id for this feature temporary
-     * 
+     *
      * @return true if it is temporary ie likely to change
      */
     public boolean isIdTemporary() {
         return this.idTemporary;
     }
-    
-    
+
+
     /**
-     * Get a list of obsolete names for this feature 
-     * 
+     * Get a list of obsolete names for this feature
+     *
      * @return the list of obsolete names, possibly empty
      */
     public List<String> getObsolete() {
         return this.obsolete;
     }
-    
+
 
     /**
-     * 
-     * 
+     *
+     *
      * @param obsolete
      */
     public void setObsolete(List<String> obsolete) {
         this.obsolete = obsolete;
     }
-    
+
     /**
-     * The human readable name eg the genes the gene name
-     * 
+     * The human readable name, e.g. the gene name
+     *
      * @return the primary name
      */
     public String getPrimary() {
@@ -82,9 +82,9 @@ public class Names {
         return this.synonyms;
     }
     /**
-     * Set the list of synonyms, which are other names which have no special meaning 
+     * Set the list of synonyms, which are other names which have no special meaning
      * now or previously.
-     * 
+     *
      * @param synonyms
      */
     public void setSynonyms(List<String> synonyms) {
@@ -94,8 +94,8 @@ public class Names {
         return this.systematicId;
     }
     /**
-     * Store the feature's systematic id, and wether it's temporary
-     * 
+     * Store the feature's systematic id, and whether it's temporary
+     *
      * @param systematicId the systematic id
      * @param idTemporary true if the id is likely to change in the future
      */
@@ -107,13 +107,13 @@ public class Names {
         return this.previousSystematicIds;
     }
     /**
-     * Set a List of ids which have previously been used as systematic ids for 
+     * Set a List of ids which have previously been used as systematic ids for
      * this feature.
-     * 
+     *
      * @param previousSystematicIds
      */
     public void setPreviousSystematicIds(List<String> previousSystematicIds) {
         this.previousSystematicIds = previousSystematicIds;
     }
-    
+
 }
