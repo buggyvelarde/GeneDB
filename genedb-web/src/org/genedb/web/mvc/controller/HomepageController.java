97d8b05280a4dfa7da300ac83891f98ca59ee752
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-08-07 15:45:18 +0000

    Convert into a new style controller
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8498 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/HomepageController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/HomepageController.java
@@ -9,9 +9,13 @@ import org.genedb.db.taxon.TaxonNode;
 import org.genedb.db.taxon.TaxonNodeArrayPropertyEditor;
 
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -24,46 +28,48 @@ import javax.servlet.http.HttpServletResponse;
 
 
 /**
- * Controller for selecting and forwarding to a homepage view. It uses the one set 
- * as a phylonode property, otherwise a default one. An error, or no arguments, 
+ * Controller for selecting and forwarding to a homepage view. It uses the one set
+ * as a phylonode property, otherwise a default one. An error, or no arguments,
  * returns the default homepage.
  *
  * @author Adrian Tivey
  */
-public class HomepageController extends AbstractController {
-	
+@Controller
+@RequestMapping("/Homepage")
+public class HomepageController {
+
 	public static final String DUMMY_UNKNOWN = "XXX";
-	
+
     private static String HOMEPAGE = "homepages/";
     private static String DEFAULT_HOMEPAGE = HOMEPAGE + "malariaReleasePage";
     private static String DEFAULT_STYLE = "childListing"; // FIXME
-   
+
     private TaxonNodeArrayPropertyEditor taxonNodeArrayPropertyEditor;
-    
+
     protected final Logger logger = Logger.getLogger(this.getClass());
 
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
     	String originalOrg = ServletRequestUtils.getStringParameter(request, "organism", DUMMY_UNKNOWN);
-    	
+
 //        TaxonNodeArrayHolder tnah = new TaxonNodeArrayHolder();
-//        
+//
 //        ServletRequestDataBinder binder = new ServletRequestDataBinder(tnah);
 //        binder.initDirectFieldAccess();
 //        binder.registerCustomEditor(TaxonNode[].class, taxonNodeArrayPropertyEditor);
 //        binder.bind(request);
 //        BindingResult bindResult = binder.getBindingResult();
-        
+
         String organism = null;
         String pageName = null;
-        
+
 //        if (bindResult.hasGlobalErrors()) {
 //        	// TODO Add error message
 //            logger.debug("Binding errors - go home");
 //            return new ModelAndView(DEFAULT_HOMEPAGE);
 //        }
-        
+
 //        TaxonNode[] nodes = tnah.org;
 //        if (nodes == null || nodes.length == 0) {
 //            logger.debug("No taxon nodes - go home");
@@ -74,7 +80,7 @@ public class HomepageController extends AbstractController {
 //            logger.debug("Got too many taxon nodes");
 //            return new ModelAndView(DEFAULT_HOMEPAGE);
 //        }
-        
+
 //        TaxonNode node = nodes[0];
         String viewName = DEFAULT_HOMEPAGE;
         //String viewName = HOMEPAGE + DEFAULT_STYLE;
@@ -86,17 +92,17 @@ public class HomepageController extends AbstractController {
         //organism = node.getName(TaxonNameType.LABEL);
         //logger.warn(String.format("Retrieved an organism name of '%s'", pageName));
         }
-        	
+
 //        Map props = node.getAppDetails("WEB");
 //        if (props.containsKey("HOMEPAGE_STYLE")) {
 //            viewName = HOMEPAGE + props.get("HOMEPAGE_STYLE");
 //        }
-        
+
 //      List<NewsItem> news = checkNews();
 //      if (news.size() > 0) {
 //          model.put("news", news);
 //      }
-        
+
         ModelAndView mav = new ModelAndView(viewName);
         //mav.addObject(TAXON_NODE, node);
         mav.addObject(CRUMB, "Homepage");
@@ -108,7 +114,7 @@ public class HomepageController extends AbstractController {
         } else {
         	mav.addObject("pageName", "GeneDB");
         }
-        
+
         return mav;
     }
 
@@ -122,17 +128,17 @@ public class HomepageController extends AbstractController {
             TaxonNodeArrayPropertyEditor taxonNodeArrayPropertyEditor) {
         this.taxonNodeArrayPropertyEditor = taxonNodeArrayPropertyEditor;
     }
-    
+
 }
 
 
 /**
  * Simple struct to make databinding easier
- * 
+ *
  * @author A. Tivey (art)
  */
 class TaxonNodeArrayHolder {
-    
+
     /**
      * Field, directly accessed by databinder and for reading
      */
