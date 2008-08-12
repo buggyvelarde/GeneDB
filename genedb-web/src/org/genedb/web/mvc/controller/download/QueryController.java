75edbfc0e22429447ed8080a6c6512060b4146ab
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-08-12 09:51:49 +0000

    Need HttpServletRequest to access session easily
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8539 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/download/QueryController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/download/QueryController.java
@@ -4,6 +4,7 @@ import org.genedb.querying.core.LuceneQuery;
 import org.genedb.querying.core.Query;
 import org.genedb.querying.core.QueryException;
 import org.genedb.querying.core.QueryFactory;
+import org.genedb.web.mvc.controller.WebConstants;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.WebUtils;
 
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -38,35 +42,48 @@ public class QueryController {
     @RequestMapping(method = RequestMethod.GET)
     public String setUpForm(
             @RequestParam(value="q", required=false) String queryName,
+            HttpSession session,
             Model model) {
 
         if (!StringUtils.hasText(queryName)) {
+               session.setAttribute(WebConstants.FLASH_MSG, "Unable to identify which query to use");
             return "redirect:/QueryList";
         }
+
         Query query = queryFactory.retrieveQuery(queryName);
         if (query == null) {
-            return "redirect:/QueryList"; // FIXME - Send flash msg
+            session.setAttribute(WebConstants.FLASH_MSG, String.format("Unable to find query called '%s'", queryName));
+            return "redirect:/QueryList";
         }
+
         model.addAttribute("query", query);
+
         Map<String, Object> modelData = query.prepareModelData();
         for (Map.Entry<String, Object> entry : modelData.entrySet()) {
             model.addAttribute(entry.getKey(), entry.getValue());
         }
-        //return "search/query";
+
         return "search/"+queryName;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public ModelAndView processSubmit(
             @RequestParam(value="q", required=false) String queryName,
-            ServletRequest request
+            HttpServletRequest request,
+            HttpSession session
             ) throws QueryException {
 
         if (!StringUtils.hasText(queryName)) {
-            return new ModelAndView("redirect:/QueryList"); // FIXME - Send flash msg
+            session.setAttribute(WebConstants.FLASH_MSG, "Unable to identify which query to use");
+            return new ModelAndView("redirect:/QueryList");
         }
 
         Query query = queryFactory.retrieveQuery(queryName);
+        if (query == null) {
+            session.setAttribute(WebConstants.FLASH_MSG, String.format("Unable to find query called '%s'", queryName));
+            return new ModelAndView("redirect:/QueryList");
+        }
+
         ServletRequestDataBinder binder = new ServletRequestDataBinder(query);
         // register custom editors, if desired
         //binder.registerCustomEditor(...);
@@ -78,23 +95,24 @@ public class QueryController {
             // Problem
         }
 
-    	ModelAndView mav = null;
-    	List<String> results = query.getResults();
-    	switch (results.size()) {
-    	case 0:
-    	    logger.error("No results found for query");
-    	    mav = new ModelAndView("search/"+queryName);
-    	    break;
-    	case 1:
-    	    mav = new ModelAndView("redirect:/NamedFeature");
-    	    // TODO Send feature name
-    	    break;
-    	default:
-    	    mav = new ModelAndView("list/stupid");
+        ModelAndView mav = null;
+        List<String> results = query.getResults();
+
+        switch (results.size()) {
+        case 0:
+            logger.error("No results found for query");
+            mav = new ModelAndView("search/"+queryName);
+            break;
+        case 1:
+            mav = new ModelAndView("redirect:/NamedFeature");
+            // TODO Send feature name
+            break;
+        default:
+            mav = new ModelAndView("list/stupid");
             mav.addObject("results", results);
             break;
-    	}
-    	return mav;
+        }
+        return mav;
     }
 
 }
