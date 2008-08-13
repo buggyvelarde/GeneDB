6a238b30fa8a4e2e443d18fe0fcdd2b1d775906a
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-08-13 08:57:38 +0000

    FASTA files shouldn't have a space after the pointy bracket.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8555 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/download/DownloadUtils.java
+++ genedb-web/src/org/genedb/web/mvc/controller/download/DownloadUtils.java
@@ -1,22 +1,13 @@
 package org.genedb.web.mvc.controller.download;
 
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-
-import org.genedb.db.dao.SequenceDao;
-
 import org.gmod.schema.feature.AbstractExon;
 import org.gmod.schema.feature.AbstractGene;
-import org.gmod.schema.feature.Exon;
-import org.gmod.schema.feature.Gene;
-import org.gmod.schema.feature.MRNA;
 import org.gmod.schema.feature.ProductiveTranscript;
 import org.gmod.schema.feature.Transcript;
+
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Map;
 
 public class DownloadUtils {
 
@@ -29,7 +20,7 @@ public class DownloadUtils {
     public static void writeFasta(PrintWriter out, String header, String sequence) {
         out.print("> ");
         out.println(header);
-        
+
         int startPos = 0;
         int sequenceLen = sequence.length();
         while (startPos < sequenceLen) {
@@ -40,15 +31,15 @@ public class DownloadUtils {
             out.println(sequence.substring(startPos, endPos));
             startPos += FASTA_WIDTH;
         }
-        
+
     }
-    
+
    public static String writeFasta(String header, String sequence) {
-       StringBuilder fasta = new StringBuilder(); 
-       fasta.append("> ");
+       StringBuilder fasta = new StringBuilder();
+       fasta.append(">");
        fasta.append(header);
        fasta.append("\n");
-        
+
         int startPos = 0;
         int sequenceLen = sequence.length();
         while (startPos < sequenceLen) {
@@ -56,7 +47,7 @@ public class DownloadUtils {
             if (endPos > sequenceLen) {
                 endPos = sequenceLen;
             }
-            
+
             fasta.append(sequence.substring(startPos, endPos));
             fasta.append(" ");
             startPos += BASES_WIDTH;
@@ -64,72 +55,72 @@ public class DownloadUtils {
                 fasta.append("\n");
             }
         }
-        
+
         return fasta.toString();
     }
-    
-    public static void writeEmblEntry(PrintWriter out, String featureType, 
+
+    public static void writeEmblEntry(PrintWriter out, String featureType,
             boolean forwardStrand, int min, int max,
             Map<String, String> qualifiers) {
 
         if (featureType.length() > MAX_FEATURE_WIDTH) {
             featureType = featureType.substring(0, MAX_FEATURE_WIDTH);
         }
-        
+
         out.format("FT %-"+(FEATURE_PREFIX_WIDTH-3)+"s", featureType);
         if (!forwardStrand) {
             out.print("complement(");
         }
-        
+
         out.print(min - 1 +".."+max); // Interbase conversion
-        
+
         if (!forwardStrand) {
             out.print(")");
         }
         out.println();
-        
+
         for (Map.Entry<String, String> qualifier: qualifiers.entrySet()) {
             out.println(FEATURE_TABLE_PREFIX+"/"+qualifier.getKey()+"=\""+qualifier.getValue()+"\"");
         }
-        
+
     }
-    
-    public static String writeEmblEntry(String featureType, 
+
+    public static String writeEmblEntry(String featureType,
             boolean forwardStrand, int min, int max,
             Map<String, String> qualifiers) {
 
         StringBuilder embl = new StringBuilder();
-        
+
         if (featureType.length() > MAX_FEATURE_WIDTH) {
             featureType = featureType.substring(0, MAX_FEATURE_WIDTH);
         }
-        
+
         embl.append(String.format("FT %-"+(FEATURE_PREFIX_WIDTH-3)+"s", featureType));
         if (!forwardStrand) {
             embl.append("complement(");
         }
-        
+
         embl.append(min - 1 +".."+max); // Interbase conversion
-        
+
         if (!forwardStrand) {
             embl.append(")");
         }
         embl.append("\n");
-        
+
         for (Map.Entry<String, String> qualifier: qualifiers.entrySet()) {
             embl.append(FEATURE_TABLE_PREFIX+"/"+qualifier.getKey()+"=\""+qualifier.getValue()+"\"");
             embl.append("\n");
         }
-     
+
         return embl.toString();
     }
-    
+
     public static String getSequence(AbstractGene gene,SequenceType sequenceType) {
         String sequence = null;
         boolean alternateSpliced = false;
         Collection<Transcript> transcripts = gene.getTranscripts();
         ProductiveTranscript transcript = null;
-        
+
         if(transcripts.size() > 1 ) {
             alternateSpliced = true;
         } else {
@@ -138,7 +129,7 @@ public class DownloadUtils {
                transcript = (ProductiveTranscript) t;
            }
         }
-        
+
         if(!alternateSpliced && transcript!=null) {
             switch (sequenceType) {
                 case SPLICED_DNA:
@@ -166,13 +157,13 @@ public class DownloadUtils {
                             int stop = exon.getStop();
                             String str = new String(gene.getRankZeroFeatureLoc().getSourceFeature()
                                     .getResidues(start, stop));
-                            sequence = sequence.concat(str);   
+                            sequence = sequence.concat(str);
                         }
                     }
                     break;
             }
         }
-        
+
         return sequence;
     }
 }
