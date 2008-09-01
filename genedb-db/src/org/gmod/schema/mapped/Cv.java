9f05e931dc6610421a68b075b5a9da7b042f38ef
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-09-01 16:37:25 +0000

    Add equals and hashcode methods
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8662 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/mapped/Cv.java
+++ genedb-db/src/org/gmod/schema/mapped/Cv.java
@@ -79,4 +79,34 @@ public class Cv implements Serializable {
         cvTerm.setCv(this);
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!getClass().isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+        Cv other = (Cv) obj;
+        if (name == null) {
+            if (other.getName() != null) {
+                return false;
+            }
+        } else if (!name.equals(other.getName())) {
+            return false;
+        }
+        return true;
+    }
+
 }
