c2605845d9124c0de71f6d2503905b4b233f9a04
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-09-04 12:38:09 +0000

    Method docs.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8716 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-util/src/org/genedb/util/TwoKeyMap.java
+++ genedb-util/src/org/genedb/util/TwoKeyMap.java
@@ -9,10 +9,21 @@ import java.util.Set;
  *
  * @author rh11
  *
+ * @param <S> type of the first key
+ * @param <T> type of the second key
+ * @param <V> type of values
  */
 public class TwoKeyMap<S,T,V> {
     private Map<S,Map<T,V>> map = new HashMap<S,Map<T,V>>();
 
+    /**
+     * Get the element whose first key is <code>key1</code>
+     * and second key is <code>key2</code>.
+     *
+     * @param key1 the first key
+     * @param key2 the second key
+     * @return the element with these keys, or <code>null</code> if there is no such element.
+     */
     public V get(S key1, T key2) {
         if (!map.containsKey(key1)) {
             return null;
@@ -20,6 +31,14 @@ public class TwoKeyMap<S,T,V> {
         return map.get(key1).get(key2);
     }
 
+    /**
+     * Add a new element to the map, or replace an existing one.
+     *
+     * @param key1 the first key
+     * @param key2 the second key
+     * @param value the value to store
+     * @return the previous value, if any; otherwise <code>null</code>
+     */
     public V put(S key1, T key2, V value) {
         if (!map.containsKey(key1)) {
             map.put(key1, new HashMap<T,V>());
@@ -39,18 +58,45 @@ public class TwoKeyMap<S,T,V> {
         }
     }
 
+    /**
+     * Does this map have an entry whose first key is <code>key1</code>?
+     *
+     * @param key1 the first key to look for
+     * @return <code>true</code> if there is an entry whose first key is <code>key1</code>,
+     *          and <code>false</code> if there isn't
+     */
     public boolean containsFirstKey(S key1) {
         return map.containsKey(key1);
     }
 
+    /**
+     * Does this map have an entry whose first key is <code>key1</code>
+     * and second key is <code>key2</code>?
+     *
+     * @param key1 the first key
+     * @param key2 the second key
+     * @return<code>true</code> if there is an entry whose keys are <code>key1</code>
+     *          and <code>key2</code>, or <code>false</code> if there isn't
+     */
     public boolean containsKey(S key1, T key2) {
         return map.containsKey(key1) && map.get(key1).containsKey(key2);
     }
 
+    /**
+     * Delete all entries from this map.
+     */
     public void clear() {
         map.clear();
     }
 
+    /**
+     * Remove a single entry from the map.
+     *
+     * @param key1 the first key
+     * @param key2 the second key
+     * @return the previous value of the entry with keys <code>key1</code> and <code>key2</code>,
+     *          or <code>null</code> if there was no such entry
+     */
     public V remove(S key1, T key2) {
         if (!map.containsKey(key1)) {
             return null;
@@ -58,10 +104,21 @@ public class TwoKeyMap<S,T,V> {
         return map.get(key1).remove(key2);
     }
 
+    /**
+     * Return a set of all the first keys in use in this map.
+     *
+     * @return
+     */
     public Set<S> firstKeySet() {
         return map.keySet();
     }
 
+    /**
+     * For a given first key, return a mapping of second key to values.
+     *
+     * @param firstKey
+     * @return
+     */
     public Map<T,V> getMap(S firstKey) {
         return map.get(firstKey);
     }
