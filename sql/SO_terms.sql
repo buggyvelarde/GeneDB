e253792fba11ac2c32454cf61543e40e84c4cfc6
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-09-11 12:19:22 +0000

    A couple more useful queries.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8778 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/SO_terms.sql
+++ sql/SO_terms.sql
@@ -13,3 +13,20 @@ join dbxref on cvterm.dbxref_id = dbxref.dbxref_id
 where cvterm.cv_id = 10 /* sequence ontology CV */
 group by cvterm.name, dbxref.accession
 ;
+
+select cv.name, cvterm.name, dbxref.accession, count(*)
+from featureprop
+join cvterm on featureprop.type_id = cvterm.cvterm_id
+join cv using (cv_id)
+join dbxref on cvterm.dbxref_id = dbxref.dbxref_id
+group by cv.name, cvterm.name, dbxref.accession
+;
+
+select cv.name, cvterm.name, dbxref.accession, count(*)
+from feature_cvterm
+join cvterm on feature_cvterm.cvterm_id = cvterm.cvterm_id
+join cv using (cv_id)
+join dbxref on cvterm.dbxref_id = dbxref.dbxref_id
+where cvterm.cv_id = 10 /* sequence ontology CV */
+group by cv.name, cvterm.name, dbxref.accession
+;
