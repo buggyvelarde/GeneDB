577a27636f4824c260733f9c44dd6b0d3fb3c11d
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-09-15 14:32:44 +0000

    Add Set specific safe getter
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8827 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/utils/CollectionUtils.java
+++ genedb-db/src/org/gmod/schema/utils/CollectionUtils.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Static utility class for miscellaneous collection handling
- * 
+ *
  * @author art
  */
 public class CollectionUtils {
@@ -15,9 +16,9 @@ public class CollectionUtils {
     /**
      * Returns its argument if not null, or a new empty collection (in fact a HashSet<T>)
      * if the given one is null.
-     * 
-     * @param <T> 
-     * @param collection 
+     *
+     * @param <T>
+     * @param collection
      * @return the original collection, or an empty collection
      */
     public static <T> Collection<T> safeGetter(Collection<T> collection) {
@@ -27,12 +28,28 @@ public class CollectionUtils {
         return new HashSet<T>();
     }
 
+
+    /**
+     * Returns its argument if not null, or a new empty collection (in fact a HashSet<T>)
+     * if the given one is null.
+     *
+     * @param <T>
+     * @param collection
+     * @return the original collection, or an empty collection
+     */
+    public static <T> Set<T> safeGetter(Set<T> collection) {
+        if (collection != null) {
+            return collection;
+        }
+        return new HashSet<T>();
+    }
+
     /**
      * Returns its argument if not null, or a new empty collection (in fact an ArrayList<T>)
      * if the given one is null.
-     * 
-     * @param <T> 
-     * @param collection 
+     *
+     * @param <T>
+     * @param collection
      * @return the original collection, or an empty collection
      */
    public static <T> List<T> safeGetter(List<T> list) {
