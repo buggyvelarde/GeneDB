a627909a01fbbb15d8114978cd05db52afc4370e
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-10-02 14:15:35 +0000

    Add getParts() method to Match, which returns all the MatchParts of that match.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@8927 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/feature/Match.java
+++ genedb-db/src/org/gmod/schema/feature/Match.java
@@ -1,8 +1,40 @@
 package org.gmod.schema.feature;
 
+import org.gmod.schema.mapped.Feature;
+import org.gmod.schema.mapped.FeatureRelationship;
+import org.gmod.schema.mapped.Organism;
+
+import java.sql.Timestamp;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.persistence.Transient;
+
 
 public abstract class Match extends Region {
     Match() {
         // empty
+    }
+
+    public Match(Organism organism, String uniqueName) {
+        this(organism, uniqueName, true, false);
+    }
+
+    public Match(Organism organism, String uniqueName, boolean analysis, boolean obsolete) {
+        super(organism, uniqueName, analysis, obsolete, new Timestamp(System.currentTimeMillis()));
+    }
+
+    @Transient
+    public Iterable<MatchPart> getParts() {
+        SortedSet<MatchPart> parts = new TreeSet<MatchPart>();
+
+        for (FeatureRelationship relation : getFeatureRelationshipsForObjectId()) {
+            Feature feature = relation.getSubjectFeature();
+            if (feature instanceof MatchPart) {
+                parts.add((MatchPart) feature);
+            }
+        }
+
+        return parts;
     }
 }
