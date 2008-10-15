0f24146ee6ed70c78e4b72190ab99eb643a20104
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-10-15 16:31:01 +0000

    Change to new style controller
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9040 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/download/QueryController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/download/QueryController.java
@@ -1,10 +1,12 @@
 package org.genedb.web.mvc.controller.download;
 
-import org.genedb.querying.core.LuceneQuery;
 import org.genedb.querying.core.Query;
 import org.genedb.querying.core.QueryException;
 import org.genedb.querying.core.QueryFactory;
+import org.genedb.querying.tmpquery.BrowseCategory;
+import org.genedb.web.mvc.controller.BrowseCategoryController;
 import org.genedb.web.mvc.controller.WebConstants;
+import org.genedb.web.mvc.controller.BrowseCategoryController.BrowseCategoryBean;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +15,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.util.WebUtils;
 
-import java.text.DateFormat;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 
@@ -42,10 +41,14 @@ public class QueryController {
     private QueryFactory queryFactory;
 
 
-
     @RequestMapping(method = RequestMethod.GET)
+    public String setUpForm() {
+        return "redirect:/QueryList";
+    }
+
+    @RequestMapping(method = RequestMethod.GET , params= "q")
     public String setUpForm(
-            @RequestParam(value="q", required=false) String queryName,
+            @RequestParam(value="q") String queryName,
             ServletRequest request,
             HttpSession session,
             Model model) throws QueryException {
@@ -65,15 +68,13 @@ public class QueryController {
         logger.error(String.format("The number of parameters is '%d'", request.getParameterMap().keySet().size()));
         if (request.getParameterMap().keySet().size() == 1) {
             // That'll be q so the user has only chosen the query so far, not any values
-
-
             Map<String, Object> modelData = query.prepareModelData();
             for (Map.Entry<String, Object> entry : modelData.entrySet()) {
                 model.addAttribute(entry.getKey(), entry.getValue());
             }
-
             return "search/"+queryName;
         }
+
 
         // Attempt to fill in form
         ServletRequestDataBinder binder = new ServletRequestDataBinder(query);
@@ -82,6 +83,8 @@ public class QueryController {
         binder.bind(request);
         // optionally evaluate binding errors
         Errors errors = binder.getBindingResult();
+        Validator validator = query.getValidator();
+        validator.validate(query, errors);
 
         if (errors.getErrorCount() != 0) {
             // Problem
