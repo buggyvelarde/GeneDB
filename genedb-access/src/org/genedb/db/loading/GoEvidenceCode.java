c3a567fad6b514936b78676d8d110dbb57e3b5e8
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-11-05 16:08:08 +0000

    Refactor to use standard enum attribute technique
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9209 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/GoEvidenceCode.java
+++ genedb-access/src/org/genedb/db/loading/GoEvidenceCode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002 Genome Research Limited.
+ * Copyright (c) 2002-2008 Genome Research Limited.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Library General Public License as published
@@ -19,49 +19,35 @@
 
 package org.genedb.db.loading;
 
-import java.util.EnumMap;
 
 
 /**
- * This class represents a specific GO entry 
+ * This class represents a specific GO entry
  *
  * @author <a href="mailto:art@sanger.ac.uk">Adrian Tivey</a>
  */
 public enum GoEvidenceCode {
 
-    IC, IDA, IEA, IEP, IGI, IMP, IPI, ISS, NAS, ND, TAS, NR, RCA;
-    
-    static EnumMap<GoEvidenceCode, String> descriptions = new EnumMap<GoEvidenceCode, String>(GoEvidenceCode.class);
+    IC  ("Inferred by Curator"),
+    IDA ("Inferred from Direct Assay"),
+    IEA ("Inferred from Electronic Annotation"),
+    IEP ("Inferred from Expression Pattern"),
+    IGI ("Inferred from Genetic Interaction"),
+    IMP ("Inferred from Mutant Phenotype"),
+    IPI ("Inferred from Physical Interaction"),
+    ISS ("Inferred from Sequence or Structural Similarity"),
+    NAS ("Non-traceable Author Statement"),
+    ND  ("No biological Data available"),
+    TAS ("Traceable Author Statement"),
+    NR  ("Not Recorded"),
+    RCA ("inferred from Reviewed Computational Analysis");
 
-    static {
-        descriptions.put(GoEvidenceCode.IC,"Inferred by Curator");
-        descriptions.put(GoEvidenceCode.IDA,"Inferred from Direct Assay");
-        descriptions.put(GoEvidenceCode.IEA,"Inferred from Electronic Annotation");
-        descriptions.put(GoEvidenceCode.IEP,"Inferred from Expression Pattern");
-        descriptions.put(GoEvidenceCode.IGI,"Inferred from Genetic Interaction");
-        descriptions.put(GoEvidenceCode.IMP,"Inferred from Mutant Phenotype");
-        descriptions.put(GoEvidenceCode.IPI,"Inferred from Physical Interaction");
-        descriptions.put(GoEvidenceCode.ISS,"Inferred from Sequence or Structural Similarity");
-        descriptions.put(GoEvidenceCode.NAS,"Non-traceable Author Statement");
-        descriptions.put(GoEvidenceCode.ND,"No biological Data available");
-        descriptions.put(GoEvidenceCode.TAS,"Traceable Author Statement");
-        descriptions.put(GoEvidenceCode.NR,"Not Recorded");
-        descriptions.put(GoEvidenceCode.RCA, "inferred from Reviewed Computational Analysis");
+    private String description;
+    private GoEvidenceCode(String description) {
+        this.description = description;
     }
 
     public String getDescription() {
-    return descriptions.get(this);
+        return description;
     }
-
-
-//    /**
-//     * @return
-//     */
-//    public boolean isWith() {
-//        if (EV_IC.equals(getEvidence())) {
-//            return false;
-//        }
-//        return true;
-//    }
-
 }
