4b28f4174fefd1f1464d459987128951bedeb89f
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-11-13 16:47:30 +0000

    New ProteinMatch constructor.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9335 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/feature/ProteinMatch.java
+++ genedb-db/src/org/gmod/schema/feature/ProteinMatch.java
@@ -1,6 +1,7 @@
 package org.gmod.schema.feature;
 
 import org.gmod.schema.cfg.FeatureType;
+import org.gmod.schema.mapped.Organism;
 
 import javax.persistence.Entity;
 
@@ -9,5 +10,13 @@ import javax.persistence.Entity;
 public class ProteinMatch extends Match {
     ProteinMatch() {
         // empty
+    }
+
+    public ProteinMatch(Organism organism, String uniqueName, boolean analysis, boolean obsolete) {
+        super(organism, uniqueName, analysis, obsolete);
+    }
+
+    public ProteinMatch(Organism organism, String uniqueName) {
+        super(organism, uniqueName);
     }
 }
