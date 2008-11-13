8d12c3e72a6ba28a097a7410d9c72f55c60ab79b
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-11-13 17:34:44 +0000

    Continue OrthoMCL work
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9343 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- Mars/src/org/genedb/pipeline/domain/OrthoMCL.java
+++ Mars/src/org/genedb/pipeline/domain/OrthoMCL.java
@@ -1,10 +1,24 @@
 package org.genedb.pipeline.domain;
 
 public class OrthoMCL {
-	
-	private String targetId;
+
+	private String clusterId;
 	private String[] orthologueIds;
-	
-	
+
+
+
+    public OrthoMCL(String clusterId, String[] orthologueIds) {
+        this.clusterId = clusterId;
+        this.orthologueIds = orthologueIds;
+    }
+
+    public String getClusterId() {
+        return clusterId;
+    }
+    public String[] getOrthologueIds() {
+        return orthologueIds;
+    }
+
+
 
 }
