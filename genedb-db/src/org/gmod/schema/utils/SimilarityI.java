f3fcf4cf756ef89d5dbbaef94e8290cfc3c544fc
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-11-19 16:11:58 +0000

    Explanatory doc comment.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9397 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/utils/SimilarityI.java
+++ genedb-db/src/org/gmod/schema/utils/SimilarityI.java
@@ -5,6 +5,18 @@ import org.gmod.schema.mapped.DbXRef;
 
 import java.util.Collection;
 
+/**
+ * A SimilarityI object represents a similarity (as defined by a /similarity
+ * qualifier in an EMBL file. The method {@link Feature#addSimilarity(SimilarityI)}
+ * will add a similarity to the database.
+ * <p>
+ * The only implementation of this interface is {@link org.genedb.db.loading.Similarity}.
+ * The interface is defined here in genedb-db in order that we don't create a dependency
+ * of genedb-db on genedb-access.
+ *
+ * @author rh11
+ *
+ */
 public interface SimilarityI {
 
     public abstract String getAnalysisProgram();
