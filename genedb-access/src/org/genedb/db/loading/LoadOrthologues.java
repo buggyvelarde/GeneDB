b0754caea33aeb813a2198df254f8d4446956ed0
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-11-25 10:00:29 +0000

    Improvements to orthologue loader. The input file format has changed slightly, and algorithmically-determined orthologues are now always stored as clusters.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9426 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/LoadOrthologues.java
+++ genedb-access/src/org/genedb/db/loading/LoadOrthologues.java
@@ -1,14 +1,22 @@
 package org.genedb.db.loading;
 
+import org.genedb.db.dao.OrganismDao;
 import org.genedb.db.dao.SequenceDao;
 
+import org.gmod.schema.feature.AbstractGene;
 import org.gmod.schema.feature.Polypeptide;
 import org.gmod.schema.feature.ProductiveTranscript;
+import org.gmod.schema.feature.ProteinMatch;
 import org.gmod.schema.feature.Transcript;
+import org.gmod.schema.mapped.Analysis;
+import org.gmod.schema.mapped.Organism;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.BufferedReader;
@@ -17,19 +25,33 @@ import java.io.IOException;
 import java.io.Reader;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Load orthologue data from a simple tab-separated file.
- * Each line of the file has five fields:
+ * Each line of the file has four or five fields:
  * <ol>
  *   <li> Source organism,
  *   <li> source feature,
  *   <li> target organism,
  *   <li> target feature,
- *   <li> cluster name (may be missing or empty for unclustered orthologues)
+ *   <li> the percentage identity (optional)
  * </ol>
  * The source and target feature fields should contain the uniqueName of a transcript.
+ * For orthologue clusters, the target organism field should contain the word "cluster"
+ * and the
+ * <p>
+ * Details of the analysis should be specified as properties:
+ * <dl>
+ * <dt><code>load.analysis.program</code></dt><dd>The program used to perform the analysis (Required)</dd>
+ * <dt><code>load.analysis.programVersion</code></dt><dd>The version of the program that was used (Defaults to "unknown")</dd>
+ * <dt><code>load.analysis.algorithm</code></dt><dd>The name of the algorithm (Optional)</dd>
+ * </dl>
+ * If no analysis program is specified, it is assumed that the file contains manually curated orthologues.
  *
  * @author rh11
  *
@@ -43,7 +65,27 @@ public class LoadOrthologues extends FileProcessor {
         String inputDirectory = getRequiredProperty("load.inputDirectory");
         String fileNamePattern = getPropertyWithDefault("load.fileNamePattern", ".*\\.ortho");
 
-        new LoadOrthologues().processFileOrDirectory(inputDirectory, fileNamePattern);
+        String analysisProgram = getPropertyWithDefault("load.analysis.program", null);
+        String analysisProgramVersion = getPropertyWithDefault("load.analysis.programVersion", null);
+        String analysisAlgorithm = getPropertyWithDefault("load.analysis.algorithm", null);
+
+        boolean geneNames = hasProperty("load.orthologues.geneNames");
+
+        if (analysisProgram == null) {
+            if (analysisProgramVersion != null) {
+                throw new IllegalArgumentException("load.analysis.programVersion is specified, but load.analysis.program is not");
+            }
+            if (analysisAlgorithm != null) {
+                throw new IllegalArgumentException("load.analysis.algorithm is specified, but load.analysis.program is not");
+            }
+        } else {
+            analysisProgramVersion = "unknown";
+        }
+
+        LoadOrthologues loadOrthologues = new LoadOrthologues();
+        loadOrthologues.setAnalysisProperties(analysisProgram, analysisProgramVersion, analysisAlgorithm);
+        loadOrthologues.setGeneNames(geneNames);
+        loadOrthologues.processFileOrDirectory(inputDirectory, fileNamePattern);
     }
 
     private OrthologuesLoader loader;
@@ -51,6 +93,14 @@ public class LoadOrthologues extends FileProcessor {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"Load.xml"});
 
         this.loader = (OrthologuesLoader) applicationContext.getBean("orthologuesLoader", OrthologuesLoader.class);
+    }
+
+    private void setAnalysisProperties(String analysisProgram, String analysisProgramVersion, String analysisAlgorithm) {
+        loader.setAnalysisProperties(analysisProgram, analysisProgramVersion, analysisAlgorithm);
+    }
+
+    private void setGeneNames(boolean geneNames) {
+        loader.setGeneNames(geneNames);
     }
 
     @Override
@@ -67,14 +117,14 @@ class OrthologueFile {
         private String sourceFeature;
         private String targetOrganism;
         private String targetFeature;
-        private String cluster;
+        private Double identity = null;
 
         private Line(int lineNumber, String line) throws ParsingException {
             this.lineNumber = lineNumber;
 
             String[] fields = line.split("\\t");
             if (fields.length != 4 && fields.length != 5) {
-                throw new SyntaxError(file.toString(), lineNumber,
+                throw new SyntaxError(file, lineNumber,
                     String.format("Wrong number of fields (%d)", fields.length));
             }
 
@@ -83,8 +133,17 @@ class OrthologueFile {
             targetOrganism = fields[2];
             targetFeature  = fields[3];
 
-            if (fields.length == 5 && fields[4].length() > 0) {
-                cluster = fields[4];
+            if (fields.length > 4 && fields[4].length() > 0) {
+                try {
+                    identity = Double.parseDouble(fields[4]);
+                } catch (NumberFormatException e) {
+                    throw new SyntaxError(file, lineNumber,
+                        String.format("Could not parse identity field '%s'", fields[4]));
+                }
+                if (identity < 0 || identity > 100) {
+                    throw new DataError(file, lineNumber,
+                        String.format("Value of identity field '%s' is out of range", fields[4]));
+                }
             }
         }
 
@@ -102,10 +161,6 @@ class OrthologueFile {
 
         String getTargetFeature() {
             return targetFeature;
-        }
-
-        String getCluster() {
-            return cluster;
         }
     }
     private File file;
@@ -126,89 +181,220 @@ class OrthologueFile {
         return file;
     }
 
-    public Iterable<Line> lines() {
+    public List<Line> lines() {
         return lines;
+    }
+
+    public int numberOfLines() {
+        return lines.size();
     }
 }
 
-@Transactional(rollbackFor=DataError.class) // Will also rollback for runtime exceptions, by default
+//@Transactional(rollbackFor=DataError.class) // Will also rollback for runtime exceptions, by default
 class OrthologuesLoader {
     private static final Logger logger = Logger.getLogger(OrthologuesLoader.class);
 
+    private static final int BATCH_SIZE = 20;
+
     private SequenceDao sequenceDao;
+    private OrganismDao organismDao;
+    private SessionFactory sessionFactory;
+
+    private Organism dummyOrganism;
+    private Analysis analysis = null;
+    private boolean geneNames = false;
+
+    public void setAnalysisProperties(String analysisProgram, String analysisProgramVersion, String analysisAlgorithm) {
+        if (analysisProgram == null) {
+            return;
+        }
+
+        analysis = new Analysis();
+        analysis.setProgram(analysisProgram);
+        analysis.setProgramVersion(analysisProgramVersion);
+        analysis.setAlgorithm(analysisAlgorithm);
+    }
+
+    /**
+     * If the geneNames property is set, then the identifiers in the file
+     * are assumed to refer to genes rather than transcripts. This only
+     * makes sense for bacteria, where the gene name uniquely identifies
+     * a transcript.
+     *
+     * @param geneNames
+     */
+    void setGeneNames(boolean geneNames) {
+        this.geneNames = geneNames;
+    }
 
     public void load(OrthologueFile orthologueFile) throws DataError {
         /*
-         * Thanks to the @Transactional annotation on this class,
-         * Spring will automatically initiate a transaction when
-         * we're called, which will be committed on successful
-         * return or rolled back if we throw an exception.
+         * Note that this method is not @Transactional. We do all our
+         * work by calling transactional methods. This technique relies
+         * on bytecode instrumentation: it won't work with proxies.
          */
 
-        for (OrthologueFile.Line line: orthologueFile.lines()) {
-            logger.trace(String.format("%s:%s -> %s:%s (cluster %s)",
-                line.getSourceOrganism(), line.getSourceFeature(),
-                line.getTargetOrganism(), line.getTargetFeature(),
-                line.getCluster()));
+        if (analysis != null) {
+            persistAnalysis();
+        }
 
-            Transcript source = getTranscript(line.getSourceOrganism(), line.getSourceFeature(),
+        Map<String,Collection<Polypeptide>> clustersByName = new HashMap<String,Collection<Polypeptide>>();
+
+        /* If we don't split the load into a number of smaller transactions,
+         * it becomes impossibly slow after a while.
+         */
+        int numberOfLines = orthologueFile.numberOfLines();
+        for (int i=1; i < numberOfLines; i += BATCH_SIZE) {
+            int endIndex = i + BATCH_SIZE;
+            if (endIndex > numberOfLines) {
+                endIndex = numberOfLines;
+            }
+            processLines(orthologueFile, clustersByName,
+                orthologueFile.lines().subList(i, endIndex));
+        }
+
+        loadClusters(clustersByName);
+    }
+
+    @Transactional
+    private void persistAnalysis() {
+        SessionFactoryUtils.getSession(sessionFactory, false).persist(analysis);
+    }
+
+    @Transactional
+    private void processLines(OrthologueFile orthologueFile,
+            Map<String, Collection<Polypeptide>> clustersByName,
+            Collection<OrthologueFile.Line> lines) throws DataError
+    {
+        for (OrthologueFile.Line line: lines) {
+            logger.trace(String.format("[%d/%d] %s:%s -> %s:%s",
+                line.lineNumber, orthologueFile.numberOfLines(),
+                line.getSourceOrganism(), line.getSourceFeature(),
+                line.getTargetOrganism(), line.getTargetFeature()));
+
+            Polypeptide source = getPolypeptide(line.getSourceOrganism(), line.getSourceFeature(),
                 orthologueFile.file(), line.lineNumber);
-            Transcript target = getTranscript(line.getTargetOrganism(), line.getTargetFeature(),
-                orthologueFile.file(), line.lineNumber);
-            String cluster = line.getCluster();
-            if (cluster == null) {
-                addOrthologue(source, target, orthologueFile.file(), line.lineNumber);
+            if (source == null) {
+                // A return value of null indicates a non-fatal error
+                continue;
+            }
+
+            String targetOrganismName = line.getTargetOrganism();
+            if (targetOrganismName.equalsIgnoreCase("cluster")) {
+                String cluster = line.getTargetFeature();
+                if (!clustersByName.containsKey(cluster)) {
+                    clustersByName.put(cluster, new ArrayList<Polypeptide>());
+                }
+                clustersByName.get(cluster).add(source);
             } else {
-                throw new RuntimeException("Clusters not yet supported"); // TODO
+                // Unclustered
+                Polypeptide target = getPolypeptide(line.getTargetOrganism(), line.getTargetFeature(),
+                    orthologueFile.file(), line.lineNumber);
+                addOrthologue(source, target);
             }
         }
     }
 
-    private Transcript getTranscript(String organism, String uniqueName, File file, int lineNumber) throws DataError {
-        Transcript transcript = sequenceDao.getFeatureByUniqueName(uniqueName, Transcript.class);
-        if (transcript == null) {
-            throw new DataError(file, lineNumber, String.format("Could not find transcript feature '%s'", uniqueName));
+    @Transactional
+    private void loadClusters(Map<String, Collection<Polypeptide>> clustersByName) {
+        for (Map.Entry<String, Collection<Polypeptide>> entry: clustersByName.entrySet()) {
+            String clusterName = entry.getKey();
+            Collection<Polypeptide> transcripts = entry.getValue();
+
+            loadCluster(clusterName, transcripts);
         }
+    }
+
+    @Transactional
+    private Polypeptide getPolypeptide(String organism, String uniqueName, File file, int lineNumber) throws DataError {
+        Transcript transcript;
+
+        if (geneNames) {
+            AbstractGene gene = sequenceDao.getFeatureByUniqueName(uniqueName, AbstractGene.class);
+            Collection<Transcript> transcripts = gene.getTranscripts();
+            if (transcripts.isEmpty()) {
+                logger.error(String.format("The gene '%s' does not have any transcripts", uniqueName));
+                return null;
+            }
+            if (transcripts.size() > 1) {
+                logger.error(String.format("The gene '%s' has %d transcripts", uniqueName, transcripts.size()));
+                return null;
+            }
+            transcript = transcripts.iterator().next();
+        } else {
+            transcript = sequenceDao.getFeatureByUniqueName(uniqueName, Transcript.class);
+            if (transcript == null) {
+                throw new DataError(file, lineNumber, String.format("Could not find transcript feature '%s'", uniqueName));
+            }
+        }
+
         if (!transcript.getOrganism().getCommonName().equals(organism)) {
             throw new DataError(file, lineNumber,
                 String.format("The feature '%s' does not belong to organism '%s'",
-                    uniqueName, organism));
+                    transcript.getUniqueName(), organism));
         }
-        return transcript;
+
+        if (!(transcript instanceof ProductiveTranscript)) {
+            logger.error(String.format("%s line %d: The transcript (%s) is non-coding. " +
+                    "We can't currently store orthologues for non-coding transcripts",
+                file, lineNumber, transcript.getUniqueName()));
+            return null;
+        }
+        Polypeptide polypeptide = ((ProductiveTranscript) transcript).getProtein();
+        if (polypeptide == null) {
+            logger.error(String.format("%s line %d: The transcript (%s) has no associated polypeptide. Ignoring entry",
+                file, lineNumber, transcript.getUniqueName()));
+            return null;
+        }
+
+        return polypeptide;
     }
 
-    private void addOrthologue(Transcript source, Transcript target, File file, int lineNumber) {
-        if (!(source instanceof ProductiveTranscript)) {
-            logger.error(String.format("%s line %d: The source transcript (%s) is non-coding. " +
-                    "We can't currently store orthologues for non-coding transcripts",
-                file, lineNumber, source.getUniqueName()));
-            return;
+    @Transactional
+    private void addOrthologue(Polypeptide sourcePolypeptide, Polypeptide targetPolypeptide) {
+        if (this.analysis != null) {
+            // Since this is an algorithmically-derived orthologue, we add it as a cluster
+            String clusterName = String.format("cluster %s -> %s",
+                sourcePolypeptide.getUniqueName(), targetPolypeptide.getUniqueName());
+            List<Polypeptide> polypeptides = new ArrayList<Polypeptide>(2);
+            Collections.addAll(polypeptides, sourcePolypeptide, targetPolypeptide);
+            loadCluster(clusterName, polypeptides);
+        } else {
+            // Manually-curated orthologue. Add a simple orthologous_to relationship in both directions.
+            sourcePolypeptide.addOrthologue(targetPolypeptide);
+            targetPolypeptide.addOrthologue(sourcePolypeptide);
         }
-        Polypeptide sourcePolypeptide = ((ProductiveTranscript) source).getProtein();
-        if (sourcePolypeptide == null) {
-            logger.error(String.format("%s line %d: The source transcript (%s) has no associated polypeptide. Ignoring entry",
-                file, lineNumber, source.getUniqueName()));
-            return;
-        }
-
-        if (!(target instanceof ProductiveTranscript)) {
-            logger.error(String.format("%s line %d: The target transcript (%s) is non-coding. " +
-                    "We can't currently store orthologues for non-coding transcripts",
-                file, lineNumber, target.getUniqueName()));
-            return;
-        }
-        Polypeptide targetPolypeptide = ((ProductiveTranscript) target).getProtein();
-        if (targetPolypeptide == null) {
-            logger.error(String.format("%s line %d: The target transcript (%s) has no associated polypeptide. Ignoring entry",
-                file, lineNumber, target.getUniqueName()));
-            return;
-        }
-
-        targetPolypeptide.addOrthologue(sourcePolypeptide);
     }
 
+    @Transactional
+    private void loadCluster(String clusterName, Collection<Polypeptide> polypeptides) {
+        Session session = SessionFactoryUtils.getSession(sessionFactory, false);
+
+        logger.trace(String.format("Loading orthologue cluster '%s'", clusterName));
+        ProteinMatch clusterFeature = new ProteinMatch(dummyOrganism, clusterName, true, false);
+
+        for (Polypeptide polypeptide: polypeptides) {
+            clusterFeature.addOrthologue(polypeptide);
+        }
+
+        session.persist(clusterFeature);
+    }
+
+    /* Spring setters */
     public void setSequenceDao(SequenceDao sequenceDao) {
         this.sequenceDao = sequenceDao;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void setOrganismDao(OrganismDao organismDao) {
+        this.organismDao = organismDao;
+    }
+
+    public void afterPropertiesSet() {
+        dummyOrganism = organismDao.getOrganismByCommonName("dummy");
     }
 }
 
