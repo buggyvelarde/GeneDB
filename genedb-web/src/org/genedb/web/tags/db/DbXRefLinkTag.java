2c39ed4c32196bbccd2cf3d75fa1c2136b732f22
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-12-03 14:01:20 +0000

    GENEDB-168 Extend TranscriptDTO, add link tag
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9512 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/web/tags/db/DbXRefLinkTag.java
@@ -0,0 +1,39 @@
+package org.genedb.web.tags.db;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+
+
+
+public class DbXRefLinkTag extends SimpleTagSupport {
+
+    private String dbXRef;
+
+    public void setDbXref(String dbXRef) {
+        this.dbXRef = dbXRef;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        // TODO currently a no-op
+        // Should lookup URL
+        // different class for internal, external URL
+
+        String url = null;
+
+
+        JspWriter out = getJspContext().getOut();
+        if (url != null) {
+            out.write(String.format("<a href=\"%s>\">", url));
+        }
+        out.write(dbXRef);
+        if (url != null) {
+            out.write("</a>");
+        }
+    }
+
+}
