ce0d27ed5852bcf250546e886558f2c337bd41f0
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-12-10 11:18:58 +0000

    GENEDB-225 Merging changes via USB stick
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9571 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/ContextMapController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/ContextMapController.java
@@ -4,26 +4,31 @@ import org.genedb.db.domain.luceneImpls.BasicGeneServiceImpl;
 import org.genedb.db.domain.services.BasicGeneService;
 import org.genedb.querying.core.LuceneIndex;
 import org.genedb.querying.core.LuceneIndexFactory;
-import org.genedb.web.gui.DiagramCache;
 import org.genedb.web.mvc.model.BerkeleyMapFactory;
 
-import org.springframework.util.CollectionUtils;
-import org.springframework.validation.BindException;
-import org.springframework.web.servlet.ModelAndView;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.View;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.io.Writer;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ContextMapController extends PostOrGetFormController {
+@Controller
+@RequestMapping("/Context")
+public class ContextMapController {
     /*
      * More than 30,000 pixels per tile, and almost all browsers
      * fail. More than 5,000 or so, and Linux Firefox crashes.
      */
-    private static final int TILE_WIDTH = 5000; // in pixels
+    //private static final int TILE_WIDTH = 5000; // in pixels
+
+    private Logger logger = Logger.getLogger(this.getClass());
 
     //private DiagramCache fileDiagramCache;
     private LuceneIndexFactory luceneIndexFactory; // Injected by Spring
@@ -43,25 +48,21 @@ public class ContextMapController extends PostOrGetFormController {
         basicGeneService = new BasicGeneServiceImpl(luceneIndex);
     }
 
-    @Override
-    protected ModelAndView onSubmit(HttpServletRequest request,
-            HttpServletResponse response, Object rawCommand, BindException errors)
-            throws Exception {
+    @RequestMapping(method=RequestMethod.GET)
+    public void writeMapInfo(HttpServletRequest request, Writer out,
+            HttpServletResponse response, String chromosome) throws IOException {
 
-        Command command = (Command) rawCommand;
-
-        String text = bmf.getContextMapMap().get(command.getChromosome());
+        String text = bmf.getContextMapMap().get(chromosome);
         if (text != null) {
-            logger.trace("Cache hit for context map '"+command.getChromosome()+"' of '"+text+"'");
+            logger.trace("Cache hit for context map '"+chromosome+"' of '"+text+"'");
             cacheHit++;
         } else {
-            logger.error(String.format("The context maps for '%s' aren't cached and need to be generated", command.getChromosome()));
+            logger.error(String.format("The context maps for '%s' aren't cached and need to be generated", chromosome));
             //text = contextMapCache.getKeys().toString();
             cacheMiss++;
         }
 
-        response.getWriter().write(text);
-        return null;
+        out.write(text);
     }
 
     public LuceneIndexFactory getLuceneDao() {
@@ -80,14 +81,6 @@ public class ContextMapController extends PostOrGetFormController {
         this.view = view;
     }
 
-//    public void setDiagramCache(DiagramCache fileDiagramCache) {
-//        this.fileDiagramCache = fileDiagramCache;
-//    }
-//
-//    public void setContextMapCache(BlockingCache contextMapCache) {
-//        this.contextMapCache = contextMapCache;
-//    }
-
     public int getCacheHit() {
         return cacheHit;
     }
@@ -98,52 +91,52 @@ public class ContextMapController extends PostOrGetFormController {
 
 
 
-    public static class Command {
-        private String organism;
-        private String chromosome;
-        private int chromosomeLength;
-        private int thumbnailDisplayWidth;
-
-        public boolean hasRequiredData() {
-            return organism != null
-                && chromosome != null
-                && chromosomeLength > 0
-                && thumbnailDisplayWidth > 0;
-        }
-
-        // Getters and setters
-        public String getOrganism() {
-            return organism;
-        }
-
-        public void setOrganism(String organism) {
-            this.organism = organism;
-        }
-
-        public String getChromosome() {
-            return chromosome;
-        }
-
-        public void setChromosome(String chromosome) {
-            this.chromosome = chromosome;
-        }
-
-        public int getChromosomeLength() {
-            return chromosomeLength;
-        }
-
-        public void setChromosomeLength(int chromosomeLength) {
-            this.chromosomeLength = chromosomeLength;
-        }
-
-        public int getThumbnailDisplayWidth() {
-            return thumbnailDisplayWidth;
-        }
-
-        public void setThumbnailDisplayWidth(int displayWidth) {
-            this.thumbnailDisplayWidth = displayWidth;
-        }
-    }
+//    public static class Command {
+//        private String organism;
+//        private String chromosome;
+//        private int chromosomeLength;
+//        private int thumbnailDisplayWidth;
+//
+//        public boolean hasRequiredData() {
+//            return organism != null
+//                && chromosome != null
+//                && chromosomeLength > 0
+//                && thumbnailDisplayWidth > 0;
+//        }
+//
+//        // Getters and setters
+//        public String getOrganism() {
+//            return organism;
+//        }
+//
+//        public void setOrganism(String organism) {
+//            this.organism = organism;
+//        }
+//
+//        public String getChromosome() {
+//            return chromosome;
+//        }
+//
+//        public void setChromosome(String chromosome) {
+//            this.chromosome = chromosome;
+//        }
+//
+//        public int getChromosomeLength() {
+//            return chromosomeLength;
+//        }
+//
+//        public void setChromosomeLength(int chromosomeLength) {
+//            this.chromosomeLength = chromosomeLength;
+//        }
+//
+//        public int getThumbnailDisplayWidth() {
+//            return thumbnailDisplayWidth;
+//        }
+//
+//        public void setThumbnailDisplayWidth(int displayWidth) {
+//            this.thumbnailDisplayWidth = displayWidth;
+//        }
+//    }
 
     public void setBerkeleyMapFactory(BerkeleyMapFactory bmf) {
         this.bmf = bmf;
