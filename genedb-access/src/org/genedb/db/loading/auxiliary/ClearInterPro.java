e6548171f6199cc564ca3b732cb98c4846f691fb
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-01-09 15:42:36 +0000

    Change SQL to ANSI syntax, so it works with HSQLDB as well as PostgreSQL.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9700 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/auxiliary/ClearInterPro.java
+++ genedb-access/src/org/genedb/db/loading/auxiliary/ClearInterPro.java
@@ -1,5 +1,6 @@
 package org.genedb.db.loading.auxiliary;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ClearInterPro extends Clear {
@@ -11,27 +12,34 @@ public class ClearInterPro extends Clear {
         super(organismCommonName);
     }
 
+    ClearInterPro(Connection conn, String organismCommonName) {
+        super(conn, organismCommonName);
+    }
+
     private static final String DELETE_DOMAINS_SQL
     = "delete from feature"
-        +" using cvterm feature_type"
-        +" join cv feature_type_cv using (cv_id)"
-        +"    , organism"
-        +" where feature_type.cvterm_id = feature.type_id"
-        +" and feature_type.name = 'polypeptide_domain'"
+        +" where feature_id in ("
+        +" select feature.feature_id"
+        +" from feature"
+        +" join organism on feature.organism_id = organism.organism_id"
+        +" join cvterm feature_type on feature_type.cvterm_id = feature.type_id"
+        +" join cv feature_type_cv on feature_type.cv_id = feature_type_cv.cv_id"
+        +" where feature_type.name = 'polypeptide_domain'"
         +" and feature_type_cv.name = 'sequence'"
-        +" and feature.organism_id = organism.organism_id"
-        +" and organism.common_name = ?";
+        +" and organism.common_name = ?)";
 
     private static final String DELETE_INTERPRO_GO_TERMS_SQL
         = "delete from feature_cvterm"
-            +" using feature_cvtermprop"
-            +"     join cvterm prop_type on feature_cvtermprop.type_id = prop_type.cvterm_id"
-            +" , feature join organism using (organism_id)"
-            +" where feature_cvterm.feature_cvterm_id = feature_cvtermprop.feature_cvterm_id"
-            +" and feature_cvterm.feature_id = feature.feature_id"
-            +" and prop_type.name = 'autocomment'"
+            +" where feature_cvterm_id in ("
+            +" select feature_cvterm.feature_cvterm_id"
+            +" from feature_cvterm"
+            +" join feature_cvtermprop on feature_cvterm.feature_cvterm_id = feature_cvtermprop.feature_cvterm_id"
+            +" join cvterm prop_type on feature_cvtermprop.type_id = prop_type.cvterm_id"
+            +" join feature on feature_cvterm.feature_id = feature.feature_id"
+            +" join organism on feature.organism_id = organism.organism_id"
+            +" where prop_type.name = 'autocomment'"
             +" and feature_cvtermprop.value = 'From Interpro file'"
-            +" and organism.common_name = ?";
+            +" and organism.common_name = ?)";
 
     @Override
     protected DeleteSpec[] getDeleteSpecs() {
