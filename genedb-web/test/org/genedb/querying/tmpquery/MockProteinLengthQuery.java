8cd4367a1b5f74518e41b2243a7c0288eeb05f0b
lo2 <lo2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-02-19 17:24:58 +0000

    JUnit test for QueryController
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9842 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/test/org/genedb/querying/tmpquery/MockProteinLengthQuery.java
@@ -0,0 +1,65 @@
+package org.genedb.querying.tmpquery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.genedb.querying.core.Query;
+import org.genedb.querying.core.QueryException;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class MockProteinLengthQuery implements Query {
+
+	private List<String> results = new ArrayList<String>();
+	
+	public void initResult(List<String> results){
+		this.results = results;
+	}
+
+	@Override
+	public String getParseableDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getQueryDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getResults() throws QueryException {
+		return results;
+	}
+
+	@Override
+	public Validator getValidator() {
+		return new Validator() {
+            @Override
+            public void validate(Object target, Errors errors) {                
+                return;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public boolean supports(Class clazz) {
+                return MockProteinLengthQuery.class.isAssignableFrom(clazz);
+            }
+        };
+	}
+
+	@Override
+	public Map<String, Object> prepareModelData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
