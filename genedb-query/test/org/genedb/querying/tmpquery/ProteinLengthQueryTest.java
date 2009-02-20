725789b2e51d1331dca101684f0e517c1f6a67cb
lo2 <lo2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-02-20 11:19:29 +0000

    ProteinLengthQuery test
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@9847 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-query/test/org/genedb/querying/tmpquery/ProteinLengthQueryTest.java
@@ -0,0 +1,64 @@
+package org.genedb.querying.tmpquery;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.validation.Errors;
+import org.springframework.validation.BindException;
+
+/**
+ * 
+ * @author sangerinstitute
+ * @desc ProteinLengthQuery test case
+ *
+ */
+public class ProteinLengthQueryTest {
+	@Test
+	public void testValidLengthQuery(){
+		ProteinLengthQuery query = new ProteinLengthQuery();
+		query.setMin(50);
+		query.setMax(500);
+		Errors errors = new BindException(query, "query"); 
+		query.getValidator().validate(query, errors);
+		
+		Assert.assertFalse(errors.hasErrors());
+	}
+	
+	@Test
+	public void testMaxLengthValidation(){
+		ProteinLengthQuery query = new ProteinLengthQuery();
+		query.setMax(501);
+		Errors errors = new BindException(query, "query"); 
+		query.getValidator().validate(query, errors);
+		
+		Assert.assertTrue(errors.hasErrors());
+		Assert.assertEquals(errors.getFieldError("max").getField(), "max");
+		Assert.assertEquals(errors.getFieldError("max").getDefaultMessage(), 
+		"Max must be less than or equal to 500. Bla bla bla");
+	}
+	
+
+	@Test
+	public void testMinLengthValidation(){
+		ProteinLengthQuery query = new ProteinLengthQuery();
+		query.setMin(49);
+		Errors errors = new BindException(query, "query"); 
+		query.getValidator().validate(query, errors);
+		
+		Assert.assertTrue(errors.hasErrors());
+		Assert.assertEquals(errors.getFieldError("min").getField(), "min");
+		Assert.assertEquals(errors.getFieldError("min").getDefaultMessage(), 
+				"Min must be greater than or equal to 50. Bla bla bla");
+	}
+	
+	@Test
+	public void testPositiveRangeValidation(){
+		ProteinLengthQuery query = new ProteinLengthQuery();
+		query.setMin(500);
+		query.setMax(400);
+		Errors errors = new BindException(query, "query"); 
+		query.getValidator().validate(query, errors);
+		
+		Assert.assertTrue(errors.hasErrors());
+		Assert.assertEquals(errors.getGlobalError().getCode(), "min.greater.than.max");		
+	}
+}
