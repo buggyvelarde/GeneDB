9b164c68ec6adeaab1b034d52873333f5cbbdeb9
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-03-11 15:02:30 +0000

    Cope with API change
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10051 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/WebContent/includes/scripts/genedb/history.js
+++ genedb-web/WebContent/includes/scripts/genedb/history.js
@@ -1,37 +1,37 @@
 function initHistory(base) {
-	var url = base + "History";
-	
-	var history = new function() {
+    var url = base + "History";
+
+    var history = new function() {
 
         this.formatDownload = function(elCell, oRecord, oColumn) {
-        	elCell.innerHTML = '<img src="' + base + 'includes/images/save.gif" title="Download Results" />';
-        	elCell.style.cursor = 'pointer';
+            elCell.innerHTML = '<img src="' + base + 'includes/images/save.gif" title="Download Results" />';
+            elCell.style.cursor = 'pointer';
         };
-        
+
         this.formatView = function(elCell, oRecord, oColumn) {
-        	elCell.innerHTML = '<img src="' + base + 'includes/images/edit.gif" title="Edit Results" />';
-        	elCell.style.cursor = 'pointer';
+            elCell.innerHTML = '<img src="' + base + 'includes/images/edit.gif" title="Edit Results" />';
+            elCell.style.cursor = 'pointer';
         };
-        
+
         this.formatRemove = function(elCell, oRecord, oColumn) {
-        	elCell.innerHTML = '<img src="' + base + 'includes/images/delete.gif" title="Remove Query From History" />';
-        	elCell.style.cursor = 'pointer';
+            elCell.innerHTML = '<img src="' + base + 'includes/images/delete.gif" title="Remove Query From History" />';
+            elCell.style.cursor = 'pointer';
         };
-        
+
         this.formatName = function(elCell, oRecord, oColumn) {
-        	elCell.innerHTML = oRecord.getData().name +
-        		 '<img align="top" src="' + base + 'includes/images/pencil.png" title="Edit" />';
+            elCell.innerHTML = oRecord.getData().name +
+                 '<img align="top" src="' + base + 'includes/images/pencil.png" title="Edit" />';
         }
-        
+
         this.formatSerialNumber = function(elCell, oRecord, oColumn) {
-        	var num = myDT.getRecordIndex(oRecord);
-        	num++;
-        	elCell.innerHTML = num;
-        	var data = oRecord.getData();
-        	data.no = num;
-        	oRecord.setData(data);	
+            var num = myDT.getRecordIndex(oRecord);
+            num++;
+            elCell.innerHTML = num;
+            var data = oRecord.getData();
+            data.no = num;
+            oRecord.setData(data);
         };
-        
+
         var myColumnDefs = [
             {key:"no", label:"No.",formatter:this.formatSerialNumber},
             {key:"historyType", label:"History Type", sortable:true,minWidth:30},
@@ -48,22 +48,19 @@ function initHistory(base) {
             resultsList: "items",
             fields: ["no","historyType","name","numberItems"]
         };
-        
-        var oConfigs = { 
-	                paginator: new YAHOO.widget.Paginator({ 
-	                    alwaysVisible: false,
-	                	rowsPerPage: 25,
-	                    template : "{PreviousPageLink} <span>{CurrentPageReport}</span> {PageLinks} {NextPageLink}",
-	                    previousPageLinkLabel : '&lt;',
-	                    nextPageLinkLabel : '&gt;',
-	                    pageReportTemplate : 'Showing records <strong>{startRecord} - {endRecord}</strong> of {totalRecords}'
-	                }),  
-	                initialRequest: "/viewData",
-	                width: 600
-	        }; 	 
-        
+
+        var oConfigs = {
+                    paginator: new YAHOO.widget.Paginator({
+                        alwaysVisible: false,
+                        rowsPerPage: 25,
+                        template : "{PreviousPageLink} <span>{CurrentPageReport}</span> {PageLinks} {NextPageLink}"
+                    }),
+                    initialRequest: "/viewData",
+                    width: 600
+            };
+
         YAHOO.widget.DataTable.MSG_EMPTY = "No History Items Found.";
-        
+
         var myDT = this.myDataTable = new YAHOO.widget.DataTable("historyView", myColumnDefs,
                 this.myDataSource,oConfigs );
 
@@ -74,45 +71,45 @@ function initHistory(base) {
                 cell.className = 'yui-dt-highlighted';
             }
         };
-        
+
         var onCellClicked = function(e) {
-        	var target = e.target;
-		    var column = myDT.getColumn(target);
-		    var record = myDT.getRecord(target);
-		    var h = record.getData().no;
-		    if (column.key == 'delete') {
-		            var url = base + 'History/EditHistory?remove=true&history=' + h;
-		            var request = YAHOO.util.Connect.asyncRequest('GET', url,
-		            	{
-		                    success: function (o) {
-		                    	myDT.deleteRow(target);
-		                    	myDT.render();
-		                    },
-		                    failure: function (o) {
-		                        alert(o.statusText);
-		                    },
-		                    scope:this
-		                }
-		            );
-		            if(request.conn.status == 200) {
-		            	myDT.deleteRow(target);
-		            }
-		    } else if (column.key == 'edit') {
-		        window.location = base + 'History/EditHistory?history=' + h;
-		    }   else if (column.key == 'download') {
-		        window.location = base + 'History/Download?history=' + h;
-		    } else {
-		    	myDT.onEventShowCellEditor(e);
-		    }
+            var target = e.target;
+            var column = myDT.getColumn(target);
+            var record = myDT.getRecord(target);
+            var h = record.getData().no;
+            if (column.key == 'delete') {
+                    var url = base + 'History/EditHistory?remove=true&history=' + h;
+                    var request = YAHOO.util.Connect.asyncRequest('GET', url,
+                        {
+                            success: function (o) {
+                                myDT.deleteRow(target);
+                                myDT.render();
+                            },
+                            failure: function (o) {
+                                alert(o.statusText);
+                            },
+                            scope:this
+                        }
+                    );
+                    if(request.conn.status == 200) {
+                        myDT.deleteRow(target);
+                    }
+            } else if (column.key == 'edit') {
+                window.location = base + 'History/EditHistory?history=' + h;
+            }   else if (column.key == 'download') {
+                window.location = base + 'History/Download?history=' + h;
+            } else {
+                myDT.onEventShowCellEditor(e);
+            }
         }
-        
+
         this.myDataTable.subscribe("cellMouseoverEvent", this.highlightEditableCell);
         this.myDataTable.subscribe("cellMouseoutEvent", this.myDataTable.onEventUnhighlightCell);
         this.myDataTable.subscribe("cellClickEvent", onCellClicked);
-		
-		var handleSuccess = function(o){
-			myDT.resetCellEditor();
-			myDT._oRecordSet.updateKey(myDT._oCellEditor.record, myDT._oCellEditor.column.key, newData);
+
+        var handleSuccess = function(o){
+            myDT.resetCellEditor();
+            myDT._oRecordSet.updateKey(myDT._oCellEditor.record, myDT._oCellEditor.column.key, newData);
             myDT.formatCell(myDT._oCellEditor.cell);
             myDT.fireEvent("editorSaveEvent",
                             {editor:myDT._oCellEditor, oldData:oldData, newData:newData}
@@ -121,41 +118,41 @@ function initHistory(base) {
             myDT.render();
             var div = document.getElementById('logErrors');
             div.innerHTML = '';
-		}
+        }
 
-		var handleFailure = function(o){
-			var div = document.getElementById('logErrors');
-			if(o.status == 511) {
-				div.innerHTML = "A history item with this name already exists. <BR>" +
-						"Please provide a different name.";
-				myDT.resetCellEditor();
-				myDT._oCellEditor.cell.style.color = 'red';
-				myDT._oCellEditor.record._oData.name = oldData;
-				myDT.render();
-			} else {
-				div.innerHTML = "Some error occurred. Please Try again.";
-			}
-		}
-				
-		var oldData;
-		var newData;
-		
-		var callback =
-		{
-		  success:handleSuccess,
-		  failure: handleFailure,
-		  argument: [oldData,newData]
-		};
+        var handleFailure = function(o){
+            var div = document.getElementById('logErrors');
+            if(o.status == 511) {
+                div.innerHTML = "A history item with this name already exists. <BR>" +
+                        "Please provide a different name.";
+                myDT.resetCellEditor();
+                myDT._oCellEditor.cell.style.color = 'red';
+                myDT._oCellEditor.record._oData.name = oldData;
+                myDT.render();
+            } else {
+                div.innerHTML = "Some error occurred. Please Try again.";
+            }
+        }
 
-		
-		myDT.onEventSaveCellEditor = function (oArgs) {
-			newData = this._oCellEditor.value;
-        	var record = this._oCellEditor.record;
-        	oldData = record.getData(this._oCellEditor.column.key);
-			
-			var url = base + "History/EditName?history=" + record.getData().no + "&value=" + newData; 
-			var request = YAHOO.util.Connect.asyncRequest('GET', url,callback);
-		};
-		
+        var oldData;
+        var newData;
+
+        var callback =
+        {
+          success:handleSuccess,
+          failure: handleFailure,
+          argument: [oldData,newData]
+        };
+
+
+        myDT.onEventSaveCellEditor = function (oArgs) {
+            newData = this._oCellEditor.value;
+            var record = this._oCellEditor.record;
+            oldData = record.getData(this._oCellEditor.column.key);
+
+            var url = base + "History/EditName?history=" + record.getData().no + "&value=" + newData;
+            var request = YAHOO.util.Connect.asyncRequest('GET', url,callback);
+        };
+
     };
 }
