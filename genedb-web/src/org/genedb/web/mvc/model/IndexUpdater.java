856d0bc56ecd769a880979c0ee5d7e529774cd8e
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-03-23 11:09:56 +0000

    interface to allow periodic updaters to run in a 2-phase commit style
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10228 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-web/src/org/genedb/web/mvc/model/IndexUpdater.java
@@ -0,0 +1,24 @@
+package org.genedb.web.mvc.model;
+
+import org.genedb.db.audit.ChangeSet;
+
+/**
+ * Marks a program that can accept a changeset, as part of a two-phase
+ * commit system
+ */
+public interface IndexUpdater {
+
+    /**
+     * The entry point for processing a given list of C(R)UD changes
+     *
+     * @param cs The list of changes
+     * @return true if the update was successful
+     */
+    boolean indexFeatures(ChangeSet cs);
+
+    /**
+     * Called if all the indexing operations have succeeded, to make it really happen
+     */
+    void applyChanges();
+
+}
