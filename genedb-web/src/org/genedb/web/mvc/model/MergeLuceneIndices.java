a020216ce8252a3ebc07281e49b0660ff8d2a157
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-04-09 11:22:38 +0000

    Remove recurse option - always does now
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10465 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/model/MergeLuceneIndices.java
+++ genedb-web/src/org/genedb/web/mvc/model/MergeLuceneIndices.java
@@ -13,51 +13,28 @@ import java.io.IOException;
 
 public class MergeLuceneIndices {
 
-    private boolean recurse;
-
-
     public static void main(String[] args) throws CorruptIndexException, LockObtainFailedException, IOException {
 
         MergeLuceneIndices mli = new MergeLuceneIndices();
 
-        int startArgs = 0;
-        if (args[0].equals("-r")) {
-            mli.setRecurse(true);
-            startArgs = 1;
-        }
-
-        mli.merge(args[startArgs], "org.gmod.schema.Feature", args[startArgs+1], true);
-        mli.merge(args[startArgs], "org.gmod.schema.Organism", args[startArgs+1], false);
+        mli.merge(args[0], "org.gmod.schema.Feature", args[1], true);
+        mli.merge(args[0], "org.gmod.schema.Organism", args[1], false);
     }
-
-
 
     private void merge(String destinationDir, String indexName, String sourceDir, boolean create) throws CorruptIndexException, LockObtainFailedException, IOException {
         IndexWriter destination = new IndexWriter(destinationDir +File.separatorChar+indexName, new SimpleAnalyzer(), create);
 
         File source = new File(sourceDir);
 
-        if (isRecurse()) {
-            File[] dirs = source.listFiles();
-            Directory[] directories = new Directory[dirs.length];
-            for (int i = 0; i < dirs.length; i++) {
-                directories[i] = FSDirectory.getDirectory(dirs[i]);
-            }
-            destination.addIndexesNoOptimize(directories);
-        } else {
-            destination.addIndexesNoOptimize(new Directory[]{FSDirectory.getDirectory(sourceDir)});
+        File[] dirs = source.listFiles();
+        Directory[] directories = new Directory[dirs.length];
+        for (int i = 0; i < dirs.length; i++) {
+            directories[i] = FSDirectory.getDirectory(dirs[i]);
         }
+        destination.addIndexesNoOptimize(directories);
 
         destination.optimize();
         destination.close();
-    }
-
-    public boolean isRecurse() {
-        return recurse;
-    }
-
-    public void setRecurse(boolean recurse) {
-        this.recurse = recurse;
     }
 
 }
