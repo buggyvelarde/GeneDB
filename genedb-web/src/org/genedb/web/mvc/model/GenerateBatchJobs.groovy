591ccad15394b38abc2825f19baebf5ab6c38035
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-04-09 12:02:23 +0000

    Add _PopulateCaches target, designed to be called in bulk updates
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10471 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
+++ genedb-web/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
@@ -7,49 +7,51 @@ export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
 cd /nfs/pathdb/genedb/staging/cvs/genedb-ng/genedb-web
 '''
 
+def queueName = "normal";
+
 String baseDir = "/nfs/pathdb/genedb/staging/bulk/${args[0]}"
 new File("${baseDir}/scripts").mkdirs()
 new File("${baseDir}/output").mkdir()
 
 
-def sql = Sql.newInstance("jdbc:postgresql://pathdbsrv1-dmz:5432/staging", "genedb_ro", 
+def sql = Sql.newInstance("jdbc:postgresql://pathdbsrv1-dmz:5432/staging", "genedb_ro",
                                   "genedb_ro", "org.postgresql.Driver")
 
 sql.eachRow("select distinct(o.common_name) from organism o, feature f where f.organism_id = o.organism_id and o.common_name != 'dummy'") { row ->
 
-	def org = row.common_name
-	def antLine
+    def org = row.common_name
+    def antLine
 
-	switch (this.args[0]) {
+    switch (this.args[0]) {
 
-	case "Lucene":
-	    new File("${baseDir}/output/${org}").mkdir()
-	    antLine="ant -Ddeploy=${args[1]} -Dorganism=${org} -Ddir=${baseDir}/output/${org} _LuceneIndex"
-	    break;
+    case "Lucene":
+        new File("${baseDir}/output/${org}").mkdir()
+        antLine="ant -Ddeploy=${args[1]} -Dorganism=${org} -Ddir=${baseDir}/output/${org} _LuceneIndex"
+        break;
 
-	case "DTO":
-	    antLine=""
-	    break
+    case "DTO":
+        antLine="ant -Ddeploy=${args[1]} -Dorganism=${org} -Ddir=${baseDir}/output/${org} _PopulateCaches"
+        break
 
-	default:
-	    throw new RuntimeException("Don't know how to run '${args[0]}'")
-	}
+    default:
+        throw new RuntimeException("Don't know how to run '${args[0]}'")
+    }
 
 
-	String scriptName = "${baseDir}/scripts/script.${args[0]}.${org}"
-	File script = new File(scriptName)
-	script << boilerPlate
-	script << antLine + '\n'
-	"chmod 755 ${scriptName}".execute()
+    String scriptName = "${baseDir}/scripts/script.${args[0]}.${org}"
+    File script = new File(scriptName)
+    script << boilerPlate
+    script << antLine + '\n'
+    "chmod 755 ${scriptName}".execute()
 
-	println " ${org}"
-	Process p = ["ssh", "pcs4", "bsub -q yesterday -o ${scriptName}.out -e ${scriptName}.err ${scriptName}"].execute()
-	def sout = new StringBuffer()
+    println " ${org}"
+    Process p = ["ssh", "pcs4", "bsub -q ${queueName} -o ${scriptName}.out -e ${scriptName}.err ${scriptName}"].execute()
+    def sout = new StringBuffer()
         def serr = new StringBuffer()
-	p.consumeProcessOutput(sout, serr)
-	p.waitFor()
-	println "Output: ${sout}"
-	println "Error: ${serr}"
+    p.consumeProcessOutput(sout, serr)
+    p.waitFor()
+    println "Output: ${sout}"
+    println "Error: ${serr}"
 }
 
 println ""
