809f859586ed95a4bf06869326cb7c69afdea6c6
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-04-09 13:13:45 +0000

    Try to ensure X isn't used
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10477 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
+++ genedb-web/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
@@ -4,7 +4,7 @@ def boilerPlate = '''
 export JAVA_HOME=/software/pathogen/external/applications/java/java6
 export ANT_HOME=/software/pathogen/external/applications/ant/apache-ant
 export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
-export DISPLAY=
+unset DISPLAY
 cd /nfs/pathdb/genedb/staging/cvs/genedb-ng/genedb-web
 '''
 
@@ -45,7 +45,7 @@ sql.eachRow("select distinct(o.common_name) from organism o, feature f where f.o
     script << antLine + '\n'
     "chmod 755 ${scriptName}".execute()
 
-    println " ${org}"
+    print "${org} "
     Process p = ["ssh", "pcs4", "bsub -q ${queueName} -o ${scriptName}.out -e ${scriptName}.err ${scriptName}"].execute()
     def sout = new StringBuffer()
     def serr = new StringBuffer()
