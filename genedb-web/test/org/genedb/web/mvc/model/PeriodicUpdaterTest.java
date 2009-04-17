8b32ddf5850f84888df8cff1b50d875ff2ed9dc2
lo2 <lo2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-04-17 10:06:22 +0000

    PeriodicUpdater updates
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10506 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/test/org/genedb/web/mvc/model/PeriodicUpdaterTest.java
+++ genedb-web/test/org/genedb/web/mvc/model/PeriodicUpdaterTest.java
@@ -22,15 +22,25 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
     @Autowired
     PeriodicUpdater periodicUpdater;
     
+    /**
+     * 1.Start by clearing all the caches
+     * 2.Initialise the ChangeSet with the transcript Ids to be used with various tests
+     * 3.
+     * @throws Exception
+     */
     @Test
-    public void testChangeSetProcess()throws Exception{
-        Integer newFeatureId = 7;
-        Integer changedFeatureId = 14;
-        Integer deletedFeatureId = 19;
+    public void testTranscriptChangeSet()throws Exception{
+        Integer newTranscriptId = 7;
+        Integer changedTranscriptId = 14;
+        Integer deletedTranscriptId = 19;
         
-        //Empty the cache
+        //Clear all the caches
         CacheSynchroniser cacheSynchroniser = (CacheSynchroniser)periodicUpdater.getIndexUpdaters().get(1);
         cacheSynchroniser.getBmf().getDtoMap().clear();
+        cacheSynchroniser.getBmf().getContextMapMap().clear();
+        
+        //prevent excessive log printing
+        cacheSynchroniser.setNoPrintResult(true);
         
         //Get the changeset
         MockChangeSetImpl changeSet = 
@@ -39,71 +49,41 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
         //Add new Transcript feature
         List<Integer> newFeatureIds = new ArrayList<Integer>();
         changeSet.getNewMap().put(Transcript.class, newFeatureIds); 
-        newFeatureIds.add(newFeatureId);
+        newFeatureIds.add(newTranscriptId);
         
         //Change  transcript feature
         List<Integer> changedFeatureIds = new ArrayList<Integer>();
         changeSet.getChangedMap().put(Transcript.class, changedFeatureIds); 
-        changedFeatureIds.add(changedFeatureId);
+        changedFeatureIds.add(changedTranscriptId);
         
         //Delete  transcript feature
         cacheSynchroniser.getBmf().getDtoMap().put(19, new TranscriptDTO());
         List<Integer> deletedFeatureIds = new ArrayList<Integer>();
         changeSet.getDeletedMap().put(Transcript.class, deletedFeatureIds); 
-        deletedFeatureIds.add(deletedFeatureId);
+        deletedFeatureIds.add(deletedTranscriptId);
         
         
         //execute class under test
         boolean noErrors = periodicUpdater.processChangeSet();
         
         //Transcript DTO with featureID 7 is not null
-        TranscriptDTO transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(newFeatureId);
+        TranscriptDTO transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(newTranscriptId);
         Assert.assertNotNull(transcriptDTO);
         
+        //The changed transcript also updates/inserts it's corresponding 
+        //TopLevelFeature, in the case the ID is 1
+        String contextMap = cacheSynchroniser.getBmf().getContextMapMap().get(1);
+        Assert.assertNotNull(contextMap);
+        
         //Transcript DTO with featureID 14 is not null
-        transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(changedFeatureId);
+        transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(changedTranscriptId);
         Assert.assertNotNull(transcriptDTO);
         
         //Transcript DTO with featureID 19 IS null
-        transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(deletedFeatureId);
+        transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(deletedTranscriptId);
         Assert.assertNull(transcriptDTO);
         
         //No severe errors found
         Assert.assertTrue(noErrors);
-    }
-    
-    /**
-     * We know that the Transcript with feature ID 3 has a source feature, Top Level Feature with feature ID 1.
-     * Hence, if the changeset returns a feature ID 3 as a changed transcript, 
-     * we'll expect Top level Feature (feature ID 1) to be replaced. 
-     * @throws Exception
-     */
-    public void testChangedTranscript()throws Exception{
-        //Initialise changeset
-        Integer featureId = 3;
-        MockChangeSetImpl changeSet = 
-            (MockChangeSetImpl)periodicUpdater.getChangeTracker().changes(PeriodicUpdaterTest.class.getName());
-        changeSet.clearAll();    
-        
-        //Ensure that the TopLevelFeture with id 1 is not present in cache
-        CacheSynchroniser cacheSynchroniser = (CacheSynchroniser)periodicUpdater.getIndexUpdaters().get(1);
-        cacheSynchroniser.getBmf().getContextMapMap().clear();
-        
-        List<Integer> featureIds = new ArrayList<Integer>();
-        featureIds.add(featureId);
-        changeSet.getChangedMap().put(Transcript.class, featureIds); 
-        
-        //execute class under test
-        boolean noErrors = periodicUpdater.processChangeSet();
-        
-        //Get the context that should have now been updated
-        String contextMap = cacheSynchroniser.getBmf().getContextMapMap().get(1);
-        
-        //Transcript DTO is not null
-        Assert.assertNotNull(contextMap);
-        
-        //No severe errors found
-        Assert.assertTrue(noErrors);
-        
     }
 }
