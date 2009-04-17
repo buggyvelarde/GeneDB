cc566f86d33f1cdf1484c19c89dca4134265f5db
lo2 <lo2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-04-17 14:20:43 +0000

    PeriodicUpdater updates
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10528 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/test/org/genedb/web/mvc/model/PeriodicUpdaterTest.java
+++ genedb-web/test/org/genedb/web/mvc/model/PeriodicUpdaterTest.java
@@ -7,6 +7,7 @@ import junit.framework.Assert;
 
 import org.genedb.db.audit.MockChangeSetImpl;
 import org.gmod.schema.feature.Gene;
+import org.gmod.schema.feature.Polypeptide;
 import org.gmod.schema.feature.Transcript;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,6 +28,68 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
     @Autowired
     PeriodicUpdater periodicUpdater;
     
+    public void testTopLevelFeatureChangeSet(){
+        
+    }
+
+    
+    /**
+     * 1.Start by clearing all the caches
+     * 2.Initialise the ChangeSet with the polypeptide Ids to be used with various tests
+     * @throws Exception
+     */
+    @Test
+    public void testPolypeptideChangeSet()throws Exception{
+        Integer newPolyPep = 810;//PFA0010c:pep
+        Integer changedPep = 614;//PFA0005w:pep
+        
+        //Clear all the caches
+        CacheSynchroniser cacheSynchroniser = (CacheSynchroniser)periodicUpdater.getIndexUpdaters().get(1);
+        cacheSynchroniser.getBmf().getDtoMap().clear();
+        cacheSynchroniser.getBmf().getContextMapMap().clear();
+        
+        //prevent excessive log printing
+        cacheSynchroniser.setNoPrintResult(true);
+        
+        //Get the changeset
+        MockChangeSetImpl changeSet = 
+            (MockChangeSetImpl)periodicUpdater.getChangeTracker().changes(PeriodicUpdaterTest.class.getName());
+        
+        //Add new Gene feature to change set
+        List<Integer> newFeatureIds = new ArrayList<Integer>();
+        changeSet.getNewMap().put(Polypeptide.class, newFeatureIds); 
+        newFeatureIds.add(newPolyPep);
+        
+        //Change Gene feature
+        List<Integer> changedFeatureIds = new ArrayList<Integer>();
+        changeSet.getChangedMap().put(Polypeptide.class, changedFeatureIds); 
+        newFeatureIds.add(changedPep);
+        
+        
+        //execute class under test
+        boolean noErrors = periodicUpdater.processChangeSet();
+        
+        //Assert Transcript DTO with featureID 807 is not null
+        //ID 807(PFA0010c:mRNA) is a transcript of Polypeptide 810(PFA0010c:pep)
+        TranscriptDTO transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(807);
+        Assert.assertNotNull(transcriptDTO);
+        Assert.assertEquals("PFA0010c:mRNA", transcriptDTO.getUniqueName());
+        
+        //Assert Transcript DTO with featureID 611 is not null
+        //ID 611(PFA0005w:mRNA) is a transcript of Polypeptide 614(PFA0005w:pep)
+        transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(611);
+        Assert.assertNotNull(transcriptDTO);
+        Assert.assertEquals("PFA0005w:mRNA", transcriptDTO.getUniqueName());
+        
+        //Assert No severe errors found
+        Assert.assertTrue(noErrors);
+    }
+    
+    /**
+     * 1.Start by clearing all the caches
+     * 2.Initialise the ChangeSet with the transcript Ids to be used with various tests
+     * @throws Exception
+     */
     @Test
     public void testGeneChangeSet()throws Exception{
         Integer newGeneId = 2;//PFA0170c
@@ -44,12 +107,12 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
         MockChangeSetImpl changeSet = 
             (MockChangeSetImpl)periodicUpdater.getChangeTracker().changes(PeriodicUpdaterTest.class.getName());
         
-        //Add new Gene feature
+        //Add new Gene feature to change set
         List<Integer> newFeatureIds = new ArrayList<Integer>();
         changeSet.getNewMap().put(Gene.class, newFeatureIds); 
         newFeatureIds.add(newGeneId);
         
-        //Change Gene feature
+        //Change Gene feature in change set
         List<Integer> changedFeatureIds = new ArrayList<Integer>();
         changeSet.getChangedMap().put(Gene.class, changedFeatureIds); 
         newFeatureIds.add(changedGeneId);
@@ -62,11 +125,13 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
         //ID 3(PFA0170c:mRNA) is a transcript of Gene ID 2(PFA0170c)
         TranscriptDTO transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(3);
         Assert.assertNotNull(transcriptDTO);
+        Assert.assertEquals("PFA0170c:mRNA", transcriptDTO.getUniqueName());
         
         //Assert Transcript DTO with featureID 611 is not null
         //ID 611(PFA0005w:mRNA) is a transcript of Gene ID 610(PFA0005w)
         transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(611);
         Assert.assertNotNull(transcriptDTO);
+        Assert.assertEquals("PFA0005w:mRNA", transcriptDTO.getUniqueName());
         
         //Assert No severe errors found
         Assert.assertTrue(noErrors);
@@ -76,13 +141,13 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
     /**
      * 1.Start by clearing all the caches
      * 2.Initialise the ChangeSet with the transcript Ids to be used with various tests
-     * 3.
      * @throws Exception
      */
+    @Test
     public void testTranscriptChangeSet()throws Exception{
-        Integer newTranscriptId = 7;
-        Integer changedTranscriptId = 14;
-        Integer deletedTranscriptId = 19;
+        Integer newTranscriptId = 7;//PFA0315w:mRNA
+        Integer changedTranscriptId = 14;//PFA0380w:mRNA
+        Integer deletedTranscriptId = 19;//PFA0440w:mRNA
         
         //Clear all the caches
         CacheSynchroniser cacheSynchroniser = (CacheSynchroniser)periodicUpdater.getIndexUpdaters().get(1);
@@ -96,17 +161,17 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
         MockChangeSetImpl changeSet = 
             (MockChangeSetImpl)periodicUpdater.getChangeTracker().changes(PeriodicUpdaterTest.class.getName());
         
-        //Add new Transcript feature
+        //Add new Transcript feature to change set
         List<Integer> newFeatureIds = new ArrayList<Integer>();
         changeSet.getNewMap().put(Transcript.class, newFeatureIds); 
         newFeatureIds.add(newTranscriptId);
         
-        //Change  transcript feature
+        //Change  transcript feature in change set
         List<Integer> changedFeatureIds = new ArrayList<Integer>();
         changeSet.getChangedMap().put(Transcript.class, changedFeatureIds); 
         changedFeatureIds.add(changedTranscriptId);
         
-        //Delete  transcript feature
+        //Delete  transcript feature from change set
         cacheSynchroniser.getBmf().getDtoMap().put(19, new TranscriptDTO());
         List<Integer> deletedFeatureIds = new ArrayList<Integer>();
         changeSet.getDeletedMap().put(Transcript.class, deletedFeatureIds); 
@@ -119,6 +184,7 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
         //Transcript DTO with featureID 7 is not null
         TranscriptDTO transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(newTranscriptId);
         Assert.assertNotNull(transcriptDTO);
+        Assert.assertEquals("PFA0315w:mRNA", transcriptDTO.getUniqueName());
         
         //The changed transcript also updates/inserts it's corresponding 
         //TopLevelFeature, in the case the ID is 1
@@ -128,6 +194,7 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
         //Assert Transcript DTO with featureID 14 is not null
         transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(changedTranscriptId);
         Assert.assertNotNull(transcriptDTO);
+        Assert.assertEquals("PFA0380w:mRNA", transcriptDTO.getUniqueName());
         
         //Assert Transcript DTO with featureID 19 IS null
         transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(deletedTranscriptId);
