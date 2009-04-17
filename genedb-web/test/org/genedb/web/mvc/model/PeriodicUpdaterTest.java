c1b298af5d1aa57542c6e5e11b80d02afd68675c
lo2 <lo2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-04-17 10:34:24 +0000

    PeriodicUpdater updates
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10511 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/test/org/genedb/web/mvc/model/PeriodicUpdaterTest.java
+++ genedb-web/test/org/genedb/web/mvc/model/PeriodicUpdaterTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import junit.framework.Assert;
 
 import org.genedb.db.audit.MockChangeSetImpl;
+import org.gmod.schema.feature.Gene;
 import org.gmod.schema.feature.Transcript;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +15,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-
+/**
+ * To see the final results of the method under test, simply set the cacheSynchroniser.setNoPrintResult(false); 
+ * @author larry@sangerinstitute
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class PeriodicUpdaterTest extends AbstractUpdaterTest{
@@ -22,13 +27,58 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
     @Autowired
     PeriodicUpdater periodicUpdater;
     
+    @Test
+    public void testGeneChangeSet()throws Exception{
+        Integer newGeneId = 2;//PFA0170c
+        Integer changedGeneId = 610;//PFA0005w
+        
+        //Clear all the caches
+        CacheSynchroniser cacheSynchroniser = (CacheSynchroniser)periodicUpdater.getIndexUpdaters().get(1);
+        cacheSynchroniser.getBmf().getDtoMap().clear();
+        cacheSynchroniser.getBmf().getContextMapMap().clear();
+        
+        //prevent excessive log printing
+        cacheSynchroniser.setNoPrintResult(true);
+        
+        //Get the changeset
+        MockChangeSetImpl changeSet = 
+            (MockChangeSetImpl)periodicUpdater.getChangeTracker().changes(PeriodicUpdaterTest.class.getName());
+        
+        //Add new Gene feature
+        List<Integer> newFeatureIds = new ArrayList<Integer>();
+        changeSet.getNewMap().put(Gene.class, newFeatureIds); 
+        newFeatureIds.add(newGeneId);
+        
+        //Change Gene feature
+        List<Integer> changedFeatureIds = new ArrayList<Integer>();
+        changeSet.getChangedMap().put(Gene.class, changedFeatureIds); 
+        newFeatureIds.add(changedGeneId);
+        
+        
+        //execute class under test
+        boolean noErrors = periodicUpdater.processChangeSet();
+        
+        //Assert Transcript DTO with featureID 3 is not null
+        //ID 3(PFA0170c:mRNA) is a transcript of Gene ID 2(PFA0170c)
+        TranscriptDTO transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(3);
+        Assert.assertNotNull(transcriptDTO);
+        
+        //Assert Transcript DTO with featureID 611 is not null
+        //ID 611(PFA0005w:mRNA) is a transcript of Gene ID 610(PFA0005w)
+        transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(611);
+        Assert.assertNotNull(transcriptDTO);
+        
+        //Assert No severe errors found
+        Assert.assertTrue(noErrors);
+        
+    }
+    
     /**
      * 1.Start by clearing all the caches
      * 2.Initialise the ChangeSet with the transcript Ids to be used with various tests
      * 3.
      * @throws Exception
      */
-    @Test
     public void testTranscriptChangeSet()throws Exception{
         Integer newTranscriptId = 7;
         Integer changedTranscriptId = 14;
@@ -75,15 +125,15 @@ public class PeriodicUpdaterTest extends AbstractUpdaterTest{
         String contextMap = cacheSynchroniser.getBmf().getContextMapMap().get(1);
         Assert.assertNotNull(contextMap);
         
-        //Transcript DTO with featureID 14 is not null
+        //Assert Transcript DTO with featureID 14 is not null
         transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(changedTranscriptId);
         Assert.assertNotNull(transcriptDTO);
         
-        //Transcript DTO with featureID 19 IS null
+        //Assert Transcript DTO with featureID 19 IS null
         transcriptDTO = cacheSynchroniser.getBmf().getDtoMap().get(deletedTranscriptId);
         Assert.assertNull(transcriptDTO);
         
-        //No severe errors found
+        //Assert No severe errors found
         Assert.assertTrue(noErrors);
     }
 }
