79d482071a3157cbf5350816105f0c46c3ecdcee
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-05-22 10:04:35 +0000

    Revert to earlier version
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10858 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/download/QueryController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/download/QueryController.java
@@ -8,6 +8,9 @@ import java.util.Map;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpSession;
 
+import com.google.common.collect.Lists;
+import com.sleepycat.collections.StoredMap;
+
 import org.apache.log4j.Logger;
 import org.genedb.db.taxon.TaxonNode;
 import org.genedb.db.taxon.TaxonNodeArrayPropertyEditor;
@@ -15,7 +18,9 @@ import org.genedb.querying.core.Query;
 import org.genedb.querying.core.QueryException;
 import org.genedb.querying.core.QueryFactory;
 import org.genedb.querying.tmpquery.GeneSummary;
+import org.genedb.querying.tmpquery.TaxonQuery;
 import org.genedb.web.mvc.controller.WebConstants;
+import org.genedb.web.mvc.model.ResultsCacheFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -32,9 +37,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/Query")
-public class QueryController extends AbstractGeneDBFormController{
+public class QueryController {
 
     private static final Logger logger = Logger.getLogger(QueryController.class);
+
+    private static final String RESULTS_ATTR = "results";
+
+    @Autowired
+    private TaxonNodeArrayPropertyEditor taxonNodeArrayPropertyEditor;
+
+    @Autowired
+    private ResultsCacheFactory resultsCacheFactory;
 
     @Autowired
     private QueryFactory queryFactory;
@@ -44,6 +57,31 @@ public class QueryController extends AbstractGeneDBFormController{
         return "redirect:/QueryList";
     }
 
+//    @RequestMapping(method = RequestMethod.GET , params= "q")
+//    public String setUpForm(
+//            @RequestParam(value="q") String queryName,
+//            ServletRequest request,
+//            HttpSession session,
+//            Model model) throws QueryException {
+//
+//        logger.debug("This is the setupform method");
+//
+//        if (!StringUtils.hasText(queryName)) {
+//            session.setAttribute(WebConstants.FLASH_MSG, "Unable to identify which query to use");
+//            return "redirect:/QueryList";
+//        }
+//
+//        Query query = queryFactory.retrieveQuery(queryName);
+//        if (query == null) {
+//            session.setAttribute(WebConstants.FLASH_MSG, String.format("Unable to find query called '%s'", queryName));
+//            return "redirect:/QueryList";
+//        }
+//        model.addAttribute("query", query);
+//
+//        populateModelData(model, query);
+//        return "search/"+queryName;
+//    }
+
     @RequestMapping(method = RequestMethod.GET , params= "q")
     public String processForm(
             @RequestParam(value="q") String queryName,
@@ -51,28 +89,39 @@ public class QueryController extends AbstractGeneDBFormController{
             ServletRequest request,
             HttpSession session,
             Model model) throws QueryException {
-        
-        //Find query for request
-        Query query = findQueryType(queryName, session);
-        if (query==null){
+
+        if (!StringUtils.hasText(queryName)) {
+               session.setAttribute(WebConstants.FLASH_MSG, "Unable to identify which query to use");
             return "redirect:/QueryList";
         }
 
-        //Initialise model data somehow
+        Query query = queryFactory.retrieveQuery(queryName);
+        if (query == null) {
+            session.setAttribute(WebConstants.FLASH_MSG, String.format("Unable to find query called '%s'", queryName));
+            return "redirect:/QueryList";
+        }
+
         model.addAttribute("query", query);
         logger.debug("The number of parameters is '" + request.getParameterMap().keySet().size() + "'");
         populateModelData(model, query);
 
-        //Initialise query form
-        Errors errors = initialiseQueryForm(query, request);        
+
+        // Attempt to fill in form
+        ServletRequestDataBinder binder = new ServletRequestDataBinder(query);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy/MM/dd"), false, 10));
+        binder.registerCustomEditor(TaxonNode[].class, taxonNodeArrayPropertyEditor);
+
+        binder.bind(request);
+
+        Errors errors = binder.getBindingResult();
         if (errors.hasErrors()) {
             model.addAttribute(BindingResult.MODEL_KEY_PREFIX + "query", errors);
             logger.debug("Returning due to binding error");
             return "search/"+queryName;
         }
 
-        //Validate initialised form
         query.validate(query, errors);
+
         if (errors.hasErrors()) {
             logger.debug("Validator found errors");
             model.addAttribute(BindingResult.MODEL_KEY_PREFIX + "query", errors);
@@ -81,43 +130,35 @@ public class QueryController extends AbstractGeneDBFormController{
 
         logger.debug("Validator found no errors");
         List results = query.getResults();
-        
-        //Suppress item in results
-        suppressResultItem(suppress, results);
 
-        //Dispatch request to appropriate view
-        return findDestinationView(queryName, query, model, results, session);
-    }
+        String taxonName = null;
+        if (query instanceof TaxonQuery) {
+            TaxonNode[] nodes = ((TaxonQuery) query).getTaxons();
+            if (nodes != null && nodes.length > 0) {
+                taxonName = nodes[0].getLabel();
+            } // FIXME
+        }
 
-    
-    /**
-     * Work out the correct view destination
-     * @param queryName
-     * @param query
-     * @param taxonName
-     * @param model
-     * @param results
-     * @param session
-     * @return
-     */
-    private String findDestinationView(
-            String queryName, Query query, Model model, List results, HttpSession session){
+        if (StringUtils.hasLength(suppress)) {
+            int index = results.indexOf(suppress);
+            if (index != -1) {
+                results.remove(index);
+            } else {
+                logger.warn("Trying to remove '" + suppress + "' from results (as a result of an n-others call but it isn't present");
+            }
+        }
 
-        //Get the current taxon name
-        String taxonName = findTaxonName(query);
         String resultsKey = null;
-        
+
         switch (results.size()) {
         case 0:
             logger.debug("No results found for query");
             model.addAttribute("taxonNodeName", taxonName);
             return "search/"+queryName;
-            
         case 1:
             List<GeneSummary> gs = possiblyConvertList(results);
             resultsKey = cacheResults(gs, query, queryName, session.getId());
             return "redirect:/NamedFeature?name=" + gs.get(0).getSystematicId();
-            
         default:
             List<GeneSummary> gs2 = possiblyConvertList(results);
             resultsKey = cacheResults(gs2, query, queryName, session.getId());
@@ -129,40 +170,40 @@ public class QueryController extends AbstractGeneDBFormController{
     }
 
 
+    private List<GeneSummary> possiblyConvertList(List results) {
+        List<GeneSummary> gs;
+        Object firstItem =  results.get(0);
+        if (firstItem instanceof GeneSummary) {
+            gs = results;
+        } else {
+            gs = Lists.newArrayListWithExpectedSize(results.size());
+            for (Object o  : results) {
+                gs.add(new GeneSummary((String) o));
+            }
+        }
+        return gs;
+    }
+
+
+    private String cacheResults(List<GeneSummary> gs, Query q, String queryName, String sessionId) {
+        String key = sessionId + Integer.toString(System.identityHashCode(gs)); // CHECKME
+        StoredMap<String, ResultEntry> map = resultsCacheFactory.getResultsCacheMap();
+        ResultEntry re = new ResultEntry();
+        re.numOfResults = gs.size();
+        re.query = q;
+        re.results = gs;
+        re.queryName = queryName;
+        map.put(key, re);
+        return key;
+    }
+
+
     private void populateModelData(Model model, Query query) {
         Map<String, Object> modelData = query.prepareModelData();
         for (Map.Entry<String, Object> entry : modelData.entrySet()) {
             model.addAttribute(entry.getKey(), entry.getValue());
         }
     }
-    
-    protected Query findQueryType(String queryName, HttpSession session){
-        if (!StringUtils.hasText(queryName)) {
-               session.setAttribute(WebConstants.FLASH_MSG, "Unable to identify which query to use");
-        }
-        Query query = queryFactory.retrieveQuery(queryName);
-        if (query == null) {
-            session.setAttribute(WebConstants.FLASH_MSG, String.format("Unable to find query called '%s'", queryName));
-        }
-        return query;
-    }
-    
-    /**
-     * Remove an item in the result list
-     * @param suppress
-     * @param results
-     */
-    @SuppressWarnings("unchecked")
-    protected void suppressResultItem(String suppress, List results){
-
-        if (StringUtils.hasLength(suppress)) {
-            int index = results.indexOf(suppress);
-            if (index != -1) {
-                results.remove(index);
-            } else {
-                logger.warn("Trying to remove '" + suppress + "' from results (as a result of an n-others call but it isn't present");
-            }
-        }
-    }
 
 }
+
