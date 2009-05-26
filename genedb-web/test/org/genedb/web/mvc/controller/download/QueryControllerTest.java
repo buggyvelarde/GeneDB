cf08eaa8c260f097128a3bb887005a5af8c163a3
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-05-26 15:55:18 +0000

    Update for API change
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10908 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/test/org/genedb/web/mvc/controller/download/QueryControllerTest.java
+++ genedb-web/test/org/genedb/web/mvc/controller/download/QueryControllerTest.java
@@ -21,7 +21,7 @@ import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
 /**
- * 
+ *
  * @author Larry Oke
  * @desc The QueryController test case
  *
@@ -29,114 +29,114 @@ import org.springframework.ui.Model;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration// "classpath:/com/example/QueryControllerTest-context.xml"
 public class QueryControllerTest {
-	
+
 	@Autowired
 	private QueryController queryController;
-	
+
 	@Autowired
-	private Query proteinLengthQuery; 
-	
+	private Query proteinLengthQuery;
+
 	/**
 	 * Test to verify a successful ProteinLengthQuery submission
 	 */
 	@Test
 	public void testMultipleResultProteinLengthQuery() throws Exception{
-		
+
 		//Initialise context
 		ServletRequest request = createProteinLengthQueryServletRequest("proteinLength");
 		String queryName = request.getParameter("q");
 		HttpSession session = new MockHttpSession();
 		Model model = new ExtendedModelMap();
-		
+
 
 		//Re- Initialise results to be returned by query
 		List<String> results = new ArrayList<String>();
 		results.add("resultItem1");
 		results.add("resultItem2");
 		((MockProteinLengthQuery)proteinLengthQuery).initResult(results);
-		
+
 		//test
-		String view = queryController.processForm(queryName, request, session, model);
-		
+		String view = queryController.processForm(queryName, null, request, session, model);
+
 		//Check for incorrect view
 		Assert.assertNotSame("Wrong view returned.", "redirect:/QueryList", view);
-		
+
 		//Check for error message absence
 		Assert.assertNull(
-				"Error returned when processing form is: " + 
-				session.getAttribute(WebConstants.FLASH_MSG), 
+				"Error returned when processing form is: " +
+				session.getAttribute(WebConstants.FLASH_MSG),
 				session.getAttribute(WebConstants.FLASH_MSG));
-		
+
 		//Check for correct view
 		Assert.assertEquals("Wrong view returned.", "search/"+queryName, view);
-		
+
 	}
-	
+
 	/**
 	 * Test to verify a successful ProteinLengthQuery submission
 	 */
 	@Test
 	public void testSingleResultProteinLengthQuery() throws Exception{
-		
+
 		//Initialise context
 		ServletRequest request = createProteinLengthQueryServletRequest("proteinLength");
 		String queryName = request.getParameter("q");
 		HttpSession session = new MockHttpSession();
 		Model model = new ExtendedModelMap();
-		
+
 
 		//Re- Initialise results to be returned by query
 		List<String> results = new ArrayList<String>();
-		results.add("resultItem1");		
+		results.add("resultItem1");
 		((MockProteinLengthQuery)proteinLengthQuery).initResult(results);
-		
+
 		//test
-		String view = queryController.processForm(queryName, request, session, model);
-		
+		String view = queryController.processForm(queryName, null, request, session, model);
+
 		//Check for incorrect view
 		Assert.assertNotSame("Wrong view returned.", "redirect:/QueryList", view);
-		
+
 		//Check for error message absence
 		Assert.assertNull(
-				"Error returned when processing form is: " + 
-				session.getAttribute(WebConstants.FLASH_MSG), 
+				"Error returned when processing form is: " +
+				session.getAttribute(WebConstants.FLASH_MSG),
 				session.getAttribute(WebConstants.FLASH_MSG));
-		
+
 		//Check for correct view
 		Assert.assertEquals("Wrong view returned.", "redirect:/NamedFeature?name="+results.get(0), view);
-		
-	}	
 
-	
+	}
+
+
 	/**
 	 * Test to verify a successful ProteinLengthQuery submission
 	 */
 	@Test
 	public void testEmptyResultProteinLengthQuery() throws Exception{
-		
+
 		//Initialise context
 		ServletRequest request = createProteinLengthQueryServletRequest("proteinLength");
 		String queryName = request.getParameter("q");
 		HttpSession session = new MockHttpSession();
 		Model model = new ExtendedModelMap();
-		
+
 
 		//Re- Initialise to ensure results are empty
 		List<String> results = new ArrayList<String>();
 		((MockProteinLengthQuery)proteinLengthQuery).initResult(results);
-		
+
 		//test
-		String view = queryController.processForm(queryName, request, session, model);
-		
+		String view = queryController.processForm(queryName, null, request, session, model);
+
 		//Check for incorrect view
 		Assert.assertNotSame("Wrong view returned.", "redirect:/QueryList", view);
-		
+
 		//Check for correct view
 		Assert.assertEquals("Wrong view returned.", "search/"+queryName, view);
 	}
-	
+
 	/**
-	 * Mock the ServletRequest 
+	 * Mock the ServletRequest
 	 * @return
 	 */
 	private ServletRequest createProteinLengthQueryServletRequest(String queryName){
