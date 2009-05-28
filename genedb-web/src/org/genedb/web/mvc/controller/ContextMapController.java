b0f9f85f7fefdf2ca6971ecd29d8c807792a47c7
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-05-28 15:08:46 +0000

    GENEDB-337 Tidy up by removing unused properties
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@10941 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/ContextMapController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/ContextMapController.java
@@ -1,23 +1,19 @@
 package org.genedb.web.mvc.controller;
 
 import org.genedb.db.dao.SequenceDao;
-import org.genedb.querying.core.LuceneIndex;
-import org.genedb.querying.core.LuceneIndexFactory;
 import org.genedb.web.mvc.model.BerkeleyMapFactory;
 
-import org.apache.log4j.Logger;
 import org.gmod.schema.feature.TopLevelFeature;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.View;
 
 import java.io.IOException;
 import java.io.Writer;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -32,38 +28,14 @@ public class ContextMapController {
 
     private Logger logger = Logger.getLogger(this.getClass());
 
-    //private DiagramCache fileDiagramCache;
-    private LuceneIndexFactory luceneIndexFactory; // Injected by Spring
-    private View view; // Defined in genedb-servlet.xml
-    private int cacheHit;
-    private int cacheMiss;
-
-//    private BlockingCache contextMapCache;
     private BerkeleyMapFactory bmf;
 
     @Autowired
     private SequenceDao sequenceDao;
 
-    private Boolean initialised = Boolean.FALSE;
-
-
-    //@PostConstruct
-    private void init() {
-        synchronized (initialised) {
-            if (!initialised) {
-                LuceneIndex luceneIndex = luceneIndexFactory.getIndex("org.gmod.schema.mapped.Feature");
-                initialised = Boolean.TRUE;
-            }
-        }
-    }
-
     @RequestMapping(method=RequestMethod.GET)
     public void writeMapInfo(HttpServletRequest request, Writer out,
             HttpServletResponse response, String chromosome) throws IOException {
-
-        if (!initialised) {
-            init();
-        }
 
 
         TopLevelFeature tlf = sequenceDao.getFeatureByUniqueName(chromosome, TopLevelFeature.class);
@@ -71,95 +43,19 @@ public class ContextMapController {
 
         }
         if ( ! tlf.isTopLevelFeature()) {
-
+            // TODO
         }
 
 
         String text = bmf.getContextMapMap().get(tlf.getFeatureId());
         if (text != null) {
             logger.trace("Cache hit for context map '"+chromosome+"' of '"+text+"'");
-            cacheHit++;
         } else {
             logger.debug(String.format("The context maps for '%s' aren't cached and need to be generated", chromosome));
-            cacheMiss++;
         }
 
         out.write(text);
     }
-
-    public LuceneIndexFactory getLuceneIndexFactory() {
-        return luceneIndexFactory;
-    }
-
-    //@Required
-    public void setLuceneIndexFactory(LuceneIndexFactory luceneIndexFactory) {
-        this.luceneIndexFactory = luceneIndexFactory;
-    }
-
-    public View getView() {
-        return view;
-    }
-
-    public void setView(View view) {
-        this.view = view;
-    }
-
-    public int getCacheHit() {
-        return cacheHit;
-    }
-
-    public int getCacheMiss() {
-        return cacheMiss;
-    }
-
-
-
-//    public static class Command {
-//        private String organism;
-//        private String chromosome;
-//        private int chromosomeLength;
-//        private int thumbnailDisplayWidth;
-//
-//        public boolean hasRequiredData() {
-//            return organism != null
-//                && chromosome != null
-//                && chromosomeLength > 0
-//                && thumbnailDisplayWidth > 0;
-//        }
-//
-//        // Getters and setters
-//        public String getOrganism() {
-//            return organism;
-//        }
-//
-//        public void setOrganism(String organism) {
-//            this.organism = organism;
-//        }
-//
-//        public String getChromosome() {
-//            return chromosome;
-//        }
-//
-//        public void setChromosome(String chromosome) {
-//            this.chromosome = chromosome;
-//        }
-//
-//        public int getChromosomeLength() {
-//            return chromosomeLength;
-//        }
-//
-//        public void setChromosomeLength(int chromosomeLength) {
-//            this.chromosomeLength = chromosomeLength;
-//        }
-//
-//        public int getThumbnailDisplayWidth() {
-//            return thumbnailDisplayWidth;
-//        }
-//
-//        public void setThumbnailDisplayWidth(int displayWidth) {
-//            this.thumbnailDisplayWidth = displayWidth;
-//        }
-//    }
 
     public void setBerkeleyMapFactory(BerkeleyMapFactory bmf) {
         this.bmf = bmf;
