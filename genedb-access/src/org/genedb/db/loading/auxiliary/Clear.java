d193525dff6c7df6c4ce5aabd01fc77245ada56d
te3 <te3@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-06-26 12:29:18 +0000

    enable execute() to use the analysisProgram as well as organismCommonName in the sql command used for deleting features
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11203 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/auxiliary/Clear.java
+++ genedb-access/src/org/genedb/db/loading/auxiliary/Clear.java
@@ -1,12 +1,12 @@
 package org.genedb.db.loading.auxiliary;
 
+import org.apache.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-
-import org.apache.log4j.Logger;
 
 public abstract class Clear {
     private static final Logger logger = Logger.getLogger(Clear.class);
@@ -14,18 +14,23 @@ public abstract class Clear {
 
     private Connection conn;
     private String organismCommonName;
-
+    private String analysisProgram;
+    
     protected static <T extends Clear> void main(Class<T> clazz, String[] args) throws Exception {
-        if (args.length != 1) {
-            System.err.println("Usage: java ClearInterPro <organism common name>");
+        if (args.length != 1 && args.length != 2) {
+            System.err.printf("Usage: java %s <organism common name> [<analysis program>]\n", clazz.getName());
             System.exit(1);
         }
         String organismCommonName = args[0];
-        T instance = clazz.getDeclaredConstructor(String.class).newInstance(organismCommonName);
-        instance.clear();
+        String analysisProgram = null;
+        if (args.length > 1) {
+        	analysisProgram = args[1];
+        }
+        T instance = clazz.getDeclaredConstructor(String.class, String.class).newInstance(organismCommonName, analysisProgram);
+        instance.clear();       
     }
-
-    protected Clear(String organismCommonName) throws ClassNotFoundException, SQLException {
+   
+    protected Clear(String organismCommonName, String analysisProgram) throws ClassNotFoundException, SQLException {
         if (organismCommonName.length() == 0)
             throw new IllegalArgumentException("Empty organism name");
         String url = String.format("jdbc:postgresql://%s:%s/%s",
@@ -40,17 +45,22 @@ public abstract class Clear {
 
         this.conn = DriverManager.getConnection(url, username, password);
         this.organismCommonName = organismCommonName;
+        this.analysisProgram = null;
+        if (analysisProgram != null) {
+        	this.analysisProgram = analysisProgram;
+        }
     }
-
-    protected Clear(Connection conn, String organismCommonName) {
+    
+    protected Clear(Connection conn, String organismCommonName, String analysisProgram) {
         this.conn = conn;
         this.organismCommonName = organismCommonName;
-    }
-
+    	this.analysisProgram = analysisProgram;
+    }      
 
     protected void clear() throws SQLException {
+  
         for (DeleteSpec deleteSpec: getDeleteSpecs())
-            deleteSpec.execute();
+        	deleteSpec.execute();     
     }
 
     protected abstract DeleteSpec[] getDeleteSpecs();
@@ -63,10 +73,16 @@ public abstract class Clear {
             this.sql = sql;
         }
 
-        private void execute() throws SQLException {
+        private void execute() throws SQLException {      	
             PreparedStatement st = conn.prepareStatement(sql);
-            try {
-                st.setString(1, organismCommonName);
+            try {         	
+            	if (analysisProgram == null) {
+            		st.setString(1, organismCommonName);
+            	}
+            	else {
+            		st.setString(1, analysisProgram);
+            		st.setString(2, organismCommonName);
+            	}
                 int numberDeleted = st.executeUpdate();
                 logger.info(String.format("Deleted %d %s", numberDeleted, description));
             }
