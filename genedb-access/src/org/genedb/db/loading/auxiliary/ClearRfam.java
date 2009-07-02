361ceaa9d9dae44b36959d330f016aef84752708
nds <nds@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-07-02 12:15:30 +0000

    Corrected SQL so that it works with hsql
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11284 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/auxiliary/ClearRfam.java
+++ genedb-access/src/org/genedb/db/loading/auxiliary/ClearRfam.java
@@ -1,10 +1,11 @@
 package org.genedb.db.loading.auxiliary;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * Deletes any existing rfam results for this organism. At the momemt, we do a text search on the uniquename (Does it have rfam somewhere?)
- * to look for relevant genes and transcripts. Investigate a better way to do.  
+ * Deletes any existing rfam results for the specified organism. At the moment, we do a text search on the uniquename (Does it have rfam somewhere?)
+ * to look for relevant genes and transcripts. (Is there a better way to do this?)  
  * 
  * @author nds
  * */
@@ -17,27 +18,31 @@ public class ClearRfam extends Clear {
     ClearRfam(String organismCommonName, String analysisProgram) throws ClassNotFoundException, SQLException {
         super(organismCommonName, analysisProgram);
     }
+    
+    ClearRfam(Connection conn, String organismCommonName, String analysisProgram) {
+        super(conn, organismCommonName, analysisProgram);
+    }
 
   
     private static final String DELETE_RFAM_FEATURES_SQL
-        = "delete from feature"
-        +" using cvterm feature_type"
-        +" join cv feature_type_cv using (cv_id)"
-        +" join dbxref feature_type_dbxref using (dbxref_id)"
-        +"    , organism"
-        +" where feature_type.cvterm_id = feature.type_id"
+    = "delete from feature"
+        +" where feature_id in ("
+        +" select feature.feature_id"
+        +" from feature"
+        +" join organism on feature.organism_id = organism.organism_id"
+        +" join cvterm feature_type on feature_type.cvterm_id = feature.type_id"
+        +" join cv feature_type_cv on feature_type.cv_id = feature_type_cv.cv_id"
         +" and feature_type_cv.name = 'sequence'"
         +" and feature.uniquename LIKE '%rfam%'"
-        +" and feature.organism_id = organism.organism_id"
-        +" and organism.common_name = ?";
+        +" and organism.common_name = ?)";
     
+  
    
     @Override
     protected DeleteSpec[] getDeleteSpecs() {
-        System.out.println(DELETE_RFAM_FEATURES_SQL);
+    
         return new DeleteSpec[] {
-               
-            new DeleteSpec("Rfam features", DELETE_RFAM_FEATURES_SQL),
+          new DeleteSpec("Rfam features", DELETE_RFAM_FEATURES_SQL),
         };
     }
 }
