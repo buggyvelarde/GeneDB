779578abd78bdf6604e9e69a9cd8e695c181d525
nds <nds@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-07-03 13:04:03 +0000

    Corrected SQL so that it runs on hsql database (no 'using')
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11305 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/auxiliary/ClearHTH.java
+++ genedb-access/src/org/genedb/db/loading/auxiliary/ClearHTH.java
@@ -1,10 +1,9 @@
 package org.genedb.db.loading.auxiliary;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 /* Deletes any existing helix-turn-helix features for the chosen organism */
-
-
 
 public class ClearHTH extends Clear {
     public static void main(String[] args) throws Exception {
@@ -14,24 +13,27 @@ public class ClearHTH extends Clear {
     ClearHTH(String organismCommonName, String analysisProgram) throws ClassNotFoundException, SQLException {
         super(organismCommonName, analysisProgram);
     }
-
-  
+    
+    ClearHTH(Connection conn, String organismCommonName, String analysisProgram) {
+        super(conn, organismCommonName, analysisProgram);
+    }
+    
+    
     private static final String DELETE_HTH_FEATURES_SQL
-        = "delete from feature"
-        +" using cvterm feature_type"
-        +" join cv feature_type_cv using (cv_id)"
-        +" join dbxref feature_type_dbxref using (dbxref_id)"
-        +"    , organism"
-        +" where feature_type.cvterm_id = feature.type_id"
-        +" and feature_type_dbxref.accession = '0001081'" //helix-turn-helix
-        +" and feature_type_cv.name = 'sequence'"
-        +" and feature.organism_id = organism.organism_id"
-        +" and organism.common_name = ?";
+    = "delete from feature where feature_id in ("
+        +" select feature.feature_id from feature"
+        +" join organism on feature.organism_id = organism.organism_id"
+        +" join cvterm on cvterm.cvterm_id = feature.type_id"
+        +" join cv on cv.cv_id = cvterm.cv_id"
+        +" join dbxref on dbxref.dbxref_id=cvterm.dbxref_id "
+        +" and dbxref.accession = '0001081'" //Helix-turn-helix
+        +" and cv.name = 'sequence'"
+        +" and organism.common_name = ?)"; 
+    
     
    
     @Override
     protected DeleteSpec[] getDeleteSpecs() {
-      
         return new DeleteSpec[] {
             new DeleteSpec("Helix turn helix features", DELETE_HTH_FEATURES_SQL),
         };
