67d38870191c24dc345d898c330bdbd5da493411
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-08-19 10:23:39 +0000

    Loading script for GOA
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11575 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ genedb-access/scripts/loaders/goa.sh
@@ -0,0 +1,77 @@
+summary() {
+    echo "load GO terms from a gene association file"
+}
+
+loaderUsage() {
+    cat <<USAGE
+Usage: `basename $0` $1 [options] <file>
+Options:
+  -x goTermErrorsAreNotFatal
+    If this property is set, it is not a fatal error if a GO term mentioned in the
+    input file does not exist.
+USAGE
+    standard_options
+    echo
+}
+
+loaderHelp() {
+    cat <<HELP
+Load GO annotations from a gene association file.
+HELP
+}
+
+orthoparaLoad() {
+    options=''
+    debug=false
+
+    OPTIND=0
+    while getopts "d$stdopts" option; do
+        case "$option" in
+        d)  debug=true
+            ;;
+        x)  case "$OPTARG" in
+            goTermErrorsAreNotFatal)
+                options="$options --go-term-errors-are-not-fatal"
+                ;;
+            *)  loaderUsage >&2
+                exit 1
+                ;;
+            esac
+        *)  process_standard_options "$option"
+            ;;
+        esac
+    done
+    shift $[ $OPTIND - 1 ]
+    
+    if [ -z "$organism" ]; then
+        loaderUsage >&2
+        exit 1
+    fi
+    
+    if [ $# -ne 1 ]; then
+        loaderUsage >&2
+        exit 1
+    fi
+    
+    file="$1"
+    shift
+    
+    if [ ! -e "$file" ]; then
+        echo >&2 "`basename $0`: no such file or directory '$file'"
+        exit 1
+    fi
+    
+    read_password
+
+    if $debug; then
+        echo "Classpath:"
+        echo "$CLASSPATH" | perl -0777 -ne 'for (split(/:/,$_)) {print"\t$_\n"}'
+        set -x
+    fi
+    java -Xmx1G \
+        $database_properties \
+        org.genedb.db.loading.auxiliary.Load goaloader $options "$@"
+    
+    echo "Deleting redundant GO terms"
+    java $database_properties org.genedb.db.loading.auxiliary.DeleteRedundantGOTerms
+}
