b19e721cb1a398dd6409c7b205647f4132d6e780
te3 <te3@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-09-08 13:09:19 +0000

    Persist added featureprops and increment rank.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11732 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/auxiliary/AddFeatureProperty.java
+++ genedb-access/src/org/genedb/db/loading/auxiliary/AddFeatureProperty.java
@@ -19,122 +19,123 @@ import java.util.Set;
 /**
  * Adds feature properties to the polypeptide feature associated with a list of gene or protein systematic ids
  * The systematic ids should be listed, one per line in a file supplied on the command line
- * The type of the features listed (eg gene, polypeptide) can be specified using -Dload.featureType, 
+ * The type of the features listed (eg gene, polypeptide) can be specified using -Dload.featureType,
  * but the default is 'polypeptide' (optional)
- * 
+ *
  * The type of the feature_property is specified using -Dload.propType (required)
- * The value of the feature_property is specified using -Dload.propValue and must be enclosed 
+ * The value of the feature_property is specified using -Dload.propValue and must be enclosed
  * in quotes if it contains spaces (required)
- * 
+ *
  * -Dload.errorsAreNotFatal may be used if the file contains deprecated systematic ids (optional)
- * 
- * Even if the file contains multiple copies of the same systematic id the feature_property will only be 
+ *
+ * Even if the file contains multiple copies of the same systematic id the feature_property will only be
  * added once per feature per load session
- * 
+ *
  * Example of use:
- * ant add-featprops -Dconfig=bigtest2 -Dorganism=Pfalciparum -Dload.featureType=gene -Dload.propType=private 
+ * ant add-featprops -Dconfig=bigtest2 -Dorganism=Pfalciparum -Dload.featureType=gene -Dload.propType=private
  * -Dload.propValue="Tinas private value" -Dload.errorsAreNotFatal=true -Dfile=featureprop.test
- * 
+ *
  * @author te3
  *
  */
 public class AddFeatureProperty extends Loader {
     private static final Logger logger = Logger.getLogger(AddFeatureProperty.class);
-    
-	Boolean errorsAreNotFatal = true;
-	Collection<String> featuresSeen = new HashSet<String>();
-	String featureType = "polypeptide";
-	String propType;
-	String propValue;
-	
+
+    Boolean errorsAreNotFatal = true;
+    Collection<String> featuresSeen = new HashSet<String>();
+    String featureType = "polypeptide";
+    String propType;
+    String propValue;
+
     @Override
     protected Set<String> getOptionNames() {
-	Set<String> options = new HashSet<String>();
-	Collections.addAll(options, "featureType", "propType", "propValue", "errorsAreNotFatal");
+    Set<String> options = new HashSet<String>();
+    Collections.addAll(options, "featureType", "propType", "propValue", "errorsAreNotFatal");
         return options;
     }
     @Override
     protected boolean processOption(String optionName, String optionValue) {
 
-    	if (optionName.equals("featureType")) {
-    		featureType = optionValue;
-    		return true;
-    	}
-    	else if (optionName.equals("propType")) {
-    		propType = optionValue;
-    		return true;
-    	}
-    	else if (optionName.equals("propValue")) {
-    		propValue = optionValue;
-    		return true;
-    	}
-    	else if (optionName.equals("errorsAreNotFatal")) {
-    		errorsAreNotFatal = Boolean.valueOf(optionValue);
-    		return true;
-    	}    	
-    	return false;
+        if (optionName.equals("featureType")) {
+            featureType = optionValue;
+            return true;
+        }
+        else if (optionName.equals("propType")) {
+            propType = optionValue;
+            return true;
+        }
+        else if (optionName.equals("propValue")) {
+            propValue = optionValue;
+            return true;
+        }
+        else if (optionName.equals("errorsAreNotFatal")) {
+            errorsAreNotFatal = Boolean.valueOf(optionValue);
+            return true;
+        }
+        return false;
     }
-    
+
     public void doLoad(InputStream inputStream, Session session) throws IOException {
-    	
-	inputFile file = new inputFile(inputStream);
+
+    inputFile file = new inputFile(inputStream);
 
         int n=1;
         for (featProp prop: file.props()) {
             logger.info(String.format("[%d/%d] Adding feature property for %s '%s'", n++, file.props().size(), featureType, prop.getFeatureUniquename()));
-            loadprop(prop);
+            loadprop(prop, session);
             if (n % 50 == 1) {
                 logger.info("Clearing session");
+                session.flush();
                 session.clear();
             }
         }
     }
-    
-    private void loadprop(featProp prop) {
 
-    	Polypeptide polypeptide;
-    	if (featureType.equals("gene")) {
-    		polypeptide = getPolypeptideForGene(prop.getFeatureUniquename());
-    	}  	
-    	else if (featureType.equals("polypeptide") || featureType.equals("protein")) {
-    		polypeptide = getPolypeptideByMangledName(prop.getFeatureUniquename());
-    	}    	
-    	else {
-    		logger.error(String.format("Feature type should be gene or polypeptide not %s", featureType));
-    		return;
-    	}
-    	
-    	if (polypeptide == null) {
-    		logger.error(String.format("Could not find polypeptide for key '%s'", prop.getFeatureUniquename()));
-    		return;
-    	}
-        
-    	
-    	//Add featureproperties if not already seen this polypeptide
-    	if (!featuresSeen.contains(polypeptide.getUniqueName())) {
-    		//String type = "curation";
-    		//String value = "GO terms evidenced by RCA were predicted using the Extended Similarity Group software (PMID:18655063)";
-       		//String type2 = "private";
-    		//String value2 = "curator_dpd;date_20090805;feat terms added based on reviewed ESG output";   		
-    		addCurations(polypeptide, propType, propValue);   		
-    		featuresSeen.add(polypeptide.getUniqueName());
-    	}
+    private void loadprop(featProp prop, Session session) {
+
+        Polypeptide polypeptide;
+        if (featureType.equals("gene")) {
+            polypeptide = getPolypeptideForGene(prop.getFeatureUniquename());
+        }
+        else if (featureType.equals("polypeptide") || featureType.equals("protein")) {
+            polypeptide = getPolypeptideByMangledName(prop.getFeatureUniquename());
+        }
+        else {
+            logger.error(String.format("Feature type should be gene or polypeptide not %s", featureType));
+            return;
+        }
+
+        if (polypeptide == null) {
+            logger.error(String.format("Could not find polypeptide for key '%s'", prop.getFeatureUniquename()));
+            return;
+        }
+
+
+        //Add featureproperties if not already seen this polypeptide
+        if (!featuresSeen.contains(polypeptide.getUniqueName())) {
+            //String type = "curation";
+            //String value = "GO terms evidenced by RCA were predicted using the Extended Similarity Group software (PMID:18655063)";
+               //String type2 = "private";
+            //String value2 = "curator_dpd;date_20090805;feat terms added based on reviewed ESG output";
+            addCurations(polypeptide, propType, propValue, session);
+            featuresSeen.add(polypeptide.getUniqueName());
+        }
     }
-    
-    private void addCurations(Polypeptide polypeptide, String type, String value) {
-    	
-        int rank = polypeptide.getFeaturePropsFilteredByCvNameAndTermName("genedb_misc", type).size();
+
+    private void addCurations(Polypeptide polypeptide, String type, String value, Session session) {
+
+        int rank = polypeptide.getFeaturePropsFilteredByCvNameAndTermName("genedb_misc", type).size() + 1;
         logger.info(String.format("Adding /%s=\"%s\" with rank %d", type, value, rank));
         try {
-        	polypeptide.addFeatureProp(value, "genedb_misc", type, rank++);
-		} catch (Exception e) {
-			if (errorsAreNotFatal) {
-				logger.error(String.format("Error adding feature prop to feature '%s'", polypeptide.getUniqueName(), e));
-			} else {		
-				throw new RuntimeException("Error adding feature prop", e);
-			}
-		}
-	}
+            session.persist(polypeptide.addFeatureProp(value, "genedb_misc", type, rank++));
+        } catch (Exception e) {
+            if (errorsAreNotFatal) {
+                logger.error(String.format("Error adding feature prop to feature '%s'", polypeptide.getUniqueName(), e));
+            } else {
+                throw new RuntimeException("Error adding feature prop", e);
+            }
+        }
+    }
 }
 
 
@@ -149,7 +150,7 @@ class inputFile {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
-        
+
         int lineNumber = 0;
         while (null != (line = reader.readLine())) { //While not end of file
             if(0 < line.length()){
@@ -160,7 +161,7 @@ class inputFile {
                 featProp prop = new featProp(lineNumber, line);
                 props.add(prop);
             }
-        }          
+        }
     }
 
     public Collection<featProp> props() {
@@ -170,23 +171,23 @@ class inputFile {
 
 /* Each featProp corresponds to a line in the input file */
 class featProp {
-    
+
     private String featureUniquename;
     private int lineNumber;
-    
+
     public featProp(int lineNumber, String row) {
-    	
-    	this.lineNumber = lineNumber;
-    	this.featureUniquename = row;
-    
+
+        this.lineNumber = lineNumber;
+        this.featureUniquename = row;
+
     }
-    
+
     public int getLineNumber() {
         return lineNumber;
     }
-    
+
     public String getFeatureUniquename() {
         return featureUniquename;
     }
-    
+
 }
