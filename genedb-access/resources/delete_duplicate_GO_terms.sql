5e062059385a0d9f21b06b3d7e110a5b61e1b0fb
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-09-09 11:45:30 +0000

    refactor to use better names for table aliases
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11750 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/resources/delete_duplicate_GO_terms.sql
+++ genedb-access/resources/delete_duplicate_GO_terms.sql
@@ -1,37 +1,36 @@
 delete from feature_cvterm
 where feature_cvterm_id in (
-select general.feature_cvterm_id from feature_cvterm general
-   , cv                    
+select second.feature_cvterm_id from feature_cvterm second
+   , cv
    , cvterm evidence_type join cv evidence_type_cv
         on evidence_type.cv_id = evidence_type_cv.cv_id
-   , feature_cvtermprop general_evidence
-   , feature_cvterm specific 
-left join feature_cvtermprop specific_evidence
-    on specific_evidence.feature_cvterm_id = specific.feature_cvterm_id
-                                                                       
-   , cvterm specific_term 
-   , cvterm general_term   
-   , feature               
-where general.cvterm_id = general_term.cvterm_id
-and general_evidence.feature_cvterm_id = general.feature_cvterm_id
-and specific_term.cv_id = cv.cv_id
-and general_term.cv_id = cv.cv_id
+   , feature_cvtermprop second_evidence
+   , feature_cvterm first
+     left join feature_cvtermprop first_evidence
+         on first_evidence.feature_cvterm_id = first.feature_cvterm_id
+   , cvterm first_term
+   , cvterm second_term
+   , feature
+where second.cvterm_id = second_term.cvterm_id
+and second_evidence.feature_cvterm_id = second.feature_cvterm_id
+and first_term.cv_id = cv.cv_id
+and second_term.cv_id = cv.cv_id
 and cv.name in (
       'biological_process'
     , 'molecular_function'
     , 'cellular_component'
-)                         
-and specific.feature_id = feature.feature_id
-and specific.feature_id = general.feature_id
-and general.cvterm_id = general_term.cvterm_id
-and specific.cvterm_id = specific_term.cvterm_id
-and specific.pub_id = general.pub_id
+)
+and first.feature_id = feature.feature_id
+and first.feature_id = second.feature_id
+and second.cvterm_id = second_term.cvterm_id
+and first.cvterm_id = first_term.cvterm_id
+and first.pub_id = second.pub_id
 and evidence_type.name = 'evidence'
 and evidence_type_cv.name = 'genedb_misc'
-and specific_evidence.type_id = evidence_type.cvterm_id
-and general_evidence.type_id = evidence_type.cvterm_id
-and specific.feature_cvterm_id < general.feature_cvterm_id
-and upper(general_evidence.value) = upper(specific_evidence.value)
-and general_term.name = specific_term.name
+and first_evidence.type_id = evidence_type.cvterm_id
+and second_evidence.type_id = evidence_type.cvterm_id
+and first.feature_cvterm_id < second.feature_cvterm_id
+and upper(second_evidence.value) = upper(first_evidence.value)
+and second_term.name = first_term.name
 );
 
