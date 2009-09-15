dd7fd1a9ad1b002f80fccb3fb175bc088d43da42
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-09-15 12:54:29 +0000

    GENEDB-466 Revert to spring 3.0M3
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11778 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/genedb/db/dao/BaseDao.java
+++ genedb-db/src/org/genedb/db/dao/BaseDao.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public class BaseDao {
 
     private static final Logger logger = Logger.getLogger(BaseDao.class);
