80ac5a28418bc19e977ed157fa007032cf7576bb
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-10-02 09:47:19 +0000

    Add support for phylonodeprops to be passed thru' to page
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11933 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/HomepageController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/HomepageController.java
@@ -1,30 +1,17 @@
 package org.genedb.web.mvc.controller;
 
-import static org.genedb.web.mvc.controller.WebConstants.CRUMB;
-import static org.genedb.web.mvc.controller.WebConstants.TAXON_NODE;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.genedb.db.taxon.TaxonNameType;
 import org.genedb.db.taxon.TaxonNode;
-import org.genedb.db.taxon.TaxonNodeArrayPropertyEditor;
-
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.context.ApplicationContextAware;
+import org.genedb.db.taxon.TaxonNodeList;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.google.common.collect.Maps;
 
 
 /**
@@ -36,111 +23,52 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Controller
 @RequestMapping("/Homepage")
-public class HomepageController {
+public class HomepageController extends BaseController {
 
-    public static final String DUMMY_UNKNOWN = "XXX";
+    private static String HOMEPAGE = "jsp:homepages/";
+    private static final String DEFAULT_HOMEPAGE = HOMEPAGE + "frontPage";
+    private static final String DEFAULT_SINGLE = HOMEPAGE + "singleOrg";
+    private static final String DEFAULT_GROUP = HOMEPAGE + "group";
 
-    private static String HOMEPAGE = "homepages/";
-    private static String DEFAULT_HOMEPAGE = HOMEPAGE + "frontPage";
-    private static String DEFAULT_STYLE = "childListing"; // FIXME
-
-    private TaxonNodeArrayPropertyEditor taxonNodeArrayPropertyEditor;
+    private static final String APP_PREFIX = "app_www_homePage_";
 
     protected final Logger logger = Logger.getLogger(this.getClass());
 
+
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        String originalOrg = ServletRequestUtils.getStringParameter(request, "organism", DUMMY_UNKNOWN);
-
-//        TaxonNodeArrayHolder tnah = new TaxonNodeArrayHolder();
-//
-//        ServletRequestDataBinder binder = new ServletRequestDataBinder(tnah);
-//        binder.initDirectFieldAccess();
-//        binder.registerCustomEditor(TaxonNode[].class, taxonNodeArrayPropertyEditor);
-//        binder.bind(request);
-//        BindingResult bindResult = binder.getBindingResult();
-
-        String organism = null;
-        String pageName = null;
-
-//        if (bindResult.hasGlobalErrors()) {
-//          // TODO Add error message
-//            logger.debug("Binding errors - go home");
-//            return new ModelAndView(DEFAULT_HOMEPAGE);
-//        }
-
-//        TaxonNode[] nodes = tnah.org;
-//        if (nodes == null || nodes.length == 0) {
-//            logger.debug("No taxon nodes - go home");
-//            return new ModelAndView(DEFAULT_HOMEPAGE);
-//        }
-//        if (nodes.length > 1) {
-//            // TODO Add error message
-//            logger.debug("Got too many taxon nodes");
-//            return new ModelAndView(DEFAULT_HOMEPAGE);
-//        }
-
-//        TaxonNode node = nodes[0];
-        String viewName = DEFAULT_HOMEPAGE;
-        //String viewName = HOMEPAGE + DEFAULT_STYLE;
-        if (!originalOrg.equals(DUMMY_UNKNOWN)) {
-            pageName = originalOrg;
-            organism = originalOrg;
-        //pageName = node.getName(TaxonNameType.HTML_SHORT); // TODO Pass in taxon node
-        //logger.warn(String.format("Retrieved a page name of '%s'", pageName));
-        //organism = node.getName(TaxonNameType.LABEL);
-        //logger.warn(String.format("Retrieved an organism name of '%s'", pageName));
-        }
-
-//        Map props = node.getAppDetails("WEB");
-//        if (props.containsKey("HOMEPAGE_STYLE")) {
-//            viewName = HOMEPAGE + props.get("HOMEPAGE_STYLE");
-//        }
-
-//      List<NewsItem> news = checkNews();
-//      if (news.size() > 0) {
-//          model.put("news", news);
-//      }
-
-        ModelAndView mav = new ModelAndView(viewName);
-        //mav.addObject(TAXON_NODE, node);
-        mav.addObject(CRUMB, "Homepage");
-        if (organism != null) {
-            mav.addObject("organism", organism);
-        }
-        if (pageName != null) {
-            mav.addObject("pageName", pageName);
-        } else {
-            mav.addObject("pageName", "GeneDB");
-        }
-        mav.addObject("taxonNodeName", organism);
-
-        return mav;
+    public ModelAndView displayRootHomepage() throws Exception {
+        TaxonNodeList root = new TaxonNodeList();
+        root.add(getTaxonNodeManager().getTaxonNodeForLabel("Root"));
+        return displayHomepage(root);
     }
 
+    @RequestMapping(method = RequestMethod.GET, value="/{originalOrg}")
+    public ModelAndView displayHomepage(@PathVariable("originalOrg") TaxonNodeList originalOrg) throws Exception {
 
-    private List<NewsItem> checkNews(TaxonNode node) {
-        return new ArrayList<NewsItem>(0);
-    }
+        if (originalOrg.getNodes().size() != 1) {
+            return new ModelAndView(DEFAULT_HOMEPAGE);
+        }
 
-    public void setTaxonNodeArrayPropertyEditor(
-            TaxonNodeArrayPropertyEditor taxonNodeArrayPropertyEditor) {
-        this.taxonNodeArrayPropertyEditor = taxonNodeArrayPropertyEditor;
+        TaxonNode node = originalOrg.getNodes().get(0); // Ignore more than one taxon request and use the first
+
+        Map<String, String> map = Maps.newHashMap();
+
+        for (Map.Entry<String, String> entry : node.getAppDetails().entrySet()) {
+        	if (entry.getKey().startsWith(APP_PREFIX)) {
+        		map.put(entry.getKey().substring(APP_PREFIX.length()), entry.getValue());
+        	}
+        }
+
+        if (node.isRoot()) {
+            return new ModelAndView(DEFAULT_HOMEPAGE);
+        }
+
+        if (node.isLeaf()) {
+            return new ModelAndView(DEFAULT_SINGLE, map);
+        }
+
+        return new ModelAndView(DEFAULT_GROUP, map);
     }
 
 }
 
-
-/**
- * Simple struct to make databinding easier
- *
- * @author A. Tivey (art)
- */
-class TaxonNodeArrayHolder {
-
-    /**
-     * Field, directly accessed by databinder and for reading
-     */
-    TaxonNode[] org;
-}
