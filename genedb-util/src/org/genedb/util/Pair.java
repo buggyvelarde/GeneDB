8e09b03cd8f7dea28d8e79360f4b4107088831a0
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-10-05 09:47:34 +0000

    Make constructor public
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@11957 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-util/src/org/genedb/util/Pair.java
+++ genedb-util/src/org/genedb/util/Pair.java
@@ -5,7 +5,7 @@ public class Pair<S, T> {
     private S first;
     private T second;
 
-    Pair(S first, T second) {
+    public Pair(S first, T second) {
         this.first = first;
         this.second = second;
     }
