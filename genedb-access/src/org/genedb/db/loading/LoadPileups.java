af1d6aee410461e6eb3f063d1c4373b66dd829d3
rn2 <rn2@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-10-23 10:42:18 +0000

    Pileup loader
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12173 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-access/src/org/genedb/db/loading/LoadPileups.java
+++ genedb-access/src/org/genedb/db/loading/LoadPileups.java
@@ -47,12 +47,14 @@ import java.util.Map;
  */
 public class LoadPileups extends FileProcessor {
     private static final Logger logger = Logger.getLogger(LoadPileups.class);
+    private static String dbSchema;
     public static void main(String[] args) throws MissingPropertyException, IOException, ParsingException, SQLException {
         if (args.length > 0) {
             logger.warn("Ignoring command-line arguments");
         }
         String inputDirectory = getRequiredProperty("load.inputDirectory");
         String fileNamePattern = getPropertyWithDefault("load.fileNamePattern", ".*\\.pileup");
+        dbSchema=getRequiredProperty("load.dbSchema");
         LoadPileups loadPileups = new LoadPileups();
         loadPileups.processFileOrDirectory(inputDirectory, fileNamePattern);
     }
@@ -66,7 +68,7 @@ public class LoadPileups extends FileProcessor {
     @Override
     protected void processFile(File inputFile, Reader reader) throws IOException, ParsingException {
         PileupFile pileupFile = new PileupFile(inputFile, reader);
-        loader.load(pileupFile);
+        loader.load(pileupFile,dbSchema);
     }
 }
 class PileupFile {
@@ -197,7 +199,7 @@ class PileupsLoader {
 
     
   
-    public void load(PileupFile pileupFile) throws DataError {
+    public void load(PileupFile pileupFile,String dbSchema) throws DataError {
 
         // Insert into variation.Analysis table
         Date analysistimestamp;
@@ -216,7 +218,7 @@ class PileupsLoader {
         throw new DataError(errorMessage);
         }
         int n=simpleJdbcTemplate.update(
-            "insert into variation.Analysis("+
+            "insert into "+dbSchema+".Analysis("+
             "program,programversion,timeexecuted"+
             ") values ("+
             "?,?,?"+
@@ -227,10 +229,10 @@ class PileupsLoader {
         }
 
         long analysisId = (Long) simpleJdbcTemplate.queryForMap(
-            "select currval('variation.analysis_analysis_id_seq'::regclass) as analysis_id")
+            "select currval('"+dbSchema+".analysis_analysis_id_seq'::regclass) as analysis_id")
             .get("analysis_id");
 
-        // Now insert into variation.AnalysisProp
+        // Now insert into "+dbSchema+".AnalysisProp
         
         int cvId = (Integer) simpleJdbcTemplate.queryForMap(
             "select cv_id from cv where name='genedb_misc'")
@@ -246,7 +248,7 @@ class PileupsLoader {
 
 
         simpleJdbcTemplate.batchUpdate(
-            "insert into variation.AnalysisProp("+
+            "insert into "+dbSchema+".AnalysisProp("+
             " analysis_id, type_id, value"+
             ") values ("+
             "?,"+
@@ -277,7 +279,7 @@ class PileupsLoader {
             String pileupFeatureUniqueName = "Pileup_"+pileupFile.metaline().get_Strain_ID()+"_"+pileupFile.metaline().get_Strain_Sample_ID()+"_"+analysisId+"_"+line.lineNumber;
 
             n = simpleJdbcTemplate.update(
-                "insert into variation.Feature("+
+                "insert into "+dbSchema+".Feature("+
                 " organism_id,uniquename,type_id,is_analysis,is_obsolete,timeaccessioned,timelastmodified"+
                 ") values ("+
                 "?,?,(select cvterm_id as type_id from cvterm where name='variation_call'),true,false,?,?"+
@@ -288,7 +290,7 @@ class PileupsLoader {
             }
 
             long featureId = (Long) simpleJdbcTemplate.queryForMap(
-            "select currval('variation.feature_feature_id_seq'::regclass) as feature_id")
+            "select currval('"+dbSchema+".feature_feature_id_seq'::regclass) as feature_id")
             .get("feature_id");
 
             // Insert in to featureloc
@@ -303,7 +305,7 @@ class PileupsLoader {
             Integer fmax = fmin+1;
 
             n = simpleJdbcTemplate.update(
-                "insert into variation.FeatureLoc("+
+                "insert into "+dbSchema+".FeatureLoc("+
                 "feature_id,srcfeature_id,fmin,is_fmin_partial,fmax,is_fmax_partial,strand,phase,locgroup,rank"+
                 ") values ("+
                 "?,(select feature_id from feature where uniquename=?),?,?,?,?,?,?,?,?"+
@@ -320,7 +322,7 @@ class PileupsLoader {
                 String headerfield=headerIterator.next();
                 dataStr+="("+headerfield+","+datafield+")";
                 n = simpleJdbcTemplate.update(
-                    "insert into variation.FeatureProp("+
+                    "insert into "+dbSchema+".FeatureProp("+
                     "feature_id,type_id,value,rank"+
                     ") values ("+
                     "?,(select cvterm_id from cvterm where name=? and cv_id=?),?,?"+
@@ -335,7 +337,7 @@ class PileupsLoader {
          // now associate this feature with the Analysis using AnalysisFeature record
 
             n = simpleJdbcTemplate.update(
-                "insert into variation.AnalysisFeature("+
+                "insert into "+dbSchema+".AnalysisFeature("+
                 "feature_id,analysis_id"+
                 ") values ("+
                 "?,?"+
