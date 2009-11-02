66dbf73f001cc0e2344922b03941b9c231c8a9f2
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-11-02 20:20:47 +0000

    GENEDB-508 Extend so you can pass in baseUrl
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12255 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/tags/db/HomepageSelectTag.java
+++ genedb-web/src/org/genedb/web/tags/db/HomepageSelectTag.java
@@ -11,12 +11,15 @@ import javax.servlet.jsp.JspWriter;
 
 public class HomepageSelectTag extends AbstractHomepageTag {
 
+    private String baseUrl;
+    private boolean leafOnly = false;
+
     @Override
     protected void display(TaxonNode root, JspWriter out) throws IOException {
         List<TaxonNode> nodes = new ArrayList<TaxonNode>();
         getAllChildren(root, nodes);
 
-        out.write("<select name=\"organism\" onChange=\"document.location.href='/Homepage/\'+this.value\">");
+        out.write("<select name=\"organism\" onChange=\"document.location.href='"+baseUrl+"\'+this.value\">");
         for (TaxonNode node : nodes) {
             out.write("<option value=\"");
             out.write(node.getLabel());
@@ -28,10 +31,27 @@ public class HomepageSelectTag extends AbstractHomepageTag {
     }
 
     private void getAllChildren(TaxonNode node, List<TaxonNode> list) {
-        list.add(node);
+        if (node.isLeaf()) {
+            if (node.isPopulated()) {
+                list.add(node);
+            }
+        } else {
+            if (!leafOnly && node.isChildrenPopulated()) {
+                list.add(node);
+            }
+        }
+
         for (TaxonNode child : node.getChildren()) {
             getAllChildren(child, list);
         }
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public void setLeafOnly(boolean leafOnly) {
+        this.leafOnly = leafOnly;
     }
 
 }
