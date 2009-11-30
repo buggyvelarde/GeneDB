b7ffb77a30ec75beb636484223ca8204af5a11d1
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-11-30 13:08:12 +0000

    GENEDB-497 No need to store allNames when not debugging
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12492 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-db/src/org/gmod/schema/mapped/Feature.java
+++ genedb-db/src/org/gmod/schema/mapped/Feature.java
@@ -767,7 +767,7 @@ public abstract class Feature implements java.io.Serializable, HasPubsAndDbXRefs
      * @return
      */
     @Transient
-    @Field(name = "allNames", index = Index.TOKENIZED, store = Store.YES)
+    @Field(name = "allNames", index = Index.TOKENIZED, store = Store.NO)
     @Analyzer(impl = AllNamesAnalyzer.class)
     String getAllNames() {
         StringBuilder allNames = new StringBuilder();
