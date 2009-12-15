c4026d6c2890443515068df3735c9a9b8e13ef43
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-12-15 11:46:53 +0000

    Update to Matt's list
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12690 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/cleanup/01.delete-unpublished.sql
+++ sql/cleanup/01.delete-unpublished.sql
@@ -3,13 +3,13 @@ using organism
 where feature.organism_id = organism.organism_id
 and (
         organism.genus = 'Staphylococcus'
-        and organism.species = in ('aureus (EMRSA15)', 'aureus (LGA251)')
+        and organism.species = in ('aureus (EMRSA15)', 'aureus (LGA251)', 'aureus TW20')
     ) or (
         organism.genus = 'Plasmodium'
         and organism.species in ('berghei', 'berghei (3x coverage)')
     ) or (
         organism.genus = 'Streptococcus'
-        and organism.species in ('pneumoniae TIGR4', 'pneumoniae D39', 'pneumoniae OXC141')
+        and organism.species in ('pneumoniae TIGR4', 'pneumoniae D39', 'pneumoniae OXC141', 'pneumoniae ATCC700669')
     )
 )
 ;
