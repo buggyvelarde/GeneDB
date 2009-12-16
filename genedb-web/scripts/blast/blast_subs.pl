366653e99e3222ef158d50696914f00cc58dc932
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-12-16 12:32:03 +0000

    Make OmniBLAST entries for all proteins and all transcripts, rather than one for each organism
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12723 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/scripts/blast/blast_subs.pl
+++ genedb-web/scripts/blast/blast_subs.pl
@@ -20,6 +20,7 @@ close $f_dbs;
 
 open my $f_organism_names, "<", "organisms.tsv"
     or die "Failed to open organisms.tsv: $!\n";
+my @common_names;
 while (<$f_organism_names>) {
     chomp;
     our ($common_name, $genus, $species) = split /\t/, $_;
@@ -27,8 +28,45 @@ while (<$f_organism_names>) {
     
     next unless exists $organisms{$common_name};
     
+    push @common_names, $common_name;
+    
     my $text = $template;
     $text =~ s/\$\{(common_name(_lc)?|genus|species)\}/no strict "refs"; $$1/eg;
     print $text, "\n\n";
 }
 close $f_organism_names;
+
+
+my ($transcript_databases, $protein_databases) = ("", "");
+for my $common_name (@common_names) {
+    $transcript_databases .= qq(                             "genedb/GeneDB_${common_name}_Genes",);
+    $protein_databases    .= qq(                             "genedb/GeneDB_${common_name}_Proteins",);
+}
+
+print <<END;
+# OmniBLAST
+'genedb_proteins' => {
+        'home'          =>  'http://beta.genedb.org/Homepage',
+        'name'         =>  'GeneDB proteins',
+        'action'      =>  '/blast/blast_server',
+        'prologue'      =>  qq(
+These databases contain the polypeptide sequences of all the GeneDB organisms.
+        ),
+
+     'databases'     =>  [
+$protein_databases
+         ],
+},
+'genedb_transcripts' => {
+        'home'          =>  'http://beta.genedb.org/Homepage',
+        'name'         =>  'GeneDB transcripts',
+        'action'      =>  '/blast/blast_server',
+        'prologue'      =>  qq(
+These databases contain the transcript sequences of all the GeneDB organisms.
+        ),
+
+     'databases'     =>  [
+$transcript_databases
+         ],
+},
+END
