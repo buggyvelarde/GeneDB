0a1904945fe926165ac8b37ac6b8211eaa56ca07
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-12-16 03:18:36 +0000

    Use latest organisms
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12710 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/mvc/controller/ClassicCompatabilityController.java
+++ genedb-web/src/org/genedb/web/mvc/controller/ClassicCompatabilityController.java
@@ -2,63 +2,111 @@ package org.genedb.web.mvc.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
-/**
- *
- *
- * @author Adrian Tivey
- */
+
 @Controller
 @RequestMapping("/genedb/")
 public class ClassicCompatabilityController {
 
-    Logger logger = Logger.getLogger(ClassicCompatabilityController.class);
+    private static final String WWW = "redirect:http://www.genedb.org/";
 
-    @RequestMapping(method=RequestMethod.GET, value="organisms")
-    public ModelAndView listOrganisms() {
+    private static final String CLASSIC = "redirect:http://classic.genedb.org/";
 
-        return new ModelAndView();
+    private static final String CLASSIC_FULL = CLASSIC + "genedb/";
+
+    private static Set<String> classicOnly = Sets.newHashSet();
+
+    // The organismprop have proved too flakey so hardcode here for now
+    private static Map<String, String> classicMap = Maps.newHashMap();
+
+    static {
+        classicOnly.add("annulata");
+        classicOnly.add("asp");
+        classicOnly.add("cdubliniensis");
+        classicOnly.add("cerevisiae");
+        classicOnly.add("cjejuni");
+        classicOnly.add("dicty");
+        classicOnly.add("ehistolytica");
+        classicOnly.add("ehuxleyi");
+        classicOnly.add("glossina");
+        classicOnly.add("llongipalpis");
+        classicOnly.add("pberghei");
+        classicOnly.add("pert");
+        classicOnly.add("pombe");
+
+        classicMap.put("etenella", "Etenella");
+        classicMap.put("ncaninum", "Ncaninum");
+        classicMap.put("pchabaudi", "Pchabaudi");
+        classicMap.put("malaria", "Pfalciparum");
+        classicMap.put("pknowlesi", "Pknowlesi");
+        classicMap.put("tbrucei427", "Tbruceibrucei427");
+        classicMap.put("lbraziliensis", "Lbraziliensis");
+        classicMap.put("linfantum", "Linfantum");
+        classicMap.put("leish", "Lmajor");
+        classicMap.put("tryp", "Tbruceibrucei927");
+        classicMap.put("tcruzi", "Tcruzi");
+        classicMap.put("tcongolense", "Tcongolense");
+        classicMap.put("tgambiense", "Tbruceigambiense");
+        classicMap.put("tvivax", "Tvivax");
+        classicMap.put("smansoni", "Smansoni");
+        classicMap.put("bronchi", "Bbronchiseptica");
+        classicMap.put("bfragilis", "Bfragilis_NCTC9343");
+        classicMap.put("parapert", "Bparapertussis");
+        classicMap.put("bpseudomallei", "Bpseudomallei");
+        classicMap.put("cabortus", "Cabortus");
+        classicMap.put("diphtheria", "Cdiphtheriae");
+        classicMap.put("ecarot", "Ecarotovora");
+        classicMap.put("rleguminosarum", "Rleguminosarum");
+        classicMap.put("saureusMRSA", "Saureus_MRSA252");
+        classicMap.put("saureusMSSA", "Saureus_MSSA4776");
+        classicMap.put("scoelicolor", "Scoelicolor");
+        classicMap.put("spyogenes", "Spyogenes");
+        classicMap.put("styphi", "Styphi");
+    }
+
+    private Logger logger = Logger.getLogger(ClassicCompatabilityController.class);
+
+
+    // Homepage
+    @RequestMapping(method=RequestMethod.GET, value="/genedb/${nickname}/")
+    public String viewHomepage(@PathVariable("nickname") String nickname) {
+        if (classicOnly.contains(nickname)) {
+            return CLASSIC_FULL + nickname + "/";
+        }
+        return WWW + "Homepage/" + classicMap.get(nickname);
     }
 
 
-    @RequestMapping(method=RequestMethod.GET, params="fulltext/gene/${search}/organism")
-    public ModelAndView deleteHistoryItem(HttpServletRequest request,HttpServletResponse response,
-            @RequestParam("historyItem") int historyItem) {
-
-//        HttpSession session = request.getSession(false);
-//        HistoryManager historyManager = historyManagerFactory.getHistoryManager(session);
-//        logger.info("Removing item from history");
-//        historyManager.removeItem(historyItem, historyManager.getVersion());
-        return new ModelAndView("redirect:/History/View");
+    // Blast page
+    @RequestMapping(method=RequestMethod.GET, value="/genedb/${nickname}/blast.jsp")
+    public String viewBlastPage(@PathVariable("nickname") String nickname) {
+        if (classicOnly.contains(nickname)) {
+            return CLASSIC + "genedb/" + nickname + "/blast.jsp";
+        }
+        return WWW + "Homepage/" + classicMap.get(nickname);
     }
 
 
-    @RequestMapping(method=RequestMethod.GET, value="/History")
-    public ModelAndView listHistory(HttpServletRequest request,
-            HttpServletResponse response) {
-//        HttpSession session = request.getSession(false);
-//        if (session == null) {
-//            // No session
-//            response.setStatus(SESSION_FAILED_ERROR_CODE);
-//            return null;
-//        }
-//
-//        HistoryManager historyManager = historyManagerFactory.getHistoryManager(session);
-//
-        Map<String,Object> model = new HashMap<String,Object>();
-//        model.put("items", historyManager.getHistoryItems());
+    // Canonical gene
+    @RequestMapping(method=RequestMethod.GET, value="/genedb/Search")
+    public String viewGenePage(
+            @RequestParam("name") String name,
+            @RequestParam("organism") String nickname) {
 
-        return new ModelAndView("history/list", model);
+        if (classicOnly.contains(nickname)) {
+            return CLASSIC_FULL + "Search?name=" + name + "&organism=" + nickname;
+        }
+        return WWW + "gene/" + name;
     }
 
 }
