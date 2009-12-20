f3b09c004153595940c2e680e4d260511c4c6ae2
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-12-20 18:51:33 +0000

    OPEN - issue GENEDB-632: /JBrowse doesn't need controller https://developer.genedb.org/jira/browse/GENEDB-632
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12772 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-web/src/org/genedb/web/tags/db/HomepageSelectTag.java
+++ genedb-web/src/org/genedb/web/tags/db/HomepageSelectTag.java
@@ -5,9 +5,13 @@ import org.genedb.db.taxon.TaxonNode;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.servlet.jsp.JspWriter;
+
+import static org.genedb.db.taxon.TaxonNameType.FULL;
 
 public class HomepageSelectTag extends AbstractHomepageTag {
 
@@ -15,22 +19,30 @@ public class HomepageSelectTag extends AbstractHomepageTag {
 
     private String baseUrl;
     private boolean leafOnly = false;
+    private String suffix = "";
     private String title = DEFAULT_TITLE;
 
     @Override
     protected void display(TaxonNode root, JspWriter out) throws IOException {
         List<TaxonNode> nodes = new ArrayList<TaxonNode>();
         getAllChildren(root, nodes);
-        
-        
-        
-        out.write(String.format("<select name=\"organism\" onChange=\"if (this.selectedIndex != 0) { document.location.href='%s'+this.value ;}\">", baseUrl));
+
+        if (leafOnly) {
+            Collections.sort(nodes, new Comparator<TaxonNode>() {
+                @Override
+                public int compare(TaxonNode tn1, TaxonNode tn2) {
+                    return tn1.getName(FULL).compareTo(tn2.getName(FULL));
+                }
+
+            });
+        }
+
+        out.write(String.format("<select name=\"organism\" onChange=\"if (this.selectedIndex != 0) { document.location.href='%s'+this.value+'%s' ;}\">", baseUrl, suffix));
         if (this.title != null) {
             //out.write(String.format("<option value=\"%s\" disabled=\"disabled\">%s</option>", "none", title));
             out.write(String.format("<option value=\"%s\">%s</option>", "none", title));
         }
         for (TaxonNode node : nodes) {
-        	
             out.write(String.format("<option value=\"%s\">%s</option>", node.getLabel(), node.getName(TaxonNameType.FULL)));
         }
         out.write("</select>");
@@ -46,7 +58,6 @@ public class HomepageSelectTag extends AbstractHomepageTag {
                 list.add(node);
             }
         }
-
         for (TaxonNode child : node.getChildren()) {
             getAllChildren(child, list);
         }
@@ -62,6 +73,10 @@ public class HomepageSelectTag extends AbstractHomepageTag {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
     }
 
 }
