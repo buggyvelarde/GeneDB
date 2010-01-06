5aa9810670d330a3f448a788fd4fce241c310188
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-01-06 21:36:55 +0000

    SVN rationalise
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12835 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- genedb-query/src/org/genedb/query/sql/SqlQuery.java
+++ genedb-query/src/org/genedb/query/sql/SqlQuery.java
@@ -17,47 +17,47 @@ import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 /**
  * Query that retrieves information from a database
- * 
+ *
  * @author art
  */
 public class SqlQuery extends AbstractQuery implements DataSourceAware {
 
     protected String sql;
     protected DataSource dataSource;
-    
+
     /**
-     * The args that will be injected into the sql statement placeholders ('?'). 
+     * The args that will be injected into the sql statement placeholders ('?').
      */
     protected Object[] args;
-    
+
     private static final Logger logger = Logger.getLogger(SqlQuery.class);
-    
+
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    
+
     public void setSql(String sql) {
         this.sql = sql;
     }
-    
+
     /**
-     * A setter for the args object. It's protected so that the subclasses have to call it. 
-     * 
+     * A setter for the args object. It's protected so that the subclasses have to call it.
+     *
      * @param args
      */
     protected void setArgs(Object[] args)
     {
     	this.args = args;
     }
-    
-    
+
+
     public Object[] getArgs()
     {
     	return args.clone();
     }
-    
-   
-    
+
+
+
     /**
      * @param args
      */
@@ -82,15 +82,15 @@ public class SqlQuery extends AbstractQuery implements DataSourceAware {
     public Result process() {
         final SimpleListResult slr = new SimpleListResult();
         JdbcTemplate jt = new JdbcTemplate(dataSource);
-        
+
         jt.query(sql, args,
             new RowCallbackHandler() {
             public void processRow(ResultSet rs) throws SQLException {
-            	ResultSetMetaData meta = rs.getMetaData(); 
+            	ResultSetMetaData meta = rs.getMetaData();
             	int colNum = meta.getColumnCount();
-            	
+
             	Object[] result = new Object[colNum];
-            	
+
             	// gv1 notes that this doesn't take into account column names
             	for (int i = 1; i <= colNum;i++)
             	{
@@ -102,15 +102,15 @@ public class SqlQuery extends AbstractQuery implements DataSourceAware {
         );
         return slr;
     }
-    
+
     /**
-     * 
+     *
      * Will run the query using the supplied callBack.
-     * 
+     *
      * @param callBack
      * @throws Exception
      */
-	public void processCallBack(RowCallbackHandler callBack) 
+	public void processCallBack(RowCallbackHandler callBack)
 	{
 		JdbcTemplate jt = new JdbcTemplate(dataSource);
 		logger.debug(args);
@@ -118,15 +118,15 @@ public class SqlQuery extends AbstractQuery implements DataSourceAware {
 		jt.query(sql, args, callBack);
 	}
 
-    
+
 //    @Override
 //    public void writeSpringBean(PrintWriter pw) {
-//        pw.println("\tGot a sql query"); 
+//        pw.println("\tGot a sql query");
 //        addProperty(pw, "description", getDescription());
 //        addProperty(pw, "help", getHelp());
 //        addProperty(pw, "sql", getSql());
 //    }
-    
+
 //    private void addProperty(PrintWriter pw, String key, String value) {
 //        pw.println("\t\t<property name=\""+key+"\" value=\""+value+"\" />");
 //    }
@@ -144,6 +144,6 @@ public class SqlQuery extends AbstractQuery implements DataSourceAware {
 
     public void setIndex(int index) {
     // TODO Auto-generated method stub
-    
+
     }
 }
