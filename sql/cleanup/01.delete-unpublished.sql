742980ca3945731d05e0ccf09d6bcff9a42ef708
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-01-06 12:03:52 +0000

    Prepare for pberghei release
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12819 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/cleanup/01.delete-unpublished.sql
+++ sql/cleanup/01.delete-unpublished.sql
@@ -2,13 +2,13 @@ delete from feature
 using organism
 where feature.organism_id = organism.organism_id
 and (
-        (  
+        (
             organism.genus = 'Staphylococcus'
             and organism.species in ('aureus (EMRSA15)', 'aureus (LGA251)', 'aureus (TW20)')
         )
         or (
             organism.genus = 'Plasmodium'
-            and organism.species in ('berghei', 'berghei (3x coverage)')
+            and organism.species in ('berghei (3x coverage)')
         )
         or (
             organism.genus = 'Streptococcus'
