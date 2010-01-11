569e117f37689ef3304033b2a805a945944ee1cf
rh11 <rh11@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-01-11 12:58:37 +0000

    Copy test/org/genedb/querying from genedb-query into ngxx
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@12923 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ ngxx/test/org/genedb/querying/tmpquery/ProteinMassQueryTest.java
@@ -0,0 +1,33 @@
+package org.genedb.querying.tmpquery;
+
+import static org.junit.Assert.*;
+
+import org.genedb.querying.core.QueryException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:testContext.xml"})
+public class ProteinMassQueryTest {
+
+    @Autowired
+    private ProteinMassQuery proteinMassQuery;
+
+   
+    @SuppressWarnings("unchecked")
+    @Test
+    public void basic() throws QueryException {
+
+        proteinMassQuery.setMin(10);
+        proteinMassQuery.setMax(11);
+        List results = proteinMassQuery.getResults();
+        assertEquals(2542, results.size());
+    }
+
+}
---
+++ ngxx/test/org/genedb/querying/tmpquery/SimpleNameQueryTest.java
@@ -0,0 +1,32 @@
+package org.genedb.querying.tmpquery;
+
+import static org.junit.Assert.*;
+
+import org.genedb.querying.core.QueryException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:testContext.xml"})
+public class SimpleNameQueryTest {
+
+    @Autowired
+    private SimpleNameQuery simpleNameQuery;
+
+   
+    @SuppressWarnings("unchecked")
+    @Test
+    public void basic() throws QueryException {
+
+        simpleNameQuery.setSearch("Tb927.1.*");
+        List results = simpleNameQuery.getResults();
+        assertEquals(522, results.size());
+    }
+
+}
