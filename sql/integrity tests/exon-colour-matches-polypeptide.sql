4b2f05c79516320fabc084bf4218eef228face0a
nds <nds@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-02-02 17:36:26 +0000

    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@13163 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/integrity tests/exon-colour-matches-polypeptide.sql
+++ sql/integrity tests/exon-colour-matches-polypeptide.sql
@@ -5,7 +5,6 @@
 
 select organism.common_name as organism
      , transcript.uniquename as transcript
-     , transcript_exon.rank
      , exon.uniquename as exon
      , exon_colour.featureprop_id as exon_colour_featureprop_id
      , exon_colour.value as exon_colour
@@ -14,24 +13,22 @@ select organism.common_name as organism
      , product.name as product
 from feature transcript
 join organism using (organism_id)
-join feature_relationship transcript_exon on transcript_exon.object_id = transcript.feature_id
-join feature exon on transcript_exon.subject_id = exon.feature_id
-join feature_relationship transcript_polypeptide on transcript_polypeptide.object_id = transcript.feature_id
-join feature polypeptide on transcript_polypeptide.subject_id = polypeptide.feature_id
+join feature_relationship exon_transcript on exon_transcript.object_id = transcript.feature_id
+join feature exon on exon_transcript.subject_id = exon.feature_id
+join feature_relationship polypeptide_transcript on polypeptide_transcript.object_id = transcript.feature_id
+join feature polypeptide on polypeptide_transcript.subject_id = polypeptide.feature_id
 join featureprop exon_colour on exon_colour.feature_id = exon.feature_id
 join featureprop polypeptide_colour on polypeptide_colour.feature_id = polypeptide.feature_id
 left join feature_cvterm polypeptide_product on polypeptide_product.feature_id = polypeptide.feature_id
 left join cvterm product on polypeptide_product.cvterm_id = product.cvterm_id
-where transcript.type_id = (
+where transcript.type_id in (
+                  select cvterm_id 
+                  from cvterm
+                  where name in ('snRNA', 'tRNA', 'transcript', 'pseudogenic_transcript', 'rRNA', 'mRNA'))
+and   exon.type_id in (
                   select cvterm.cvterm_id
-                  from cvterm join cv on cvterm.cv_id = cv.cv_id
-                  where cv.name = 'sequence'
-                  and cvterm.name = 'mRNA')
-and   exon.type_id = (
-                  select cvterm.cvterm_id
-                  from cvterm join cv on cvterm.cv_id = cv.cv_id
-                  where cv.name = 'sequence'
-                  and cvterm.name = 'exon')
+                  from cvterm 
+                  where cvterm.name in ('exon', 'pseudogenic_exon'))
 and   polypeptide.type_id = (
                   select cvterm.cvterm_id
                   from cvterm join cv on cvterm.cv_id = cv.cv_id
