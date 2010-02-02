6e80204c8c6aae741d2905365d285b99b69d5b6d
nds <nds@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-02-02 11:24:06 +0000

    test to see if all exons have the same colour
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@13144 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ sql/integrity tests/all_exons_have_same_colour.sql
@@ -0,0 +1,28 @@
+#This test checks if all the exons for a given transcript have the same rank=0 colour.
+
+select organism.common_name as organism
+     , transcript.feature_id as transcript_id
+     , transcript.uniquename as transcript_name
+     , count(distinct exon_colour.value) as number_of_exon_colours
+from feature transcript
+join feature_relationship exon_transcript on exon_transcript.object_id = transcript.feature_id
+join feature exon on exon_transcript.subject_id = exon.feature_id
+join featureprop exon_colour on exon_colour.feature_id = exon.feature_id
+join organism on transcript.organism_id=organism.organism_id
+where transcript.type_id in (select cvterm_id 
+                             from cvterm
+                             where name in ('snRNA', 'tRNA', 'transcript', 'pseudogenic_transcript', 'rRNA', 'mRNA'))
+and exon.type_id = (select cvterm_id
+                    from cvterm
+                    where name = 'exon')
+and exon_colour.type_id = (select cvterm_id
+                           from cvterm 
+                           join cv using (cv_id)
+                           where cvterm.name='colour'
+                           and cv.name='genedb_misc')
+and exon_colour.rank = 0
+group by organism, transcript_id, transcript_name
+having count(distinct exon_colour.value) > 1; 
+                            
+                            
+                            
