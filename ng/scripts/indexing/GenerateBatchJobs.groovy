7a270d63316884f090b288312a5d3602902cd4bf
art <art@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-03-08 16:10:22 +0000

    Missed one - or 2
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@13424 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/scripts/indexing/GenerateBatchJobs.groovy
+++ ng/scripts/indexing/GenerateBatchJobs.groovy
@@ -46,7 +46,7 @@ export JAVA_HOME=/software/pathogen/external/applications/java/java6
 export ANT_HOME=/software/pathogen/external/applications/ant/apache-ant
 export PATH=\${ANT_HOME}/bin:\${JAVA_HOME}/bin:\$PATH
 unset DISPLAY
-cd "${srcDirRoot.toAbsolutePath()}"
+cd \"${srcDirRoot.toAbsolutePath()}\"
 """
 
 def queueName = options.q ? options.q : "yesterday";
