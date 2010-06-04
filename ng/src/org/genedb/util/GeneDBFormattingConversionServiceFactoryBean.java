8ab30ac879110454ae752cd7801d1b5a40a0710c
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-06-04 15:52:07 +0000

    GENEDB-710 - Added a logger.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@13952 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/src/org/genedb/util/GeneDBFormattingConversionServiceFactoryBean.java
+++ ng/src/org/genedb/util/GeneDBFormattingConversionServiceFactoryBean.java
@@ -2,6 +2,7 @@ package org.genedb.util;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.springframework.format.Formatter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.support.FormattingConversionServiceFactoryBean;
@@ -10,13 +11,14 @@ public class GeneDBFormattingConversionServiceFactoryBean extends
 		FormattingConversionServiceFactoryBean {
 
 	private Map<Class<?>, Formatter<?>> formatters;
+	private static final Logger logger = Logger.getLogger(GeneDBFormattingConversionServiceFactoryBean.class);
 
 	@Override
 	protected void installFormatters(FormatterRegistry registry) {
 		super.installFormatters(registry);
-		System.err.println("*** Registering ");
+		logger.info("*** Registering ");
 		for (Map.Entry<Class<?>, Formatter<?>> entry : formatters.entrySet()) {
-			System.err.println("*** Registering '"+entry.getKey()+"', '"+entry.getValue()+"'");
+			logger.info("*** Registering '"+entry.getKey()+"', '"+entry.getValue()+"'");
 			registry.addFormatterForFieldType(entry.getKey(), entry.getValue());
 		}
 	}
