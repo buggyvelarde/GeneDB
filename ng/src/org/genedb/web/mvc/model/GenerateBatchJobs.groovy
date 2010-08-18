f84461c6614f553b48dec46a7cab01c246919576
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-08-18 12:07:23 +0000

    fixed boilerplate
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@14570 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
+++ ng/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
@@ -31,8 +31,10 @@ export JAVA_HOME=/software/pathogen/external/applications/java/java6
 export ANT_HOME=/software/pathogen/external/applications/ant/apache-ant
 export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
 unset DISPLAY
-cd ${sourceHome}
+
 '''
+
+boilerPlate += 'cd "' +  sourceHome + '"'  
 
 def queueName = "yesterday";
 
