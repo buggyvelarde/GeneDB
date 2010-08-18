70f8a501b83d26daeeb8251f020760b08ee23e03
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-08-18 12:55:08 +0000

    removed references to bulk - this should be specified in the supplied script arguments
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@14576 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/scripts/indexing/index.sh
+++ ng/scripts/indexing/index.sh
@@ -143,13 +143,13 @@ echo "Groovy Orgs List: $ORGANISMS_JOINED"
 # Generate lucene indicces and check for any errors.
 #
 
-mkdir -p $TMPDIR/bulk/Lucene/scripts
+mkdir -p $TMPDIR/Lucene/scripts
 GENERATE_LUCENE="groovy -cp $POSTGRES_DRIVER $SOURCE_HOME/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy Lucene nightly $SOURCE_HOME $TMPDIR $ORGANISMS_JOINED "
 #echo $GENERATE_LUCENE
 eval $GENERATE_LUCENE
 
 
-LUCENE_ERRORS=`cat $TMPDIR/bulk/Lucene/scripts/*.err`
+LUCENE_ERRORS=`cat $TMPDIR/Lucene/scripts/*.err`
 LEN_LUCENE_ERRORS=${#VAR}
 if [[ $LUCENE_ERRORS > 0 ]]; then
     echo "Found errors in Lucene"
@@ -165,13 +165,13 @@ fi
 #
 
 cd $SOURCE_HOME
-rm -fr $TMPDIR/bulk/Lucene/destination
+rm -fr $TMPDIR/Lucene/destination
 
 # gv1 - tested on laptop:
 # ant -f build-apps.xml -Dconfig=gv1-osx -Dmerge.lucene.destination=/Users/gv1/Desktop/lucene/merged/ -Dmerge.lucene.origin=/Users/gv1/Desktop/lucene/organisms/ runMergeLuceneIndices
 
 
-MERGE_LUCENE="ant -f build-apps.xml -Dconfig=nightly -Dmerge.lucene.destination=$TMPDIR/bulk/Lucene/destination -Dmerge.lucene.origin=$TMPDIR/bulk/Lucene/output runMergeLuceneIndices"
+MERGE_LUCENE="ant -f build-apps.xml -Dconfig=nightly -Dmerge.lucene.destination=$TMPDIR/Lucene/destination -Dmerge.lucene.origin=$TMPDIR/Lucene/output runMergeLuceneIndices"
 #echo $MERGE_LUCENE
 eval $MERGE_LUCENE
 
@@ -179,7 +179,7 @@ for OUTDIR in $OUTDIRS
 do
     echo "Copying merged lucenes to $OUTDIR/lucene"
     rm -fr $OUTDIR/lucene
-    cp -r  $TMPDIR/bulk/Lucene/destination  $OUTDIR/lucene
+    cp -r  $TMPDIR/Lucene/destination  $OUTDIR/lucene
 done
 
 # gv1 - hardcoded exit for testing
@@ -200,13 +200,13 @@ echo MAKE_DICTIONARY_LUCENE $MAKE_DICTIONARY_LUCENE
 # Generate DTO caches and check for errors.
 #
 
-mkdir -p $TMPDIR/bulk/DTO/scripts
+mkdir -p $TMPDIR/DTO/scripts
 GENERATE_DTO="groovy -cp $POSTGRES_DRIVER $SOURCE_HOME/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy DTO nightly $SOURCE_HOME $TMPDIR $ORGANISMS_JOINED "
 echo GENERATE_DTO $GENERATE_DTO
 ###${GENERATE_DTO}
 
 
-DTO_ERRORS=`cat $TMPDIR/bulk/DTO/scripts/*.err`
+DTO_ERRORS=`cat $TMPDIR/DTO/scripts/*.err`
 LEN_DTO_ERRORS=${#VAR}
 if [[ $LEN_DTO_ERRORS > 0 ]]; then
     echo "Found errors in DTO"
@@ -220,20 +220,20 @@ fi
 # Merge the DTO caches into place. Sshing into a BIG MEM machine to do this. 
 #
 
-###rm -fr $TMPDIR/bulk/DTO/destination
+###rm -fr $TMPDIR/DTO/destination
 
 # gv1 - tested on laptop:
 # ant -f build-apps.xml -Dconfig=gv1-osx-cachetest -Dmerge.indices.destination=/Users/gv1/Desktop/dto/merged/ -Dmerge.indices.origin=/Users/gv1/Desktop/dto/output/ runMergeIndices 
 
-MERGE_DTO="ant -f $SOURCE_HOME/ant-build.xml -Dconfig=nightly -Dmerge.indices.destination=$TMPDIR/bulk/DTO/destination -Dmerge.indices.origin=$TMPDIR/bulk/DTO/output runMergeIndices"
+MERGE_DTO="ant -f $SOURCE_HOME/ant-build.xml -Dconfig=nightly -Dmerge.indices.destination=$TMPDIR/DTO/destination -Dmerge.indices.origin=$TMPDIR/DTO/output runMergeIndices"
 echo $MERGE_DTO
 ###ssh pcs4s "$MERGE_DTO"
 
 for OUTDIR in $OUTDIRS
 do
-    echo "Copying merged indices from $TMPDIR/bulk/DTO/destination to $OUTDIR/cache"
+    echo "Copying merged indices from $TMPDIR/DTO/destination to $OUTDIR/cache"
     ###rm -fr $OUTDIR/cache;
-    ###cp -r $TMPDIR/bulk/DTO/destination $OUTDIR/cache;
+    ###cp -r $TMPDIR/DTO/destination $OUTDIR/cache;
 done
 
 
