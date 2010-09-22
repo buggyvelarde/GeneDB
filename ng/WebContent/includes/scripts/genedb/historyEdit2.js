36cbd6533470b9c835120ca34044daf9e5351bca
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-09-22 16:56:42 +0000

    Added change event listeners to the format type radio buttons.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@14767 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/WebContent/includes/scripts/genedb/historyEdit2.js
+++ ng/WebContent/includes/scripts/genedb/historyEdit2.js
@@ -13,21 +13,63 @@ function toggle() {
 	});
 }
 
-function tabOutputChosen() {
-	$(".tabOutput").show();
-	$(".noTabOutput").hide();
-	$("selectFieldTitle").text("Select fields for columns");
-}
 
-function htmlOutputChosen() {
-	$(".htmlOutput").show();
-	$(".noHtmlOutput").hide();
-	$("selectFieldTitle").text("Select fields for columns");
-}
 
-function fastaOutputChosen() {
-	$(".sequenceOutput").show();
-	$(".noSequenceOutput").hide();
-	$("selectFieldTitle").text("Select fields for FASTA headers");
-}
 
+$(function(){
+	
+	$("input[name='sequenceType']").change(function (event) {
+		
+        var value = $(event.target).val();
+        
+        // actually disabling the form elements results in their value not being sent
+        if (value == "INTERGENIC_5") {
+        	$('#prime5').show();
+        	$('#prime3').hide();
+        	
+        } else if (value == "INTERGENIC_3") {
+        	
+        	$('#prime5').hide();
+        	$('#prime3').show();
+        	
+        } else if (value == "INTERGENIC_3and5") {
+        	
+        	$('#prime5').show();
+        	$('#prime3').show();
+        	
+        } else {
+        	
+        	$('#prime5').hide();
+        	$('#prime3').hide();
+        	        	
+        }
+    });
+	
+	$("input[name='cust_format']").change(function (event) {
+		
+		var value = $(event.target).val();
+		
+		if (value == "TAB") {
+			$(".tabOutput").show();
+			$(".noTabOutput").hide();
+			$("#selectFieldTitle").html("Select fields for TAB columns");
+		
+		} else if ((value == "HTML") ||  (value == "EXCEL")) { // we can treat HTML and EXCEL to have equivalent options for now
+			$(".htmlOutput").show();
+			$(".noHtmlOutput").hide();
+			$("#selectFieldTitle").html("Select fields for HTML columns");
+		
+		} else if (value == "FASTA") {
+			$(".sequenceOutput").show();
+			$(".noSequenceOutput").hide();
+			$("#selectFieldTitle").html("Select fields for FASTA headers");
+			
+		}
+		
+	});
+	
+	$("input[name='sequenceType']:first").change();
+	$("input[name='cust_format']:first").change();
+	
+	
+});
