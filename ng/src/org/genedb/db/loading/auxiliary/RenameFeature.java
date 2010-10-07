5612b50f36dfd741225650e7b978012e681ccca5
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-10-07 15:05:53 +0000

    Added support for storing previous systematic IDS.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@14822 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/src/org/genedb/db/loading/auxiliary/RenameFeature.java
+++ ng/src/org/genedb/db/loading/auxiliary/RenameFeature.java
@@ -4,12 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.gmod.schema.mapped.CvTerm;
 import org.gmod.schema.mapped.Feature;
+import org.gmod.schema.mapped.FeatureSynonym;
+import org.gmod.schema.mapped.Synonym;
 import org.hibernate.Session;
 
 /**
@@ -28,45 +32,91 @@ public class RenameFeature extends Loader {
 	protected void doLoad(InputStream inputStream, Session session)
 			throws IOException {
 		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        CvTerm synonymType = cvDao.getCvTermByNameAndCvName("previous_systematic_id", "genedb_synonym_type");
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
-
+        
         while ((line = reader.readLine()) != null) { 
             if(line.length() > 0) {
-            	try {
-            		String[] split = line.split(delimiter);
-                	
-                	if (split.length < 2) {
-                		continue;
-                	}
-                	
-                	String oldUniqueName = split[0];
-                	String newUniqueName = split[1];
-                	
-                	Feature feature = sequenceDao.getFeatureByUniqueName(oldUniqueName, Feature.class);
-                	
-                	if (oldUniqueName.length() < 1 || newUniqueName.length() < 1) {
-                		throw new Exception(String.format("Error on this line : %s", line));
-                	}
-                	
-                	logger.info(String.format("%s -> %s", oldUniqueName, newUniqueName));
-                	
-                	if (feature != null) {
-                		feature.setUniqueName(newUniqueName);
-                		session.update(feature);
-                		
-            		} else {
-            			logger.warn(String.format("Could not find %s", oldUniqueName));
-            		}
-                	
-                	
-            	} catch (Exception e) {
-            		throw new RuntimeException (String.format("Problem (%x) with this line: %s", e.getMessage(), line), e);
-            		
+            	
+        		String[] split = line.split(delimiter);
+            	
+            	if (split.length < 2) {
+            		continue;
             	}
             	
+            	String oldUniqueName = split[0];
+            	String newUniqueName = split[1];
+            	
+            	Feature feature = sequenceDao.getFeatureByUniqueName(oldUniqueName, Feature.class);
+            	
+            	if (oldUniqueName.length() < 1 || newUniqueName.length() < 1) {
+            		throw new RuntimeException(String.format("Error on this line : %s.", line));
+            	}
+            	
+            	logger.info(String.format("Converting '%s' -> '%s'", oldUniqueName, newUniqueName));
+            	
+            	if (feature != null) {
+            		feature.setUniqueName(newUniqueName);
+            		
+            		Collection<String> previous = feature.getPreviousSystematicIds();
+            		
+            		logger.info("Previous Systematic IDS : " + previous);
+            		
+            		if (! previous.contains(oldUniqueName)) {
+            			
+            			logger.info("Storing '" + oldUniqueName + "' as a previous systematic ID");
+            			
+            			Synonym synonym = new Synonym();
+            			
+            			synonym.setName(oldUniqueName);
+            			synonym.setSynonymSGML(oldUniqueName);
+            			synonym.setType(synonymType);
+            			
+            			FeatureSynonym featureSynonym = feature.addSynonym(synonym);
+            			
+            			// must be set to false for it to be considered a previous systematic id
+            			featureSynonym.setCurrent(false);
+            			
+            			session.persist(synonym);
+            			session.persist(featureSynonym);
+            			
+            		} else {
+            			
+            			logger.info("Already a previous systematic ID: '" + oldUniqueName + "'");
+            			
+            		}
+            		
+            		if (previous.contains(newUniqueName)) {
+            			
+            			for (FeatureSynonym  featureSynonym: feature.getFeatureSynonyms()) {
+            				
+            				Synonym synonym = featureSynonym.getSynonym();
+            				
+            				if (synonym.getName().equals(newUniqueName) && synonym.getType().equals(synonymType)) {
+            					
+            					logger.warn("Removing '" + newUniqueName + "' from the synonyms list (as it's now the current name)");
+            					
+            					session.delete(featureSynonym);
+            					session.delete(synonym);
+            					
+            				}
+            				
+            			}
+            			
+            		}
+            		
+            		session.update(feature);
+            		logger.info("New name: '" + feature.getUniqueName() + "'");
+            		
+        		} else {
+        			
+        			logger.error(String.format("Could not find feature with uniquename '%s'", oldUniqueName));
+        			
+        		}
+                
             }
-
         }
         
 	}
