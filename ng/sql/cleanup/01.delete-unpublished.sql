3eb8d6b877c86f081b09ec8db37810d0682d8792
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-10-08 16:07:47 +0000

    Modified cleanup script to delete the organisms entirely. Added Preichenowi to it.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@14827 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/sql/cleanup/01.delete-unpublished.sql
+++ ng/sql/cleanup/01.delete-unpublished.sql
@@ -1,18 +1,14 @@
-delete from feature
-using organism
-where feature.organism_id = organism.organism_id
-and (
-        (
-            organism.genus = 'Staphylococcus'
-            and organism.species in ('aureus (EMRSA15)', 'aureus (LGA251)', 'aureus (TW20)')
-        )
-        or (
-            organism.genus = 'Plasmodium'
-            and organism.species in ('berghei (3x coverage)')
-        )
-        or (
-            organism.genus = 'Streptococcus'
-            and organism.species in ('pneumoniae TIGR4', 'pneumoniae D39', 'pneumoniae OXC141', 'pneumoniae ATCC 700669')
-        )
-)
+delete from organism where 
+    (
+        organism.genus = 'Staphylococcus'
+        and organism.species in ('aureus (EMRSA15)', 'aureus (LGA251)', 'aureus (TW20)')
+    )
+    or (
+        organism.genus = 'Plasmodium'
+        and organism.species in ('berghei (3x coverage)', 'reichenowi')
+    )
+    or (
+        organism.genus = 'Streptococcus'
+        and organism.species in ('pneumoniae TIGR4', 'pneumoniae D39', 'pneumoniae OXC141', 'pneumoniae ATCC 700669')
+    )
 ;
