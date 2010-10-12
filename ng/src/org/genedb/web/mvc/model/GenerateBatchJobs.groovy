d029b5ab82d30b7f50b9a860c43786bdabd1b47b
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-10-12 12:33:40 +0000

    Addec debugging statements.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@14838 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
+++ ng/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
@@ -85,7 +85,7 @@ println config
 def propertyFilePath = "property-file.${config}"
 println propertyFilePath
 
-def configLoaded = new ConfigSlurper().parse(new File(propertyFilePath).toURL())
+def configLoaded = new ConfigSlurper().parse(propertyFilePath)
 
 println configLoaded
 
