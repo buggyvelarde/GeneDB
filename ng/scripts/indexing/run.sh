07969dc3b287ab5cc0bbfa26538e74e0ab3420fc
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-11-15 15:42:40 +0000

    Attempt to set dynamic path.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@15026 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/scripts/indexing/run.sh
+++ ng/scripts/indexing/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+SCRIPT_DIRECTORY=`dirname $(readlink -f $0)`
+echo SCRIPT_DIRECTORY $SCRIPT_DIRECTORY
+
 export PATH=/software/pathogen/external/applications/groovy/groovy/bin:$PATH
-sh /nfs/pathdb/index/svn/ng/scripts/indexing/index.sh
+sh $SCRIPT_DIRECTORY/index.sh
