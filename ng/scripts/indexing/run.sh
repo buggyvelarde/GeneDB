8d6411883b565db22cc8d0d8cc1e5e4a608794f1
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-11-15 15:48:06 +0000

    Added parameter passing.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@15027 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/scripts/indexing/run.sh
+++ ng/scripts/indexing/run.sh
@@ -4,4 +4,4 @@ SCRIPT_DIRECTORY=`dirname $(readlink -f $0)`
 echo SCRIPT_DIRECTORY $SCRIPT_DIRECTORY
 
 export PATH=/software/pathogen/external/applications/groovy/groovy/bin:$PATH
-sh $SCRIPT_DIRECTORY/index.sh
+sh $SCRIPT_DIRECTORY/index.sh "$@"
