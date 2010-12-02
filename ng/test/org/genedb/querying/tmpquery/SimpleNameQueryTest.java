f43e2f6db7c7f151f3aaa3eb5678fe87454b971a
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-12-02 16:11:44 +0000

    Attempt to fix the simple name query test.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@15150 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/test/org/genedb/querying/tmpquery/SimpleNameQueryTest.java
+++ ng/test/org/genedb/querying/tmpquery/SimpleNameQueryTest.java
@@ -2,6 +2,9 @@ package org.genedb.querying.tmpquery;
 
 import static org.junit.Assert.*;
 
+import org.genedb.db.taxon.TaxonNode;
+import org.genedb.db.taxon.TaxonNodeList;
+import org.genedb.db.taxon.TaxonNodeManager;
 import org.genedb.querying.core.QueryException;
 
 import org.junit.Test;
@@ -11,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:testContext-query.xml"})
@@ -18,15 +22,26 @@ public class SimpleNameQueryTest {
 
     @Autowired
     private SimpleNameQuery simpleNameQuery;
-
+    
+    @Autowired
+	private ApplicationContext applicationContext;
+    
 
     @SuppressWarnings("unchecked")
     @Test
     public void basic() throws QueryException {
-
+    	
+    	TaxonNodeManager tnm = (TaxonNodeManager) applicationContext.getBean("taxonNodeManager", TaxonNodeManager.class);
+    	TaxonNode taxonNode = tnm.getTaxonNodeForLabel("Root");
+    	TaxonNodeList taxons = new TaxonNodeList(taxonNode);
+    	
+    	simpleNameQuery.setTaxons(taxons);
+    	
         simpleNameQuery.setSearch("PF14_*");
         List results = simpleNameQuery.getResults();
-        assertEquals(0, results.size());
+        assertTrue(results.size() > 0);
+        
+        
     }
 
 }
