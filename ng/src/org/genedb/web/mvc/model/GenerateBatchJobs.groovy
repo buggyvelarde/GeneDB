a40beeb70386131dd39f27708b9c04dbd5d67e3a
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-12-11 19:14:51 +0000

    Increaded indexing and caching memory.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@15222 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
+++ ng/src/org/genedb/web/mvc/model/GenerateBatchJobs.groovy
@@ -98,12 +98,12 @@ for (org in orgs) {
     case "Lucene":
         new File("${baseDir}/output/${org}").mkdir()
         // execLine="ant -f build-apps.xml -Dconfig=${config} -Dorganism=${org} -Ddir=${baseDir}/output/${org} _LuceneIndex "
-		execLine="java -server -Djava.awt.headless=true  -Xmx512m  -classpath ${cacheClassPath}  org.genedb.web.mvc.model.PopulateLuceneIndices -o ${org} -i ${baseDir}/output/${org} ${dbname} "
+		execLine="java -server -Djava.awt.headless=true  -Xmx1024m  -classpath ${cacheClassPath}  org.genedb.web.mvc.model.PopulateLuceneIndices -o ${org} -i ${baseDir}/output/${org} ${dbname} "
         break;
 
     case "DTO":
         // execLine="ant -f build-apps.xml -Dconfig=${config} -Dorganism=${org} -Ddir=${baseDir}/output/${org} _PopulateCaches "
-		execLine="java -server -Djava.awt.headless=true  -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -classpath ${cacheClassPath} org.genedb.web.mvc.model.PopulateCaches -o ${org} --globalFileRoot ${baseDir}/output/${org} "
+		execLine="java -server -Djava.awt.headless=true  -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -classpath ${cacheClassPath} org.genedb.web.mvc.model.PopulateCaches -o ${org} --globalFileRoot ${baseDir}/output/${org} "
         new File("${baseDir}/output/${org}").mkdir()
         break;
 
