d77f0781aa3a0b97a7e2239118c12007d90a45c2
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-02-08 10:20:58 +0000

    Cleanup.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@15447 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/scripts/indexing/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIRECTORY=`dirname $(readlink -f $0)`
-echo SCRIPT_DIRECTORY $SCRIPT_DIRECTORY
-
-export PATH=/software/pathogen/external/applications/groovy/groovy/bin:$PATH
-sh $SCRIPT_DIRECTORY/index.sh "$@"
