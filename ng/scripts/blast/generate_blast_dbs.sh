1af05f82a5f0987dff57d6eeb2dca910b8861494
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-02-16 14:25:00 +0000

    Changed to nightly.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@15543 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/scripts/blast/generate_blast_dbs.sh
+++ ng/scripts/blast/generate_blast_dbs.sh
@@ -20,9 +20,12 @@ BLAST_DB_PATH="/lustre/scratch101/blastdb/Pathogen/website/genedb/"
 ORIGINAL_IFS=$IFS
 IFS=$'\n'
 
+PGPASSWORD="genedb"
+
 GET_ORGANISMS_SQL="select distinct(o.common_name) from organism o, feature f where f.organism_id = o.organism_id and o.common_name != 'dummy'"
 logecho ${GET_ORGANISMS_SQL}
-ORGANISMS_COMMAND="ORGANISMS=\`psql -t -h pgsrv1.internal.sanger.ac.uk -U pathdb -c \"${GET_ORGANISMS_SQL}\" pathogens\`"
+
+ORGANISMS_COMMAND="ORGANISMS=\`psql -t -h pgsrv2 -U genedb -c \"${GET_ORGANISMS_SQL}\" nightly\`"
 doeval $ORGANISMS_COMMAND
     
 for organism in $ORGANISMS
@@ -34,14 +37,14 @@ do
      	
      	logecho "Dumping ${organism} : "
      	
-     	DUMP_PROTEINS="chado_dump_proteins -s -o ${organism} > ${BLAST_DB_PATH}/GeneDB_${organism}_Proteins"
+     	DUMP_PROTEINS="chado_dump_proteins -s -o ${organism} > ${BLAST_DB_PATH}GeneDB_${organism}_Proteins"
      	doeval $DUMP_PROTEINS
      	
-     	DUMP_PROTEINS="chado_dump_transcripts ${organism} > ${BLAST_DB_PATH}/GeneDB_${organism}_Genes"
-     	doeval $DUMP_PROTEINS
+     	DUMP_TRANSCRIPTS="chado_dump_transcripts ${organism} > ${BLAST_DB_PATH}GeneDB_${organism}_Genes"
+     	doeval $DUMP_TRANSCRIPTS
      	
-     	DUMP_PROTEINS="chado_dump_genome -o ${organism} > ${BLAST_DB_PATH}/GeneDB_${organism}_Contigs"
-     	doeval $DUMP_PROTEINS
+     	DUMP_GENOME="chado_dump_genome -o ${organism} > ${BLAST_DB_PATH}GeneDB_${organism}_Contigs"
+     	doeval $DUMP_GENOME
      	
      	logecho "Dumped!"
      	
