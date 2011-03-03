349ebc057e8356fb31cf4ee836eff0dfd787ef21
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-03-03 14:51:05 +0000

    Added trailing semicolon.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@15644 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/sql/cleanup/01.delete-unpublished.sql
+++ ng/sql/cleanup/01.delete-unpublished.sql
@@ -3,5 +3,4 @@ DELETE FROM organism WHERE organism_id IN (
         FROM organismprop 
         JOIN cvterm ON organismprop.type_id = cvterm.cvterm_id AND cvterm.name IN ('webservices_public', 'genedb_public') 
         WHERE value = 'no'
-)
-
+);
