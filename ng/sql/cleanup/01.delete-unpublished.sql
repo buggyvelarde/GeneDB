0e1738b2c834bfdffc59e0ca7e9ae5e39b358884
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-05-03 09:55:41 +0000

    Fixed delete unpublished script, that was filtering too heavily.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@16014 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/sql/cleanup/01.delete-unpublished.sql
+++ ng/sql/cleanup/01.delete-unpublished.sql
@@ -1,6 +1,22 @@
 DELETE FROM organism WHERE organism_id IN (
-    SELECT distinct(organism_id) 
-        FROM organismprop 
-        JOIN cvterm ON organismprop.type_id = cvterm.cvterm_id AND cvterm.name IN ('webservices_public', 'genedb_public') 
-        WHERE value = 'no'
+    SELECT distinct(organism.organism_id) 
+        FROM 
+            organism, 
+            organismprop webservices_public_prop, 
+            organismprop genedb_public_prop, 
+            cvterm webservices_public_cvterm, 
+            cvterm genedb_public_cvterm 
+        WHERE
+            webservices_public_prop.organism_id = organism.organism_id
+        AND
+            genedb_public_prop.organism_id = organism.organism_id
+        AND
+            webservices_public_prop.type_id = webservices_public_cvterm.cvterm_id 
+                AND webservices_public_cvterm.name = 'webservices_public' 
+                AND webservices_public_prop.value = 'no'
+        AND 
+            genedb_public_prop.type_id = genedb_public_cvterm.cvterm_id 
+                AND genedb_public_cvterm.name = 'genedb_public' 
+                AND genedb_public_prop.value = 'no'
+        
 );
