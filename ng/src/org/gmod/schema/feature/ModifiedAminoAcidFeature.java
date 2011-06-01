398efc497e8750c46d4a241a3abf597ffd29c94e
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-06-01 11:39:23 +0000

    Added indexing.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/genedb-ng/trunk@16199 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- ng/src/org/gmod/schema/feature/ModifiedAminoAcidFeature.java
+++ ng/src/org/gmod/schema/feature/ModifiedAminoAcidFeature.java
@@ -3,9 +3,11 @@ package org.gmod.schema.feature;
 import javax.persistence.Entity;
 
 import org.gmod.schema.cfg.FeatureType;
+import org.hibernate.search.annotations.Indexed;
 
 @Entity
 @FeatureType(cv="sequence", term="modified_amino_acid_feature")
+@Indexed
 public class ModifiedAminoAcidFeature extends AminoAcid {
 	
 }
