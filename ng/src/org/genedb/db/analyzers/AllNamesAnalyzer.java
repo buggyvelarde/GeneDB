e717eaeedcf83a5d6368f852a9ac9fa76d21d891
Giles Velarde <gv1@sanger.ac.uk>
2011-11-07 14:51:32 +0000

    added tokenizing on commas - experimental see RT 227590

--- ng/src/org/genedb/db/analyzers/AllNamesAnalyzer.java
+++ ng/src/org/genedb/db/analyzers/AllNamesAnalyzer.java
@@ -1,6 +1,9 @@
 package org.genedb.db.analyzers;
 
 import java.io.Reader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
@@ -14,12 +17,24 @@ public class AllNamesAnalyzer extends Analyzer {
     }
 
     private class AllNamesTokenizer extends WhitespaceTokenizer {
+
+        protected Set<Character> tokens = new HashSet<Character>(Arrays.asList(new Character[] { ',' }));
+
         public AllNamesTokenizer(Reader in) {
             super(in);
         }
+
         @Override
         protected char normalize(char c) {
             return Character.toLowerCase(c);
-          }
+        }
+        
+        @Override
+        protected boolean isTokenChar(char c) {
+            if (super.isTokenChar(c)) 
+                return true;
+            return tokens.contains(c);
+        }
+
     }
 }
